f227de2e5f3441ca7d46476eda4983c52939b005
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-05-26 11:21:35 +0200

    Update mapextension.js

--- General Hotel Extensions/mapextension.js
+++ General Hotel Extensions/mapextension.js
@@ -6,8 +6,8 @@
     render: ({ trace, element }) => {
       const GoogleMap = document.createElement('iframe');
       const { apiKey, origin, destination, travelMode, zoom, height, width } = trace.payload;
-      GoogleMap.width = width || '600';
-      GoogleMap.height = height || '400';
+      GoogleMap.width = width || height;
+      GoogleMap.height = height || width;
       GoogleMap.style.border = '0';
       GoogleMap.loading = 'lazy';
       GoogleMap.allowFullscreen = true;
