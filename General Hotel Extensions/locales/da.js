585b962c9017246200dd84f2cdc0aadf294e39d3
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:43:44 +0200

    Create da.js

---
+++ General Hotel Extensions/locales/da.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Pris fra: {price} {currency}",
+    capacityText: "Ideel til: {min}-{max} personer",
+    sizeText: "Størrelse: {size} m²",
+
+    // General UI
+    next: "Næste", back: "Tilbage", edit: "Rediger", submit: "Send", selected: "Valgt", days: "dage", day: "dag",
+    // Step 1: Dates
+    titleChooseYourStay: "Vælg dine opholdsdatoer", undertitleChooseYourStay: "Vælg perioden for dit planlagte ophold", noDatesSelected: "Ingen datoer valgt",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Vælg hvor længe du ønsker at blive i den valgte periode", selectDuration: "Vælg varighed", exactlyAsSpecified: "Præcis som angivet", threeDays: "3 dage", fourDays: "4 dage", fiveDays: "5 dage", sixDays: "6 dage", sevenDays: "7 dage", eightDays: "8 dage", nineDays: "9 dage", tenDays: "10 dage", enterExactDates: "Eller indtast den præcise rejselængde i dage", from: "Fra", until: "Til", youCanChoose: "Du kan tilføje maksimalt", maxDaysAllowedText: "Du kan tilføje op til {totalDays} dage",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Vælg dit ophold", undertitlePickAccommodation: "Du kan vælge en eller flere typer indkvartering. Hvis du reserverer for flere personer, tilføj flere efter behov.", FormSuites: "Suiter", FormRooms: "Værelser", accommodationCategories: "Indkvarteringskategorier",
+    // Step 4: Travelers
+    titleWhoTraveling: "Hvem skal rejse?", undertitleWhoTraveling: "Angiv antallet af voksne og børn, og fortæl os, hvis du har særlige ønsker.", adults14Plus: "Voksne (14+)", children: "Børn", specialRequests: "Særlige ønsker (fx kost, tilgængelighed, kæledyr)", specialRequestsTxt: "Fortæl os venligst, hvis du har særlige ønsker.",
+    // Step 5: Contact
+    titleContactInformation: "Kontaktoplysninger", undertitleContactInformationBooking: "Angiv venligst dine oplysninger, så vi kan fuldføre din reservation.", firstName: "Fornavn", lastName: "Efternavn", emailVF: "E-mail", phoneNumber: "Telefonnummer",
+    // Step 6: Review
+    titleReview: "Gennemse og send", undertitleReviewBooking: "Gennemgå venligst dine reservationsoplysninger, inden du sender.", reviewStayDates: "Opholdsdatoer", reviewTravelDates: "Rejsedatoer", reviewDurationOFStay: "Opholdets varighed", reviewAccommodation: "Indkvartering", reviewTypes: "Typer", reviewTravelers: "Rejsende", reviewAdults: "Voksne", reviewSpecialRequests: "Særlige ønsker", firstAndLastname: "Fornavn og efternavn",
+    // Final Message
+    thankSubmission: "Tak for din forespørgsel!", formSubmitted: "Din formular er blevet sendt.", formTeam: "Vores team vil kontakte dig snarest.",
+};
