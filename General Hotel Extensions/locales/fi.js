7e45c38e1fe26c30d28db84ff86d438f96754382
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:44:33 +0200

    Create fi.js

---
+++ General Hotel Extensions/locales/fi.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Hinta alkaen: {price} {currency}",
+    capacityText: "Ihanteellinen: {min}-{max} henkilölle",
+    sizeText: "Koko: {size} m²",
+
+    // General UI
+    next: "Seuraava", back: "Takaisin", edit: "Muokkaa", submit: "Lähetä", selected: "Valittu", days: "päivää", day: "päivä",
+    // Step 1: Dates
+    titleChooseYourStay: "Valitse majoituksesi päivämäärät", undertitleChooseYourStay: "Valitse suunnitellun oleskelusi ajanjakso", noDatesSelected: "Ei valittuja päivämääriä",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Valitse kuinka kauan haluat viipyä valittuna ajanjaksona", selectDuration: "Valitse kesto", exactlyAsSpecified: "Täsmälleen kuten määritetty", threeDays: "3 päivää", fourDays: "4 päivää", fiveDays: "5 päivää", sixDays: "6 päivää", sevenDays: "7 päivää", eightDays: "8 päivää", nineDays: "9 päivää", tenDays: "10 päivää", enterExactDates: "Tai syötä matkan tarkka kesto päivinä", from: "Alkaen", until: "Päättyen", youCanChoose: "Voit lisätä enintään", maxDaysAllowedText: "Voit lisätä enintään {totalDays} päivää",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Valitse majoitus", undertitlePickAccommodation: "Voit valita yhden tai useamman majoitustyypin. Jos varaat useammalle henkilölle, lisää tarvittaessa lisää.", FormSuites: "Sviitit", FormRooms: "Huoneet", accommodationCategories: "Majoitusluokat",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kuka matkustaa?", undertitleWhoTraveling: "Anna aikuisten ja lasten määrä ja kerro meille, jos sinulla on erityistoiveita.", adults14Plus: "Aikuiset (14+)", children: "Lapset", specialRequests: "Erityistoiveet (esim. ruokavalio, esteettömyys, lemmikit)", specialRequestsTxt: "Kerro meille, jos sinulla on erityistoiveita.",
+    // Step 5: Contact
+    titleContactInformation: "Yhteystiedot", undertitleContactInformationBooking: "Anna tietosi, jotta voimme viimeistellä varauksesi.", firstName: "Etunimi", lastName: "Sukunimi", emailVF: "Sähköposti", phoneNumber: "Puhelinnumero",
+    // Step 6: Review
+    titleReview: "Tarkista ja lähetä", undertitleReviewBooking: "Tarkista varauksesi tiedot ennen lähettämistä.", reviewStayDates: "Majoituspäivät", reviewTravelDates: "Matkapäivät", reviewDurationOFStay: "Oleskelun kesto", reviewAccommodation: "Majoitus", reviewTypes: "Tyypit", reviewTravelers: "Matkustajat", reviewAdults: "Aikuiset", reviewSpecialRequests: "Erityistoiveet", firstAndLastname: "Etunimi ja sukunimi",
+    // Final Message
+    thankSubmission: "Kiitos pyynnöstäsi!", formSubmitted: "Lomakkeesi on lähetetty onnistuneesti.", formTeam: "Tiimimme ottaa sinuun yhteyttä pian.",
+};
