5d94e2d33a3b0e19e1c632be516922f580e2d529
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:36:01 +0200

    Create nl.js

---
+++ General Hotel Extensions/locales/nl.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Prijs vanaf: {price} {currency}",
+    capacityText: "Ideaal voor: {min}-{max} personen",
+    sizeText: "Grootte: {size} m²",
+
+    // General UI
+    next: "Volgende", back: "Terug", edit: "Bewerken", submit: "Verzenden", selected: "Geselecteerd", days: "dagen", day: "dag",
+    // Step 1: Dates
+    titleChooseYourStay: "Kies de data van je verblijf", undertitleChooseYourStay: "Selecteer de periode van je geplande verblijf", noDatesSelected: "Geen data geselecteerd",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Selecteer hoe lang je wilt verblijven in de gekozen periode", selectDuration: "Selecteer een duur", exactlyAsSpecified: "Precies zoals opgegeven", threeDays: "3 dagen", fourDays: "4 dagen", fiveDays: "5 dagen", sixDays: "6 dagen", sevenDays: "7 dagen", eightDays: "8 dagen", nineDays: "9 dagen", tenDays: "10 dagen", enterExactDates: "Of voer de exacte duur van de reis in dagen in", from: "Van", until: "Tot", youCanChoose: "Je kunt maximaal toevoegen", maxDaysAllowedText: "Je kunt maximaal {totalDays} dagen toevoegen",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Kies je accommodatie", undertitlePickAccommodation: "Je kunt één of meerdere accommodatietypen selecteren. Als je voor meerdere personen boekt, voeg er dan meer toe indien nodig.", FormSuites: "Suites", FormRooms: "Kamers", accommodationCategories: "Accommodatiecategorieën",
+    // Step 4: Travelers
+    titleWhoTraveling: "Wie gaat er reizen?", undertitleWhoTraveling: "Voer het aantal volwassenen en kinderen in en laat ons weten of je speciale wensen hebt.", adults14Plus: "Volwassenen (14+)", children: "Kinderen", specialRequests: "Speciale verzoeken (bijv. dieetwensen, toegankelijkheid, huisdieren)", specialRequestsTxt: "Laat ons weten of je speciale verzoeken hebt.",
+    // Step 5: Contact
+    titleContactInformation: "Contactgegevens", undertitleContactInformationBooking: "Geef alstublieft uw gegevens zodat we uw reservering kunnen voltooien.", firstName: "Voornaam", lastName: "Achternaam", emailVF: "E-mail", phoneNumber: "Telefoonnummer",
+    // Step 6: Review
+    titleReview: "Controleren en verzenden", undertitleReviewBooking: "Controleer uw reserveringsgegevens voordat u verzendt.", reviewStayDates: "Verblijfsdata", reviewTravelDates: "Reisdata", reviewDurationOFStay: "Duur van het verblijf", reviewAccommodation: "Accommodatie", reviewTypes: "Typen", reviewTravelers: "Reizigers", reviewAdults: "Volwassenen", reviewSpecialRequests: "Speciale verzoeken", firstAndLastname: "Voor- en achternaam",
+    // Final Message
+    thankSubmission: "Bedankt voor uw aanvraag!", formSubmitted: "Uw formulier is succesvol verzonden.", formTeam: "Ons team neemt spoedig contact met u op.",
+};
