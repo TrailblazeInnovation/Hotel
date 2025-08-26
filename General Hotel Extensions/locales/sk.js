36841cf09d37986f417301dd4fb4a7bbf845b720
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:38:01 +0200

    Create sk.js

---
+++ General Hotel Extensions/locales/sk.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Cena od: {price} {currency}",
+    capacityText: "Ideálne pre: {min}-{max} osôb",
+    sizeText: "Veľkosť: {size} m²",
+
+    // General UI
+    next: "Ďalej", back: "Späť", edit: "Upraviť", submit: "Odoslať", selected: "Vybraté", days: "dni", day: "deň",
+    // Step 1: Dates
+    titleChooseYourStay: "Vyberte dátumy pobytu", undertitleChooseYourStay: "Vyberte obdobie plánovaného pobytu", noDatesSelected: "Nie sú vybraté žiadne dátumy",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Vyberte, ako dlho chcete zostať vo vybranom období", selectDuration: "Vyberte dĺžku pobytu", exactlyAsSpecified: "Presne ako je uvedené", threeDays: "3 dni", fourDays: "4 dni", fiveDays: "5 dní", sixDays: "6 dní", sevenDays: "7 dní", eightDays: "8 dní", nineDays: "9 dní", tenDays: "10 dní", enterExactDates: "Alebo zadajte presnú dĺžku cesty v dňoch", from: "Od", until: "Do", youCanChoose: "Môžete pridať maximálne", maxDaysAllowedText: "Môžete pridať maximálne {totalDays} dní",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Vyberte si ubytovanie", undertitlePickAccommodation: "Môžete si vybrať jeden alebo viac typov ubytovania. Ak rezervujete pre viac osôb, pridajte ďalšie podľa potreby.", FormSuites: "Suita", FormRooms: "Izby", accommodationCategories: "Kategórie ubytovania",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kto bude cestovať?", undertitleWhoTraveling: "Zadajte počet dospelých a detí a dajte nám vedieť, či máte špeciálne požiadavky.", adults14Plus: "Dospelí (14+)", children: "Deti", specialRequests: "Špeciálne požiadavky (napr. strava, prístupnosť, domáce zvieratá)", specialRequestsTxt: "Dajte nám prosím vedieť, či máte špeciálne požiadavky.",
+    // Step 5: Contact
+    titleContactInformation: "Kontaktné údaje", undertitleContactInformationBooking: "Prosím, zadajte svoje údaje, aby sme mohli dokončiť vašu rezerváciu.", firstName: "Meno", lastName: "Priezvisko", emailVF: "E-mail", phoneNumber: "Telefónne číslo",
+    // Step 6: Review
+    titleReview: "Skontrolovať a odoslať", undertitleReviewBooking: "Skontrolujte údaje svojej rezervácie pred odoslaním.", reviewStayDates: "Dátumy pobytu", reviewTravelDates: "Dátumy cesty", reviewDurationOFStay: "Dĺžka pobytu", reviewAccommodation: "Ubytovanie", reviewTypes: "Typy", reviewTravelers: "Cestujúci", reviewAdults: "Dospelí", reviewSpecialRequests: "Špeciálne požiadavky", firstAndLastname: "Meno a priezvisko",
+    // Final Message
+    thankSubmission: "Ďakujeme za vašu žiadosť!", formSubmitted: "Formulár bol úspešne odoslaný.", formTeam: "Náš tím vás bude čoskoro kontaktovať.",
+};
