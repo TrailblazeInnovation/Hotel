6e2d8384a7c2699a46f53a43a172a4791cc38df3
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:37:15 +0200

    Create ro.js

---
+++ General Hotel Extensions/locales/ro.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Preț de la: {price} {currency}",
+    capacityText: "Ideal pentru: {min}-{max} persoane",
+    sizeText: "Dimensiune: {size} m²",
+
+    // General UI
+    next: "Următor", back: "Înapoi", edit: "Editează", submit: "Trimite", selected: "Selectat", days: "zile", day: "zi",
+    // Step 1: Dates
+    titleChooseYourStay: "Alege datele sejurului tău", undertitleChooseYourStay: "Selectează perioada sejurului planificat", noDatesSelected: "Nicio dată selectată",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Selectează cât timp dorești să stai în perioada aleasă", selectDuration: "Selectează o durată", exactlyAsSpecified: "Exact așa cum este specificat", threeDays: "3 zile", fourDays: "4 zile", fiveDays: "5 zile", sixDays: "6 zile", sevenDays: "7 zile", eightDays: "8 zile", nineDays: "9 zile", tenDays: "10 zile", enterExactDates: "Sau introdu durata exactă a călătoriei în zile", from: "De la", until: "Până la", youCanChoose: "Poți adăuga maximum", maxDaysAllowedText: "Poți adăuga maximum {totalDays} zile",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Alege cazarea", undertitlePickAccommodation: "Poți selecta unul sau mai multe tipuri de cazare. Dacă rezervi pentru mai multe persoane, adaugă mai multe după necesitate.", FormSuites: "Suite", FormRooms: "Camere", accommodationCategories: "Categorii de cazare",
+    // Step 4: Travelers
+    titleWhoTraveling: "Cine va călători?", undertitleWhoTraveling: "Introdu numărul de adulți și copii și spune-ne dacă ai cerințe speciale.", adults14Plus: "Adulți (14+)", children: "Copii", specialRequests: "Cerințe speciale (ex. dietă, accesibilitate, animale de companie)", specialRequestsTxt: "Te rugăm să ne spui dacă ai cerințe speciale.",
+    // Step 5: Contact
+    titleContactInformation: "Informații de contact", undertitleContactInformationBooking: "Te rugăm să ne oferi datele tale pentru a finaliza rezervarea.", firstName: "Prenume", lastName: "Nume", emailVF: "E-mail", phoneNumber: "Număr de telefon",
+    // Step 6: Review
+    titleReview: "Verifică și trimite", undertitleReviewBooking: "Te rugăm să verifici datele rezervării înainte de trimitere.", reviewStayDates: "Datele sejurului", reviewTravelDates: "Datele călătoriei", reviewDurationOFStay: "Durata sejurului", reviewAccommodation: "Cazare", reviewTypes: "Tipuri", reviewTravelers: "Călători", reviewAdults: "Adulți", reviewSpecialRequests: "Cerințe speciale", firstAndLastname: "Prenume și nume",
+    // Final Message
+    thankSubmission: "Îți mulțumim pentru cerere!", formSubmitted: "Formularul tău a fost trimis cu succes.", formTeam: "Echipa noastră te va contacta în curând.",
+};
