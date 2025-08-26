967c26d0f426a1d5cff0ec0e94624283c488e893
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:52:03 +0200

    Create th.js

---
+++ General Hotel Extensions/locales/th.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "ราคาเริ่มต้น: {price} {currency}",
+    capacityText: "เหมาะสำหรับ: {min}-{max} คน",
+    sizeText: "ขนาด: {size} ตร.ม.",
+
+    // General UI
+    next: "ถัดไป", back: "ย้อนกลับ", edit: "แก้ไข", submit: "ส่ง", selected: "เลือกแล้ว", days: "วัน", day: "วัน",
+    // Step 1: Dates
+    titleChooseYourStay: "เลือกวันที่เข้าพักของคุณ", undertitleChooseYourStay: "โปรดเลือกช่วงเวลาที่คุณต้องการเข้าพัก", noDatesSelected: "ยังไม่ได้เลือกวันที่",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "เลือกว่าคุณต้องการพักนานเท่าใดในช่วงเวลาที่เลือก", selectDuration: "เลือกระยะเวลา", exactlyAsSpecified: "ตามที่ระบุไว้", threeDays: "3 วัน", fourDays: "4 วัน", fiveDays: "5 วัน", sixDays: "6 วัน", sevenDays: "7 วัน", eightDays: "8 วัน", nineDays: "9 วัน", tenDays: "10 วัน", enterExactDates: "หรือกรอกระยะเวลาการเดินทางที่แน่นอนเป็นวัน", from: "จาก", until: "ถึง", youCanChoose: "คุณสามารถเพิ่มได้สูงสุด", maxDaysAllowedText: "คุณสามารถเพิ่มได้สูงสุด {totalDays} วัน",
+    // Step 3: Accommodation
+    titlePickAccommodation: "เลือกที่พัก", undertitlePickAccommodation: "คุณสามารถเลือกที่พักได้หนึ่งประเภทหรือหลายประเภท หากคุณจองสำหรับหลายคน ให้เพิ่มตามความจำเป็น.", FormSuites: "ห้องสวีท", FormRooms: "ห้องพัก", accommodationCategories: "ประเภทที่พัก",
+    // Step 4: Travelers
+    titleWhoTraveling: "ใครจะเดินทาง?", undertitleWhoTraveling: "กรุณาระบุจำนวนผู้ใหญ่และเด็ก และแจ้งให้เราทราบหากคุณมีคำขอพิเศษ.", adults14Plus: "ผู้ใหญ่ (14+)", children: "เด็ก", specialRequests: "คำขอพิเศษ (เช่น อาหาร การเข้าถึง สัตว์เลี้ยง)", specialRequestsTxt: "โปรดแจ้งให้เราทราบหากคุณมีคำขอพิเศษ.",
+    // Step 5: Contact
+    titleContactInformation: "ข้อมูลติดต่อ", undertitleContactInformationBooking: "โปรดให้ข้อมูลของคุณเพื่อที่เราจะสามารถดำเนินการจองให้เสร็จสิ้น.", firstName: "ชื่อ", lastName: "นามสกุล", emailVF: "อีเมล", phoneNumber: "หมายเลขโทรศัพท์",
+    // Step 6: Review
+    titleReview: "ตรวจสอบและส่ง", undertitleReviewBooking: "โปรดตรวจสอบข้อมูลการจองของคุณก่อนส่ง.", reviewStayDates: "วันที่เข้าพัก", reviewTravelDates: "วันที่เดินทาง", reviewDurationOFStay: "ระยะเวลาการเข้าพัก", reviewAccommodation: "ที่พัก", reviewTypes: "ประเภท", reviewTravelers: "ผู้เดินทาง", reviewAdults: "ผู้ใหญ่", reviewSpecialRequests: "คำขอพิเศษ", firstAndLastname: "ชื่อและนามสกุล",
+    // Final Message
+    thankSubmission: "ขอบคุณสำหรับคำขอของคุณ!", formSubmitted: "แบบฟอร์มของคุณถูกส่งเรียบร้อยแล้ว.", formTeam: "ทีมงานของเราจะติดต่อคุณในไม่ช้า.",
+};
