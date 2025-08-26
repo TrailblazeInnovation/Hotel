f11c375b2833d06b5b224375b91c498ef93211b3
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:45:56 +0200

    Create hi.js

---
+++ General Hotel Extensions/locales/hi.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "कीमत से शुरू: {price} {currency}",
+    capacityText: "उपयुक्त: {min}-{max} व्यक्ति",
+    sizeText: "आकार: {size} वर्ग मीटर",
+
+    // General UI
+    next: "आगे", back: "पीछे", edit: "संपादित करें", submit: "जमा करें", selected: "चयनित", days: "दिन", day: "दिन",
+    // Step 1: Dates
+    titleChooseYourStay: "अपने ठहरने की तिथियाँ चुनें", undertitleChooseYourStay: "कृपया अपनी नियोजित यात्रा की अवधि चुनें", noDatesSelected: "कोई तिथि चयनित नहीं",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "चुनें कि आप चुनी गई अवधि में कितने दिन रहना चाहते हैं", selectDuration: "अवधि चुनें", exactlyAsSpecified: "जैसा निर्दिष्ट है", threeDays: "3 दिन", fourDays: "4 दिन", fiveDays: "5 दिन", sixDays: "6 दिन", sevenDays: "7 दिन", eightDays: "8 दिन", nineDays: "9 दिन", tenDays: "10 दिन", enterExactDates: "या यात्रा की सटीक अवधि दिनों में दर्ज करें", from: "से", until: "तक", youCanChoose: "आप अधिकतम जोड़ सकते हैं", maxDaysAllowedText: "आप अधिकतम {totalDays} दिन जोड़ सकते हैं",
+    // Step 3: Accommodation
+    titlePickAccommodation: "अपना आवास चुनें", undertitlePickAccommodation: "आप एक या एक से अधिक प्रकार के आवास चुन सकते हैं। यदि आप कई लोगों के लिए बुकिंग कर रहे हैं, तो आवश्यकता अनुसार और जोड़ें।", FormSuites: "सुइट्स", FormRooms: "कक्ष", accommodationCategories: "आवास श्रेणियाँ",
+    // Step 4: Travelers
+    titleWhoTraveling: "कौन यात्रा करेगा?", undertitleWhoTraveling: "कृपया वयस्कों और बच्चों की संख्या दर्ज करें और हमें बताएं यदि आपके पास कोई विशेष अनुरोध है।", adults14Plus: "वयस्क (14+)", children: "बच्चे", specialRequests: "विशेष अनुरोध (जैसे भोजन, पहुंच, पालतू जानवर)", specialRequestsTxt: "कृपया हमें बताएं यदि आपके पास कोई विशेष अनुरोध है।",
+    // Step 5: Contact
+    titleContactInformation: "संपर्क जानकारी", undertitleContactInformationBooking: "कृपया अपनी जानकारी प्रदान करें ताकि हम आपकी बुकिंग पूरी कर सकें।", firstName: "पहला नाम", lastName: "अंतिम नाम", emailVF: "ईमेल", phoneNumber: "फ़ोन नंबर",
+    // Step 6: Review
+    titleReview: "समीक्षा करें और जमा करें", undertitleReviewBooking: "कृपया जमा करने से पहले अपनी बुकिंग जानकारी की जाँच करें।", reviewStayDates: "ठहरने की तिथियाँ", reviewTravelDates: "यात्रा की तिथियाँ", reviewDurationOFStay: "ठहरने की अवधि", reviewAccommodation: "आवास", reviewTypes: "प्रकार", reviewTravelers: "यात्री", reviewAdults: "वयस्क", reviewSpecialRequests: "विशेष अनुरोध", firstAndLastname: "पहला और अंतिम नाम",
+    // Final Message
+    thankSubmission: "आपके अनुरोध के लिए धन्यवाद!", formSubmitted: "आपका फ़ॉर्म सफलतापूर्वक जमा हो गया है।", formTeam: "हमारी टीम जल्द ही आपसे संपर्क करेगी।",
+};
