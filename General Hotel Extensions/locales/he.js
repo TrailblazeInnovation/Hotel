62f977251f3db71b8c596bc1845df4394370c5e0
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:45:12 +0200

    Create he.js

---
+++ General Hotel Extensions/locales/he.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "מחיר מ: {price} {currency}",
+    capacityText: "מתאים ל: {min}-{max} אנשים",
+    sizeText: "גודל: {size} מ״ר",
+
+    // General UI
+    next: "הבא", back: "חזרה", edit: "ערוך", submit: "שלח", selected: "נבחר", days: "ימים", day: "יום",
+    // Step 1: Dates
+    titleChooseYourStay: "בחר את תאריכי השהייה שלך", undertitleChooseYourStay: "בחר את התקופה המתוכננת לשהייתך", noDatesSelected: "לא נבחרו תאריכים",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "בחר כמה זמן תרצה לשהות בתקופה שנבחרה", selectDuration: "בחר משך זמן", exactlyAsSpecified: "בדיוק כפי שצוין", threeDays: "3 ימים", fourDays: "4 ימים", fiveDays: "5 ימים", sixDays: "6 ימים", sevenDays: "7 ימים", eightDays: "8 ימים", nineDays: "9 ימים", tenDays: "10 ימים", enterExactDates: "או הזן את משך הנסיעה המדויק בימים", from: "מ-", until: "עד", youCanChoose: "ניתן להוסיף לכל היותר", maxDaysAllowedText: "ניתן להוסיף לכל היותר {totalDays} ימים",
+    // Step 3: Accommodation
+    titlePickAccommodation: "בחר את הלינה שלך", undertitlePickAccommodation: "באפשרותך לבחור סוג אחד או יותר של לינה. אם אתה מזמין עבור מספר אנשים, הוסף עוד לפי הצורך.", FormSuites: "סוויטות", FormRooms: "חדרים", accommodationCategories: "קטגוריות לינה",
+    // Step 4: Travelers
+    titleWhoTraveling: "מי יטייל?", undertitleWhoTraveling: "הזן את מספר המבוגרים והילדים ויידע אותנו אם יש לך בקשות מיוחדות.", adults14Plus: "מבוגרים (14+)", children: "ילדים", specialRequests: "בקשות מיוחדות (למשל תזונה, נגישות, חיות מחמד)", specialRequestsTxt: "אנא יידע אותנו אם יש לך בקשות מיוחדות.",
+    // Step 5: Contact
+    titleContactInformation: "פרטי קשר", undertitleContactInformationBooking: "אנא ספק את פרטיך כדי שנוכל להשלים את ההזמנה.", firstName: "שם פרטי", lastName: "שם משפחה", emailVF: "אימייל", phoneNumber: "מספר טלפון",
+    // Step 6: Review
+    titleReview: "בדוק ושלח", undertitleReviewBooking: "אנא בדוק את פרטי ההזמנה שלך לפני השליחה.", reviewStayDates: "תאריכי שהייה", reviewTravelDates: "תאריכי נסיעה", reviewDurationOFStay: "משך השהייה", reviewAccommodation: "לינה", reviewTypes: "סוגים", reviewTravelers: "נוסעים", reviewAdults: "מבוגרים", reviewSpecialRequests: "בקשות מיוחדות", firstAndLastname: "שם פרטי ומשפחה",
+    // Final Message
+    thankSubmission: "תודה על פנייתך!", formSubmitted: "הטופס נשלח בהצלחה.", formTeam: "הצוות שלנו יחזור אליך בקרוב.",
+};
