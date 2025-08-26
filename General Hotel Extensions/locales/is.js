c2e39b009811d633817c97e6327faf3c55d1897d
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:58:23 +0200

    Create is.js

---
+++ General Hotel Extensions/locales/is.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Verð frá: {price} {currency}",
+    capacityText: "Tilvalið fyrir: {min}-{max} manns",
+    sizeText: "Stærð: {size} m²",
+
+    // General UI
+    next: "Næsta", back: "Til baka", edit: "Breyta", submit: "Senda", selected: "Valið", days: "dagar", day: "dagur",
+    // Step 1: Dates
+    titleChooseYourStay: "Veldu dvalardaga þína", undertitleChooseYourStay: "Veldu tímabilið fyrir áætlaða dvöl þína", noDatesSelected: "Engir dagar valdir",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Veldu hversu lengi þú vilt vera á valda tímabilinu", selectDuration: "Veldu lengd", exactlyAsSpecified: "Nákvæmlega eins og tilgreint", threeDays: "3 dagar", fourDays: "4 dagar", fiveDays: "5 dagar", sixDays: "6 dagar", sevenDays: "7 dagar", eightDays: "8 dagar", nineDays: "9 dagar", tenDays: "10 dagar", enterExactDates: "Eða sláðu inn nákvæma ferðalengd í dögum", from: "Frá", until: "Til", youCanChoose: "Þú getur bætt við að hámarki", maxDaysAllowedText: "Þú getur bætt við allt að {totalDays} dögum",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Veldu gistingu", undertitlePickAccommodation: "Þú getur valið eina eða fleiri tegundir gistingar. Ef þú bókar fyrir fleiri en eina manneskju, bættu við fleiri eftir þörfum.", FormSuites: "Svíturnar", FormRooms: "Herbergi", accommodationCategories: "Gistiflokkar",
+    // Step 4: Travelers
+    titleWhoTraveling: "Hverjir munu ferðast?", undertitleWhoTraveling: "Sláðu inn fjölda fullorðinna og barna og láttu okkur vita ef þú hefur sérstakar óskir.", adults14Plus: "Fullorðnir (14+)", children: "Börn", specialRequests: "Sérstakar óskir (t.d. mataræði, aðgengi, gæludýr)", specialRequestsTxt: "Vinsamlegast láttu okkur vita ef þú hefur sérstakar óskir.",
+    // Step 5: Contact
+    titleContactInformation: "Tengiliðaupplýsingar", undertitleContactInformationBooking: "Vinsamlegast gefðu upp upplýsingarnar þínar svo við getum lokið bókuninni.", firstName: "Fornafn", lastName: "Eftirnafn", emailVF: "Netfang", phoneNumber: "Símanúmer",
+    // Step 6: Review
+    titleReview: "Yfirfara og senda", undertitleReviewBooking: "Vinsamlegast yfirfarðu bókunarupplýsingarnar þínar áður en þú sendir.", reviewStayDates: "Dvalardagar", reviewTravelDates: "Ferðadagar", reviewDurationOFStay: "Lengd dvalar", reviewAccommodation: "Gisting", reviewTypes: "Tegundir", reviewTravelers: "Ferðalangar", reviewAdults: "Fullorðnir", reviewSpecialRequests: "Sérstakar óskir", firstAndLastname: "Fornafn og eftirnafn",
+    // Final Message
+    thankSubmission: "Takk fyrir beiðnina þína!", formSubmitted: "Formið þitt hefur verið sent inn með góðum árangri.", formTeam: "Teymið okkar mun hafa samband við þig fljótlega.",
+};
