b6a47c0d6250deaa071d71246ea453309558bae8
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:51:21 +0200

    Create lv.js

---
+++ General Hotel Extensions/locales/lv.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Cena no: {price} {currency}",
+    capacityText: "Ideāli piemērots: {min}-{max} personām",
+    sizeText: "Izmērs: {size} m²",
+
+    // General UI
+    next: "Tālāk", back: "Atpakaļ", edit: "Rediģēt", submit: "Iesniegt", selected: "Izvēlēts", days: "dienas", day: "diena",
+    // Step 1: Dates
+    titleChooseYourStay: "Izvēlieties savas uzturēšanās datumus", undertitleChooseYourStay: "Lūdzu, izvēlieties plānotās uzturēšanās periodu", noDatesSelected: "Nav izvēlēti datumi",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Izvēlieties, cik ilgi vēlaties palikt izvēlētajā periodā", selectDuration: "Izvēlieties ilgumu", exactlyAsSpecified: "Tieši kā norādīts", threeDays: "3 dienas", fourDays: "4 dienas", fiveDays: "5 dienas", sixDays: "6 dienas", sevenDays: "7 dienas", eightDays: "8 dienas", nineDays: "9 dienas", tenDays: "10 dienas", enterExactDates: "Vai ievadiet precīzu ceļojuma ilgumu dienās", from: "No", until: "Līdz", youCanChoose: "Varat pievienot ne vairāk kā", maxDaysAllowedText: "Varat pievienot līdz {totalDays} dienām",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Izvēlieties naktsmītni", undertitlePickAccommodation: "Varat izvēlēties vienu vai vairākus naktsmītņu veidus. Ja rezervējat vairākiem cilvēkiem, pievienojiet vairāk pēc nepieciešamības.", FormSuites: "Apartamenti", FormRooms: "Numuri", accommodationCategories: "Naktsmītņu kategorijas",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kas ceļos?", undertitleWhoTraveling: "Ievadiet pieaugušo un bērnu skaitu un paziņojiet mums, ja jums ir īpašas prasības.", adults14Plus: "Pieaugušie (14+)", children: "Bērni", specialRequests: "Īpašas prasības (piemēram, diēta, piekļuve, mājdzīvnieki)", specialRequestsTxt: "Lūdzu, informējiet mūs, ja jums ir īpašas prasības.",
+    // Step 5: Contact
+    titleContactInformation: "Kontaktinformācija", undertitleContactInformationBooking: "Lūdzu, sniedziet savu informāciju, lai mēs varētu pabeigt jūsu rezervāciju.", firstName: "Vārds", lastName: "Uzvārds", emailVF: "E-pasts", phoneNumber: "Telefona numurs",
+    // Step 6: Review
+    titleReview: "Pārskatīt un iesniegt", undertitleReviewBooking: "Lūdzu, pārskatiet savas rezervācijas informāciju pirms iesniegšanas.", reviewStayDates: "Uzturēšanās datumi", reviewTravelDates: "Ceļojuma datumi", reviewDurationOFStay: "Uzturēšanās ilgums", reviewAccommodation: "Naktsmītne", reviewTypes: "Veidi", reviewTravelers: "Ceļotāji", reviewAdults: "Pieaugušie", reviewSpecialRequests: "Īpašas prasības", firstAndLastname: "Vārds un uzvārds",
+    // Final Message
+    thankSubmission: "Paldies par jūsu pieprasījumu!", formSubmitted: "Jūsu forma ir veiksmīgi iesniegta.", formTeam: "Mūsu komanda drīzumā sazināsies ar jums.",
+};
