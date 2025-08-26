bfcba11ee045fd938921b4577348e66a1c971035
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:36:25 +0200

    Create pl.js

---
+++ General Hotel Extensions/locales/pl.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Cena od: {price} {currency}",
+    capacityText: "Idealne dla: {min}-{max} osób",
+    sizeText: "Powierzchnia: {size} m²",
+
+    // General UI
+    next: "Dalej", back: "Wstecz", edit: "Edytuj", submit: "Wyślij", selected: "Wybrano", days: "dni", day: "dzień",
+    // Step 1: Dates
+    titleChooseYourStay: "Wybierz daty pobytu", undertitleChooseYourStay: "Wybierz okres planowanego pobytu", noDatesSelected: "Nie wybrano dat",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Wybierz, jak długo chcesz zostać w wybranym okresie", selectDuration: "Wybierz długość pobytu", exactlyAsSpecified: "Dokładnie jak określono", threeDays: "3 dni", fourDays: "4 dni", fiveDays: "5 dni", sixDays: "6 dni", sevenDays: "7 dni", eightDays: "8 dni", nineDays: "9 dni", tenDays: "10 dni", enterExactDates: "Lub wprowadź dokładny czas podróży w dniach", from: "Od", until: "Do", youCanChoose: "Możesz dodać maksymalnie", maxDaysAllowedText: "Możesz dodać maksymalnie {totalDays} dni",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Wybierz zakwaterowanie", undertitlePickAccommodation: "Możesz wybrać jeden lub kilka typów zakwaterowania. Jeśli rezerwujesz dla kilku osób, dodaj więcej w razie potrzeby.", FormSuites: "Apartamenty", FormRooms: "Pokoje", accommodationCategories: "Kategorie zakwaterowania",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kto będzie podróżować?", undertitleWhoTraveling: "Podaj liczbę dorosłych i dzieci oraz poinformuj nas, jeśli masz specjalne życzenia.", adults14Plus: "Dorośli (14+)", children: "Dzieci", specialRequests: "Specjalne życzenia (np. dieta, dostępność, zwierzęta)", specialRequestsTxt: "Prosimy, poinformuj nas, jeśli masz specjalne życzenia.",
+    // Step 5: Contact
+    titleContactInformation: "Informacje kontaktowe", undertitleContactInformationBooking: "Podaj swoje dane, abyśmy mogli dokończyć rezerwację.", firstName: "Imię", lastName: "Nazwisko", emailVF: "E-mail", phoneNumber: "Numer telefonu",
+    // Step 6: Review
+    titleReview: "Sprawdź i wyślij", undertitleReviewBooking: "Sprawdź dane swojej rezerwacji przed wysłaniem.", reviewStayDates: "Daty pobytu", reviewTravelDates: "Daty podróży", reviewDurationOFStay: "Długość pobytu", reviewAccommodation: "Zakwaterowanie", reviewTypes: "Typy", reviewTravelers: "Podróżni", reviewAdults: "Dorośli", reviewSpecialRequests: "Specjalne życzenia", firstAndLastname: "Imię i nazwisko",
+    // Final Message
+    thankSubmission: "Dziękujemy za zgłoszenie!", formSubmitted: "Twój formularz został pomyślnie wysłany.", formTeam: "Nasz zespół skontaktuje się z Tobą wkrótce.",
+};
