75f2c7e72f65ef206659a52413041df598f8f521
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:47:49 +0200

    Create no.js

---
+++ General Hotel Extensions/locales/no.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Pris fra: {price} {currency}",
+    capacityText: "Ideell for: {min}-{max} personer",
+    sizeText: "Størrelse: {size} m²",
+
+    // General UI
+    next: "Neste", back: "Tilbake", edit: "Rediger", submit: "Send inn", selected: "Valgt", days: "dager", day: "dag",
+    // Step 1: Dates
+    titleChooseYourStay: "Velg oppholdsdatoene dine", undertitleChooseYourStay: "Velg perioden for det planlagte oppholdet ditt", noDatesSelected: "Ingen datoer valgt",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Velg hvor lenge du vil bli i den valgte perioden", selectDuration: "Velg varighet", exactlyAsSpecified: "Akkurat som spesifisert", threeDays: "3 dager", fourDays: "4 dager", fiveDays: "5 dager", sixDays: "6 dager", sevenDays: "7 dager", eightDays: "8 dager", nineDays: "9 dager", tenDays: "10 dager", enterExactDates: "Eller skriv inn nøyaktig reiselengde i dager", from: "Fra", until: "Til", youCanChoose: "Du kan legge til maksimalt", maxDaysAllowedText: "Du kan legge til opptil {totalDays} dager",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Velg overnatting", undertitlePickAccommodation: "Du kan velge en eller flere typer overnatting. Hvis du bestiller for flere personer, legg til flere etter behov.", FormSuites: "Suiter", FormRooms: "Rom", accommodationCategories: "Overnattingskategorier",
+    // Step 4: Travelers
+    titleWhoTraveling: "Hvem skal reise?", undertitleWhoTraveling: "Oppgi antall voksne og barn, og fortell oss om du har spesielle ønsker.", adults14Plus: "Voksne (14+)", children: "Barn", specialRequests: "Spesielle ønsker (f.eks. diett, tilgjengelighet, kjæledyr)", specialRequestsTxt: "Vennligst gi oss beskjed hvis du har spesielle ønsker.",
+    // Step 5: Contact
+    titleContactInformation: "Kontaktinformasjon", undertitleContactInformationBooking: "Vennligst oppgi opplysningene dine slik at vi kan fullføre bestillingen din.", firstName: "Fornavn", lastName: "Etternavn", emailVF: "E-post", phoneNumber: "Telefonnummer",
+    // Step 6: Review
+    titleReview: "Gjennomgå og send inn", undertitleReviewBooking: "Vennligst gjennomgå bestillingsopplysningene dine før du sender.", reviewStayDates: "Oppholdsdatoer", reviewTravelDates: "Reisedatoer", reviewDurationOFStay: "Oppholdets varighet", reviewAccommodation: "Overnatting", reviewTypes: "Typer", reviewTravelers: "Reisende", reviewAdults: "Voksne", reviewSpecialRequests: "Spesielle ønsker", firstAndLastname: "Fornavn og etternavn",
+    // Final Message
+    thankSubmission: "Takk for forespørselen din!", formSubmitted: "Skjemaet ditt er sendt inn.", formTeam: "Vårt team vil kontakte deg snart.",
+};
