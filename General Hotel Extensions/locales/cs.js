e333c1f8317b2daecce8d02c1563956bfc489b2e
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:43:00 +0200

    Create cs.js

---
+++ General Hotel Extensions/locales/cs.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Cena od: {price} {currency}",
+    capacityText: "Ideální pro: {min}-{max} osob",
+    sizeText: "Velikost: {size} m²",
+
+    // General UI
+    next: "Další", back: "Zpět", edit: "Upravit", submit: "Odeslat", selected: "Vybráno", days: "dní", day: "den",
+    // Step 1: Dates
+    titleChooseYourStay: "Vyberte data pobytu", undertitleChooseYourStay: "Vyberte období plánovaného pobytu", noDatesSelected: "Nebyly vybrány žádné termíny",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Vyberte, jak dlouho chcete zůstat ve vybraném období", selectDuration: "Vyberte délku pobytu", exactlyAsSpecified: "Přesně dle zadání", threeDays: "3 dny", fourDays: "4 dny", fiveDays: "5 dní", sixDays: "6 dní", sevenDays: "7 dní", eightDays: "8 dní", nineDays: "9 dní", tenDays: "10 dní", enterExactDates: "Nebo zadejte přesnou délku cesty ve dnech", from: "Od", until: "Do", youCanChoose: "Můžete přidat maximálně", maxDaysAllowedText: "Můžete přidat maximálně {totalDays} dní",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Vyberte ubytování", undertitlePickAccommodation: "Můžete vybrat jeden nebo více typů ubytování. Pokud rezervujete pro více osob, přidejte další podle potřeby.", FormSuites: "Apartmá", FormRooms: "Pokoje", accommodationCategories: "Kategorie ubytování",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kdo bude cestovat?", undertitleWhoTraveling: "Zadejte počet dospělých a dětí a dejte nám vědět, zda máte speciální požadavky.", adults14Plus: "Dospělí (14+)", children: "Děti", specialRequests: "Speciální požadavky (např. strava, přístupnost, domácí mazlíčci)", specialRequestsTxt: "Dejte nám prosím vědět, zda máte speciální požadavky.",
+    // Step 5: Contact
+    titleContactInformation: "Kontaktní informace", undertitleContactInformationBooking: "Uveďte prosím své údaje, abychom mohli dokončit vaši rezervaci.", firstName: "Jméno", lastName: "Příjmení", emailVF: "E-mail", phoneNumber: "Telefonní číslo",
+    // Step 6: Review
+    titleReview: "Zkontrolovat a odeslat", undertitleReviewBooking: "Před odesláním prosím zkontrolujte údaje své rezervace.", reviewStayDates: "Termíny pobytu", reviewTravelDates: "Termíny cesty", reviewDurationOFStay: "Délka pobytu", reviewAccommodation: "Ubytování", reviewTypes: "Typy", reviewTravelers: "Cestující", reviewAdults: "Dospělí", reviewSpecialRequests: "Speciální požadavky", firstAndLastname: "Jméno a příjmení",
+    // Final Message
+    thankSubmission: "Děkujeme za vaši žádost!", formSubmitted: "Formulář byl úspěšně odeslán.", formTeam: "Náš tým vás bude brzy kontaktovat.",
+};
