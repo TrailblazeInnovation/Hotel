99a72ca1e150d7eee6d15fdcd93e867b462bb9f8
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:37:38 +0200

    Create ru.js

---
+++ General Hotel Extensions/locales/ru.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Цена от: {price} {currency}",
+    capacityText: "Идеально для: {min}-{max} человек",
+    sizeText: "Размер: {size} м²",
+
+    // General UI
+    next: "Далее", back: "Назад", edit: "Редактировать", submit: "Отправить", selected: "Выбрано", days: "дней", day: "день",
+    // Step 1: Dates
+    titleChooseYourStay: "Выберите даты пребывания", undertitleChooseYourStay: "Выберите период вашего планируемого пребывания", noDatesSelected: "Даты не выбраны",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Выберите, как долго вы хотите оставаться в выбранный период", selectDuration: "Выберите продолжительность", exactlyAsSpecified: "Точно как указано", threeDays: "3 дня", fourDays: "4 дня", fiveDays: "5 дней", sixDays: "6 дней", sevenDays: "7 дней", eightDays: "8 дней", nineDays: "9 дней", tenDays: "10 дней", enterExactDates: "Или введите точную продолжительность поездки в днях", from: "С", until: "По", youCanChoose: "Вы можете добавить максимум", maxDaysAllowedText: "Вы можете добавить максимум {totalDays} дней",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Выберите жильё", undertitlePickAccommodation: "Вы можете выбрать один или несколько типов жилья. Если вы бронируете для нескольких человек, добавьте больше при необходимости.", FormSuites: "Люксы", FormRooms: "Номера", accommodationCategories: "Категории жилья",
+    // Step 4: Travelers
+    titleWhoTraveling: "Кто будет путешествовать?", undertitleWhoTraveling: "Введите количество взрослых и детей и сообщите нам, если у вас есть особые пожелания.", adults14Plus: "Взрослые (14+)", children: "Дети", specialRequests: "Особые пожелания (например, питание, доступность, домашние животные)", specialRequestsTxt: "Пожалуйста, сообщите нам, если у вас есть особые пожелания.",
+    // Step 5: Contact
+    titleContactInformation: "Контактная информация", undertitleContactInformationBooking: "Пожалуйста, укажите свои данные, чтобы мы могли завершить бронирование.", firstName: "Имя", lastName: "Фамилия", emailVF: "Эл. почта", phoneNumber: "Номер телефона",
+    // Step 6: Review
+    titleReview: "Проверить и отправить", undertitleReviewBooking: "Пожалуйста, проверьте данные вашего бронирования перед отправкой.", reviewStayDates: "Даты пребывания", reviewTravelDates: "Даты поездки", reviewDurationOFStay: "Продолжительность пребывания", reviewAccommodation: "Жильё", reviewTypes: "Типы", reviewTravelers: "Путешественники", reviewAdults: "Взрослые", reviewSpecialRequests: "Особые пожелания", firstAndLastname: "Имя и фамилия",
+    // Final Message
+    thankSubmission: "Спасибо за вашу заявку!", formSubmitted: "Ваша форма успешно отправлена.", formTeam: "Наша команда свяжется с вами в ближайшее время.",
+};
