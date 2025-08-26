464bd0b2648cdabaa1e314000db03cc32f98d4fd
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:57:37 +0200

    Create ga.js

---
+++ General Hotel Extensions/locales/ga.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Praghas ó: {price} {currency}",
+    capacityText: "Foirfe do: {min}-{max} duine",
+    sizeText: "Méid: {size} m²",
+
+    // General UI
+    next: "Ar Aghaidh", back: "Siar", edit: "Cuir in Eagar", submit: "Seol", selected: "Roghnaithe", days: "laethanta", day: "lá",
+    // Step 1: Dates
+    titleChooseYourStay: "Roghnaigh dátaí do fhanachta", undertitleChooseYourStay: "Roghnaigh tréimhse do fhanachta phleanáilte", noDatesSelected: "Níl aon dátaí roghnaithe",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Roghnaigh cé chomh fada is mian leat fanacht sa tréimhse roghnaithe", selectDuration: "Roghnaigh fad", exactlyAsSpecified: "Go díreach mar a sonraíodh", threeDays: "3 lá", fourDays: "4 lá", fiveDays: "5 lá", sixDays: "6 lá", sevenDays: "7 lá", eightDays: "8 lá", nineDays: "9 lá", tenDays: "10 lá", enterExactDates: "Nó cuir isteach fad cruinn an turais i laethanta", from: "Ó", until: "Go", youCanChoose: "Is féidir leat a chur leis ar a mhéad", maxDaysAllowedText: "Is féidir leat suas le {totalDays} lá a chur leis",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Roghnaigh lóistín", undertitlePickAccommodation: "Is féidir leat cineál lóistín amháin nó níos mó a roghnú. Má tá tú ag cur in áirithe do níos mó daoine, cuir cinn eile leis de réir mar is gá.", FormSuites: "Súiteanna", FormRooms: "Seomraí", accommodationCategories: "Catagóirí lóistín",
+    // Step 4: Travelers
+    titleWhoTraveling: "Cé atá ag taisteal?", undertitleWhoTraveling: "Iontráil líon na ndaoine fásta agus na bpáistí agus inis dúinn má tá iarrataí speisialta agat.", adults14Plus: "Daoine Fásta (14+)", children: "Páistí", specialRequests: "Iarrataí speisialta (m.sh. aiste bia, inrochtaineacht, peataí)", specialRequestsTxt: "Inis dúinn le do thoil má tá iarrataí speisialta agat.",
+    // Step 5: Contact
+    titleContactInformation: "Eolas Teagmhála", undertitleContactInformationBooking: "Tabhair do shonraí le do thoil ionas gur féidir linn d’áirithint a chríochnú.", firstName: "Ainm", lastName: "Sloinne", emailVF: "Ríomhphost", phoneNumber: "Uimhir Teileafóin",
+    // Step 6: Review
+    titleReview: "Athbhreithnigh agus Seol", undertitleReviewBooking: "Athbhreithnigh do shonraí áirithinte le do thoil sula seoltar iad.", reviewStayDates: "Dátaí fanachta", reviewTravelDates: "Dátaí taistil", reviewDurationOFStay: "Fad fanachta", reviewAccommodation: "Lóistín", reviewTypes: "Cineálacha", reviewTravelers: "Taistealaithe", reviewAdults: "Daoine fásta", reviewSpecialRequests: "Iarrataí speisialta", firstAndLastname: "Ainm agus sloinne",
+    // Final Message
+    thankSubmission: "Go raibh maith agat as d’iarratas!", formSubmitted: "Seoladh do fhoirm go rathúil.", formTeam: "Beidh ár bhfoireann i dteagmháil leat go luath.",
+};
