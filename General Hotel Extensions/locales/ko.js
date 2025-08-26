246e93d84b8634173de6db05b641d3cde76b9511
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:47:21 +0200

    Create ko.js

---
+++ General Hotel Extensions/locales/ko.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "가격: {price} {currency} 부터",
+    capacityText: "적합 인원: {min}-{max} 명",
+    sizeText: "크기: {size} ㎡",
+
+    // General UI
+    next: "다음", back: "이전", edit: "편집", submit: "제출", selected: "선택됨", days: "일", day: "일",
+    // Step 1: Dates
+    titleChooseYourStay: "숙박 날짜를 선택하세요", undertitleChooseYourStay: "계획된 숙박 기간을 선택하세요", noDatesSelected: "선택된 날짜 없음",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "선택한 기간 동안 머물 기간을 선택하세요", selectDuration: "기간 선택", exactlyAsSpecified: "지정된 대로", threeDays: "3일", fourDays: "4일", fiveDays: "5일", sixDays: "6일", sevenDays: "7일", eightDays: "8일", nineDays: "9일", tenDays: "10일", enterExactDates: "또는 정확한 여행 기간(일)을 입력하세요", from: "부터", until: "까지", youCanChoose: "최대 추가 가능", maxDaysAllowedText: "최대 {totalDays} 일을 추가할 수 있습니다",
+    // Step 3: Accommodation
+    titlePickAccommodation: "숙소 선택", undertitlePickAccommodation: "하나 이상의 숙박 유형을 선택할 수 있습니다. 여러 명을 예약하는 경우 필요에 따라 더 추가하세요.", FormSuites: "스위트룸", FormRooms: "객실", accommodationCategories: "숙박 카테고리",
+    // Step 4: Travelers
+    titleWhoTraveling: "누가 여행하나요?", undertitleWhoTraveling: "성인과 어린이의 수를 입력하고 특별 요청이 있으면 알려주세요.", adults14Plus: "성인 (14+)", children: "어린이", specialRequests: "특별 요청 (예: 식단, 접근성, 반려동물)", specialRequestsTxt: "특별 요청이 있으면 알려주세요.",
+    // Step 5: Contact
+    titleContactInformation: "연락처 정보", undertitleContactInformationBooking: "예약을 완료할 수 있도록 정보를 제공해 주세요.", firstName: "이름", lastName: "성", emailVF: "이메일", phoneNumber: "전화번호",
+    // Step 6: Review
+    titleReview: "검토 및 제출", undertitleReviewBooking: "제출하기 전에 예약 정보를 확인해 주세요.", reviewStayDates: "숙박 날짜", reviewTravelDates: "여행 날짜", reviewDurationOFStay: "숙박 기간", reviewAccommodation: "숙소", reviewTypes: "유형", reviewTravelers: "여행자", reviewAdults: "성인", reviewSpecialRequests: "특별 요청", firstAndLastname: "이름과 성",
+    // Final Message
+    thankSubmission: "요청해 주셔서 감사합니다!", formSubmitted: "양식이 성공적으로 제출되었습니다.", formTeam: "저희 팀이 곧 연락드리겠습니다.",
+};
