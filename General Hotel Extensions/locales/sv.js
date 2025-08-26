16eea3ecfdbb3605a3c39e4aeba3c5ffc87b3ad8
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:38:47 +0200

    Create sv.js

---
+++ General Hotel Extensions/locales/sv.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Pris från: {price} {currency}",
+    capacityText: "Perfekt för: {min}-{max} personer",
+    sizeText: "Storlek: {size} m²",
+
+    // General UI
+    next: "Nästa", back: "Tillbaka", edit: "Redigera", submit: "Skicka", selected: "Vald", days: "dagar", day: "dag",
+    // Step 1: Dates
+    titleChooseYourStay: "Välj dina vistelsedatum", undertitleChooseYourStay: "Välj perioden för din planerade vistelse", noDatesSelected: "Inga datum valda",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Välj hur länge du vill stanna under den valda perioden", selectDuration: "Välj en längd", exactlyAsSpecified: "Exakt som angivet", threeDays: "3 dagar", fourDays: "4 dagar", fiveDays: "5 dagar", sixDays: "6 dagar", sevenDays: "7 dagar", eightDays: "8 dagar", nineDays: "9 dagar", tenDays: "10 dagar", enterExactDates: "Eller ange den exakta reslängden i dagar", from: "Från", until: "Till", youCanChoose: "Du kan lägga till högst", maxDaysAllowedText: "Du kan lägga till högst {totalDays} dagar",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Välj ditt boende", undertitlePickAccommodation: "Du kan välja en eller flera typer av boenden. Om du bokar för flera personer, lägg till fler vid behov.", FormSuites: "Svit(er)", FormRooms: "Rum", accommodationCategories: "Boendekategorier",
+    // Step 4: Travelers
+    titleWhoTraveling: "Vem ska resa?", undertitleWhoTraveling: "Ange antal vuxna och barn och meddela oss om du har några speciella önskemål.", adults14Plus: "Vuxna (14+)", children: "Barn", specialRequests: "Speciella önskemål (t.ex. kost, tillgänglighet, husdjur)", specialRequestsTxt: "Vänligen meddela oss om du har några speciella önskemål.",
+    // Step 5: Contact
+    titleContactInformation: "Kontaktinformation", undertitleContactInformationBooking: "Vänligen lämna dina uppgifter så vi kan slutföra din bokning.", firstName: "Förnamn", lastName: "Efternamn", emailVF: "E-post", phoneNumber: "Telefonnummer",
+    // Step 6: Review
+    titleReview: "Granska och skicka", undertitleReviewBooking: "Vänligen granska dina bokningsuppgifter innan du skickar.", reviewStayDates: "Vistelsedatum", reviewTravelDates: "Resedatum", reviewDurationOFStay: "Vistelsens längd", reviewAccommodation: "Boende", reviewTypes: "Typer", reviewTravelers: "Resenärer", reviewAdults: "Vuxna", reviewSpecialRequests: "Speciella önskemål", firstAndLastname: "För- och efternamn",
+    // Final Message
+    thankSubmission: "Tack för din förfrågan!", formSubmitted: "Ditt formulär har skickats in.", formTeam: "Vårt team kommer att kontakta dig inom kort.",
+};
