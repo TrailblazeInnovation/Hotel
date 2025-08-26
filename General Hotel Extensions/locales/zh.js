0e926df9b948a11c304df208b95df482672b2634
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:40:55 +0200

    Create zh.js

---
+++ General Hotel Extensions/locales/zh.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "价格起：{price} {currency}",
+    capacityText: "适合：{min}-{max} 人",
+    sizeText: "面积：{size} 平方米",
+
+    // General UI
+    next: "下一步", back: "返回", edit: "编辑", submit: "提交", selected: "已选择", days: "天", day: "天",
+    // Step 1: Dates
+    titleChooseYourStay: "选择您的入住日期", undertitleChooseYourStay: "请选择您计划入住的时间段", noDatesSelected: "未选择日期",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "请选择您希望在所选时间段停留的时长", selectDuration: "选择时长", exactlyAsSpecified: "完全按指定", threeDays: "3天", fourDays: "4天", fiveDays: "5天", sixDays: "6天", sevenDays: "7天", eightDays: "8天", nineDays: "9天", tenDays: "10天", enterExactDates: "或输入准确的出行天数", from: "从", until: "到", youCanChoose: "您最多可以添加", maxDaysAllowedText: "您最多可以添加 {totalDays} 天",
+    // Step 3: Accommodation
+    titlePickAccommodation: "选择住宿", undertitlePickAccommodation: "您可以选择一种或多种住宿类型。如果为多人预订，请根据需要添加更多。", FormSuites: "套房", FormRooms: "客房", accommodationCategories: "住宿类别",
+    // Step 4: Travelers
+    titleWhoTraveling: "谁将出行？", undertitleWhoTraveling: "请输入成人和儿童人数，并告诉我们您是否有特殊要求。", adults14Plus: "成人（14+）", children: "儿童", specialRequests: "特殊要求（例如饮食、无障碍、宠物）", specialRequestsTxt: "请告诉我们您是否有特殊要求。",
+    // Step 5: Contact
+    titleContactInformation: "联系方式", undertitleContactInformationBooking: "请提供您的信息，以便我们完成您的预订。", firstName: "名字", lastName: "姓氏", emailVF: "电子邮件", phoneNumber: "电话号码",
+    // Step 6: Review
+    titleReview: "检查并提交", undertitleReviewBooking: "请在提交之前检查您的预订信息。", reviewStayDates: "入住日期", reviewTravelDates: "旅行日期", reviewDurationOFStay: "入住时长", reviewAccommodation: "住宿", reviewTypes: "类型", reviewTravelers: "出行人", reviewAdults: "成人", reviewSpecialRequests: "特殊要求", firstAndLastname: "姓名",
+    // Final Message
+    thankSubmission: "感谢您的请求！", formSubmitted: "您的表单已成功提交。", formTeam: "我们的团队将尽快与您联系。",
+};
