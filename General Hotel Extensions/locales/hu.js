d57a09c73bc588d39fd27b8116090d7f2b437859
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:46:20 +0200

    Create hu.js

---
+++ General Hotel Extensions/locales/hu.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Ár ettől: {price} {currency}",
+    capacityText: "Ideális: {min}-{max} fő számára",
+    sizeText: "Méret: {size} m²",
+
+    // General UI
+    next: "Következő", back: "Vissza", edit: "Szerkesztés", submit: "Küldés", selected: "Kiválasztva", days: "nap", day: "nap",
+    // Step 1: Dates
+    titleChooseYourStay: "Válassza ki tartózkodásának dátumait", undertitleChooseYourStay: "Kérjük, válassza ki a tervezett tartózkodás időszakát", noDatesSelected: "Nincs kiválasztott dátum",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Válassza ki, mennyi ideig szeretne maradni a kiválasztott időszakban", selectDuration: "Válasszon időtartamot", exactlyAsSpecified: "Pontosan a megadottak szerint", threeDays: "3 nap", fourDays: "4 nap", fiveDays: "5 nap", sixDays: "6 nap", sevenDays: "7 nap", eightDays: "8 nap", nineDays: "9 nap", tenDays: "10 nap", enterExactDates: "Vagy adja meg az utazás pontos időtartamát napokban", from: "Tól", until: "Ig", youCanChoose: "Legfeljebb hozzáadhat", maxDaysAllowedText: "Legfeljebb {totalDays} napot adhat hozzá",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Válasszon szállást", undertitlePickAccommodation: "Kiválaszthat egy vagy több szállástípust. Ha több ember számára foglal, adjon hozzá továbbiakat szükség szerint.", FormSuites: "Lakosztályok", FormRooms: "Szobák", accommodationCategories: "Szálláskategóriák",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kik utaznak?", undertitleWhoTraveling: "Adja meg a felnőttek és gyermekek számát, és jelezze, ha van különleges kérése.", adults14Plus: "Felnőttek (14+)", children: "Gyermekek", specialRequests: "Különleges kérések (pl. étrend, akadálymentesség, háziállatok)", specialRequestsTxt: "Kérjük, jelezze, ha van különleges kérése.",
+    // Step 5: Contact
+    titleContactInformation: "Elérhetőségek", undertitleContactInformationBooking: "Kérjük, adja meg adatait, hogy be tudjuk fejezni a foglalást.", firstName: "Keresztnév", lastName: "Vezetéknév", emailVF: "E-mail", phoneNumber: "Telefonszám",
+    // Step 6: Review
+    titleReview: "Ellenőrzés és küldés", undertitleReviewBooking: "Kérjük, ellenőrizze a foglalási adatokat a küldés előtt.", reviewStayDates: "Tartózkodás dátumai", reviewTravelDates: "Utazás dátumai", reviewDurationOFStay: "Tartózkodás időtartama", reviewAccommodation: "Szállás", reviewTypes: "Típusok", reviewTravelers: "Utasok", reviewAdults: "Felnőttek", reviewSpecialRequests: "Különleges kérések", firstAndLastname: "Keresztnév és vezetéknév",
+    // Final Message
+    thankSubmission: "Köszönjük a kérését!", formSubmitted: "Űrlapját sikeresen elküldte.", formTeam: "Csapatunk hamarosan felveszi Önnel a kapcsolatot.",
+};
