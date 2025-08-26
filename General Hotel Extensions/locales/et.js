ca78a237515399f92b0a5c298695cb7843967134
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:49:40 +0200

    Create et.js

---
+++ General Hotel Extensions/locales/et.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Hind alates: {price} {currency}",
+    capacityText: "Ideaalne: {min}-{max} inimesele",
+    sizeText: "Suurus: {size} m²",
+
+    // General UI
+    next: "Järgmine", back: "Tagasi", edit: "Muuda", submit: "Saada", selected: "Valitud", days: "päeva", day: "päev",
+    // Step 1: Dates
+    titleChooseYourStay: "Vali oma peatumise kuupäevad", undertitleChooseYourStay: "Palun vali oma planeeritud peatumise periood", noDatesSelected: "Kuupäevi pole valitud",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Vali, kui kauaks soovid valitud perioodil jääda", selectDuration: "Vali kestus", exactlyAsSpecified: "Täpselt nagu määratud", threeDays: "3 päeva", fourDays: "4 päeva", fiveDays: "5 päeva", sixDays: "6 päeva", sevenDays: "7 päeva", eightDays: "8 päeva", nineDays: "9 päeva", tenDays: "10 päeva", enterExactDates: "Või sisesta reisi täpne kestus päevades", from: "Alates", until: "Kuni", youCanChoose: "Saad lisada maksimaalselt", maxDaysAllowedText: "Saad lisada kuni {totalDays} päeva",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Vali majutus", undertitlePickAccommodation: "Sa võid valida ühe või mitu majutustüüpi. Kui broneerid mitmele inimesele, lisa vastavalt vajadusele rohkem.", FormSuites: "Sviidid", FormRooms: "Toad", accommodationCategories: "Majutuskategooriad",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kes reisib?", undertitleWhoTraveling: "Sisesta täiskasvanute ja laste arv ning anna teada, kui sul on erisoove.", adults14Plus: "Täiskasvanud (14+)", children: "Lapsed", specialRequests: "Erisoovid (nt toit, ligipääsetavus, lemmikloomad)", specialRequestsTxt: "Palun anna teada, kui sul on erisoove.",
+    // Step 5: Contact
+    titleContactInformation: "Kontaktandmed", undertitleContactInformationBooking: "Palun anna oma andmed, et saaksime sinu broneeringu lõpule viia.", firstName: "Eesnimi", lastName: "Perekonnanimi", emailVF: "E-post", phoneNumber: "Telefoninumber",
+    // Step 6: Review
+    titleReview: "Kontrolli ja saada", undertitleReviewBooking: "Palun kontrolli oma broneeringu andmeid enne saatmist.", reviewStayDates: "Peatumise kuupäevad", reviewTravelDates: "Reisi kuupäevad", reviewDurationOFStay: "Peatumise kestus", reviewAccommodation: "Majutus", reviewTypes: "Tüübid", reviewTravelers: "Reisijad", reviewAdults: "Täiskasvanud", reviewSpecialRequests: "Erisoovid", firstAndLastname: "Ees- ja perekonnanimi",
+    // Final Message
+    thankSubmission: "Täname sinu päringu eest!", formSubmitted: "Sinu vorm on edukalt saadetud.", formTeam: "Meie meeskond võtab sinuga peagi ühendust.",
+};
