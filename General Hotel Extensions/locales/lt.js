53570a7551620d58d2ff3d77dddc964c214a1dbd
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:50:34 +0200

    Create lt.js

---
+++ General Hotel Extensions/locales/lt.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Kaina nuo: {price} {currency}",
+    capacityText: "Idealu: {min}-{max} žmonėms",
+    sizeText: "Dydis: {size} m²",
+
+    // General UI
+    next: "Toliau", back: "Atgal", edit: "Redaguoti", submit: "Siųsti", selected: "Pasirinkta", days: "dienos", day: "diena",
+    // Step 1: Dates
+    titleChooseYourStay: "Pasirinkite savo viešnagės datas", undertitleChooseYourStay: "Pasirinkite planuojamos viešnagės laikotarpį", noDatesSelected: "Datos nepasirinktos",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Pasirinkite, kiek laiko norite pasilikti pasirinktu laikotarpiu", selectDuration: "Pasirinkite trukmę", exactlyAsSpecified: "Tiksliai kaip nurodyta", threeDays: "3 dienos", fourDays: "4 dienos", fiveDays: "5 dienos", sixDays: "6 dienos", sevenDays: "7 dienos", eightDays: "8 dienos", nineDays: "9 dienos", tenDays: "10 dienų", enterExactDates: "Arba įveskite tikslią kelionės trukmę dienomis", from: "Nuo", until: "Iki", youCanChoose: "Galite pridėti daugiausia", maxDaysAllowedText: "Galite pridėti daugiausia {totalDays} dienų",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Pasirinkite apgyvendinimą", undertitlePickAccommodation: "Galite pasirinkti vieną ar kelis apgyvendinimo tipus. Jei rezervuojate keliems žmonėms, pridėkite daugiau pagal poreikį.", FormSuites: "Apartamentai", FormRooms: "Kambariai", accommodationCategories: "Apgyvendinimo kategorijos",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kas keliaus?", undertitleWhoTraveling: "Įveskite suaugusiųjų ir vaikų skaičių ir praneškite mums, jei turite ypatingų pageidavimų.", adults14Plus: "Suaugusieji (14+)", children: "Vaikai", specialRequests: "Ypatingi pageidavimai (pvz., mityba, prieinamumas, augintiniai)", specialRequestsTxt: "Prašome pranešti, jei turite ypatingų pageidavimų.",
+    // Step 5: Contact
+    titleContactInformation: "Kontaktinė informacija", undertitleContactInformationBooking: "Prašome pateikti savo duomenis, kad galėtume užbaigti jūsų rezervaciją.", firstName: "Vardas", lastName: "Pavardė", emailVF: "El. paštas", phoneNumber: "Telefono numeris",
+    // Step 6: Review
+    titleReview: "Peržiūrėti ir siųsti", undertitleReviewBooking: "Prašome peržiūrėti savo rezervacijos duomenis prieš siųsdami.", reviewStayDates: "Viešnagės datos", reviewTravelDates: "Kelionės datos", reviewDurationOFStay: "Viešnagės trukmė", reviewAccommodation: "Apgyvendinimas", reviewTypes: "Tipai", reviewTravelers: "Kelionių dalyviai", reviewAdults: "Suaugusieji", reviewSpecialRequests: "Ypatingi pageidavimai", firstAndLastname: "Vardas ir pavardė",
+    // Final Message
+    thankSubmission: "Ačiū už jūsų užklausą!", formSubmitted: "Jūsų forma sėkmingai išsiųsta.", formTeam: "Mūsų komanda netrukus su jumis susisieks.",
+};
