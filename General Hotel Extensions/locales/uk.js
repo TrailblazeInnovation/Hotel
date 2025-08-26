4a32102ef18d413526487b3be2362ae86d08674e
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:39:59 +0200

    Create uk.js

---
+++ General Hotel Extensions/locales/uk.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Ціна від: {price} {currency}",
+    capacityText: "Ідеально для: {min}-{max} осіб",
+    sizeText: "Розмір: {size} м²",
+
+    // General UI
+    next: "Далі", back: "Назад", edit: "Редагувати", submit: "Надіслати", selected: "Вибрано", days: "днів", day: "день",
+    // Step 1: Dates
+    titleChooseYourStay: "Виберіть дати вашого перебування", undertitleChooseYourStay: "Виберіть період вашого запланованого перебування", noDatesSelected: "Дати не вибрані",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Виберіть, як довго ви хочете залишатися у вибраний період", selectDuration: "Виберіть тривалість", exactlyAsSpecified: "Точно як зазначено", threeDays: "3 дні", fourDays: "4 дні", fiveDays: "5 днів", sixDays: "6 днів", sevenDays: "7 днів", eightDays: "8 днів", nineDays: "9 днів", tenDays: "10 днів", enterExactDates: "Або введіть точну тривалість поїздки в днях", from: "З", until: "До", youCanChoose: "Ви можете додати максимум", maxDaysAllowedText: "Ви можете додати максимум {totalDays} днів",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Виберіть житло", undertitlePickAccommodation: "Ви можете вибрати один або кілька типів житла. Якщо ви бронюєте для кількох осіб, додайте більше за потреби.", FormSuites: "Люкс", FormRooms: "Кімнати", accommodationCategories: "Категорії житла",
+    // Step 4: Travelers
+    titleWhoTraveling: "Хто буде подорожувати?", undertitleWhoTraveling: "Вкажіть кількість дорослих і дітей та повідомте нам, якщо у вас є особливі побажання.", adults14Plus: "Дорослі (14+)", children: "Діти", specialRequests: "Особливі побажання (наприклад, харчування, доступність, домашні тварини)", specialRequestsTxt: "Будь ласка, повідомте нам, якщо у вас є особливі побажання.",
+    // Step 5: Contact
+    titleContactInformation: "Контактна інформація", undertitleContactInformationBooking: "Будь ласка, надайте свої дані, щоб ми могли завершити ваше бронювання.", firstName: "Ім’я", lastName: "Прізвище", emailVF: "Електронна пошта", phoneNumber: "Номер телефону",
+    // Step 6: Review
+    titleReview: "Перевірте та надішліть", undertitleReviewBooking: "Будь ласка, перевірте дані вашого бронювання перед відправкою.", reviewStayDates: "Дати перебування", reviewTravelDates: "Дати подорожі", reviewDurationOFStay: "Тривалість перебування", reviewAccommodation: "Житло", reviewTypes: "Типи", reviewTravelers: "Мандрівники", reviewAdults: "Дорослі", reviewSpecialRequests: "Особливі побажання", firstAndLastname: "Ім’я та прізвище",
+    // Final Message
+    thankSubmission: "Дякуємо за вашу заявку!", formSubmitted: "Ваша форма успішно надіслана.", formTeam: "Наша команда зв’яжеться з вами найближчим часом.",
+};
