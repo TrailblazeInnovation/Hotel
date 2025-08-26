9d224637eeb7cc660f0fb5a21f89f211b901566f
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:46:48 +0200

    Create ja.js

---
+++ General Hotel Extensions/locales/ja.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "料金：{price} {currency} から",
+    capacityText: "最適人数: {min}-{max} 人",
+    sizeText: "サイズ: {size} ㎡",
+
+    // General UI
+    next: "次へ", back: "戻る", edit: "編集", submit: "送信", selected: "選択済み", days: "日", day: "日",
+    // Step 1: Dates
+    titleChooseYourStay: "滞在日を選択してください", undertitleChooseYourStay: "ご予定の滞在期間を選択してください", noDatesSelected: "日付が選択されていません",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "選択した期間にどのくらい滞在したいかを選んでください", selectDuration: "期間を選択", exactlyAsSpecified: "指定どおり", threeDays: "3日間", fourDays: "4日間", fiveDays: "5日間", sixDays: "6日間", sevenDays: "7日間", eightDays: "8日間", nineDays: "9日間", tenDays: "10日間", enterExactDates: "または旅行の日数を正確に入力してください", from: "開始", until: "終了", youCanChoose: "最大で追加できます", maxDaysAllowedText: "最大 {totalDays} 日まで追加できます",
+    // Step 3: Accommodation
+    titlePickAccommodation: "宿泊施設を選択", undertitlePickAccommodation: "1つまたは複数の宿泊タイプを選択できます。複数人で予約する場合は、必要に応じて追加してください。", FormSuites: "スイート", FormRooms: "客室", accommodationCategories: "宿泊カテゴリー",
+    // Step 4: Travelers
+    titleWhoTraveling: "誰が旅行しますか？", undertitleWhoTraveling: "大人と子供の人数を入力し、特別なご要望があればお知らせください。", adults14Plus: "大人 (14+)", children: "子供", specialRequests: "特別なご要望（例: 食事制限、バリアフリー、ペット）", specialRequestsTxt: "特別なご要望があればお知らせください。",
+    // Step 5: Contact
+    titleContactInformation: "連絡先情報", undertitleContactInformationBooking: "予約を完了するために、あなたの情報をご提供ください。", firstName: "名", lastName: "姓", emailVF: "メール", phoneNumber: "電話番号",
+    // Step 6: Review
+    titleReview: "確認して送信", undertitleReviewBooking: "送信する前に予約内容をご確認ください。", reviewStayDates: "滞在日", reviewTravelDates: "旅行日", reviewDurationOFStay: "滞在期間", reviewAccommodation: "宿泊施設", reviewTypes: "タイプ", reviewTravelers: "旅行者", reviewAdults: "大人", reviewSpecialRequests: "特別なご要望", firstAndLastname: "氏名",
+    // Final Message
+    thankSubmission: "ご依頼ありがとうございます！", formSubmitted: "フォームは正常に送信されました。", formTeam: "当チームより近日中にご連絡いたします。",
+};
