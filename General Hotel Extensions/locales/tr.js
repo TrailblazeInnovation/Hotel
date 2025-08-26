56b231a1cd7016842b501ddb680c3ee641aea373
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-26 18:39:27 +0200

    Create tr.js

---
+++ General Hotel Extensions/locales/tr.js
@@ -0,0 +1,23 @@
+export const translations = {
+    // Formatting for dynamic room data
+    priceFrom: "Fiyat başlangıç: {price} {currency}",
+    capacityText: "Şunlar için ideal: {min}-{max} kişi",
+    sizeText: "Boyut: {size} m²",
+
+    // General UI
+    next: "İleri", back: "Geri", edit: "Düzenle", submit: "Gönder", selected: "Seçildi", days: "gün", day: "gün",
+    // Step 1: Dates
+    titleChooseYourStay: "Konaklama tarihlerinizi seçin", undertitleChooseYourStay: "Planladığınız konaklama süresini seçin", noDatesSelected: "Tarih seçilmedi",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Seçilen dönemde ne kadar kalmak istediğinizi seçin", selectDuration: "Süre seçin", exactlyAsSpecified: "Belirtildiği gibi", threeDays: "3 gün", fourDays: "4 gün", fiveDays: "5 gün", sixDays: "6 gün", sevenDays: "7 gün", eightDays: "8 gün", nineDays: "9 gün", tenDays: "10 gün", enterExactDates: "Veya seyahat süresini gün olarak girin", from: "Başlangıç", until: "Bitiş", youCanChoose: "En fazla ekleyebilirsiniz", maxDaysAllowedText: "En fazla {totalDays} gün ekleyebilirsiniz",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Konaklama seçin", undertitlePickAccommodation: "Bir veya birden fazla konaklama türü seçebilirsiniz. Birden fazla kişi için rezervasyon yapıyorsanız gerekirse daha fazlasını ekleyin.", FormSuites: "Süitler", FormRooms: "Odalar", accommodationCategories: "Konaklama kategorileri",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kim seyahat edecek?", undertitleWhoTraveling: "Yetişkin ve çocuk sayısını girin ve özel istekleriniz varsa bize bildirin.", adults14Plus: "Yetişkin (14+)", children: "Çocuklar", specialRequests: "Özel istekler (örn. diyet, erişilebilirlik, evcil hayvanlar)", specialRequestsTxt: "Lütfen özel istekleriniz varsa bize bildirin.",
+    // Step 5: Contact
+    titleContactInformation: "İletişim bilgileri", undertitleContactInformationBooking: "Rezervasyonunuzu tamamlayabilmemiz için lütfen bilgilerinizi girin.", firstName: "Ad", lastName: "Soyad", emailVF: "E-posta", phoneNumber: "Telefon numarası",
+    // Step 6: Review
+    titleReview: "Kontrol et ve gönder", undertitleReviewBooking: "Göndermeden önce rezervasyon bilgilerinizi kontrol edin.", reviewStayDates: "Konaklama tarihleri", reviewTravelDates: "Seyahat tarihleri", reviewDurationOFStay: "Konaklama süresi", reviewAccommodation: "Konaklama", reviewTypes: "Türler", reviewTravelers: "Seyahat edenler", reviewAdults: "Yetişkinler", reviewSpecialRequests: "Özel istekler", firstAndLastname: "Ad ve soyad",
+    // Final Message
+    thankSubmission: "Talebiniz için teşekkürler!", formSubmitted: "Formunuz başarıyla gönderildi.", formTeam: "Ekibimiz kısa süre içinde sizinle iletişime geçecektir.",
+};
