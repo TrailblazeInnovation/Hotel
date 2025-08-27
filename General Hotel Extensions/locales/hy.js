828f576f334f932e41871653c5fd869eabce6bba
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:42:28 +0200

    Create hy.js

---
+++ General Hotel Extensions/locales/hy.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Հարմար է՝ {min}-{max} անձի համար",
+    sizeText: "Չափսը՝ {size} մ²",
+
+    // General UI
+    next: "Հաջորդը", back: "Վերադառնալ", edit: "Խմբագրել", submit: "Ուղարկել", selected: "Ընտրված", days: "օրեր", day: "օր",
+    // Step 1: Dates
+    titleChooseYourStay: "Ընտրեք Ձեր մնալու օրերը", undertitleChooseYourStay: "Խնդրում ենք ընտրել նախատեսված մնալու ժամանակահատվածը", noDatesSelected: "Օրեր ընտրված չեն",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Ընտրեք, թե որքան ժամանակ եք ցանկանում մնալ ընտրված ժամանակահատվածում", selectDuration: "Ընտրեք տևողությունը", exactlyAsSpecified: "Ճշգրիտ ինչպես նշված է", threeDays: "3 օր", fourDays: "4 օր", fiveDays: "5 օր", sixDays: "6 օր", sevenDays: "7 օր", eightDays: "8 օր", nineDays: "9 օր", tenDays: "10 օր", enterExactDates: "Կամ մուտքագրեք ճշգրիտ տևողությունը օրերով", from: "Սկսած", until: "Մինչև", youCanChoose: "Կարող եք ավելացնել առավելագույնը", maxDaysAllowedText: "Կարող եք ավելացնել առավելագույնը {totalDays} օր",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Ընտրեք Ձեր կացարանը", undertitlePickAccommodation: "Դուք կարող եք ընտրել մեկ կամ մի քանի տեսակի կացարան։ Եթե ամրագրում եք մի քանի անձի համար, ավելացրեք ըստ անհրաժեշտության։", FormSuites: "Սյուիթներ", FormRooms: "Սենյակներ", accommodationCategories: "Կացարանի կատեգորիաներ",
+    // Step 4: Travelers
+    titleWhoTraveling: "Ովքե՞ր են ճանապարհորդելու:", undertitleWhoTraveling: "Մուտքագրեք մեծահասակների և երեխաների քանակը և մեզ տեղեկացրեք, եթե ունեք հատուկ պահանջներ։", adults14Plus: "Մեծահասակներ (14+)", children: "Երեխաներ", specialRequests: "Հատուկ պահանջներ (օր. սննդակարգ, հասանելիություն, տնային կենդանիներ)", specialRequestsTxt: "Խնդրում ենք տեղեկացրեք մեզ, եթե ունեք հատուկ պահանջներ։",
+    // Step 5: Contact
+    titleContactInformation: "Կոնտակտային տվյալներ", undertitleContactInformationBooking: "Խնդրում ենք տրամադրել Ձեր տվյալները, որպեսզի կարողանանք ավարտել ամրագրումը։", firstName: "Անուն", lastName: "Ազգանուն", emailVF: "Էլ. փոստ", phoneNumber: "Հեռախոսահամար",
+    // Step 6: Review
+    titleReview: "Վերանայել և ուղարկել", undertitleReviewBooking: "Խնդրում ենք ստուգել ամրագրման տվյալները ուղարկելուց առաջ։", reviewStayDates: "Մնալու օրեր", reviewTravelDates: "Ճանապարհորդության օրեր", reviewDurationOFStay: "Մնալու տևողություն", reviewAccommodation: "Կացարան", reviewTypes: "Տեսակներ", reviewTravelers: "Ճանապարհորդներ", reviewAdults: "Մեծահասակներ", reviewSpecialRequests: "Հատուկ պահանջներ", firstAndLastname: "Անուն և ազգանուն",
+    // Final Message
+    thankSubmission: "Շնորհակալություն Ձեր հարցման համար!", formSubmitted: "Ձեր ձևը հաջողությամբ ուղարկվել է։", formTeam: "Մեր թիմը շուտով կկապվի Ձեզ հետ։",
+};
