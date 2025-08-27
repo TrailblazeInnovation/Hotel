6b135fea70197dd6e9d3ecf8dc1ce30bfaec04f6
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:19:44 +0200

    Create qu.js

---
+++ General Hotel Extensions/locales/qu.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Allin kaypaq: {min}-{max} runakunapaq",
+    sizeText: "Sayaykuna: {size} m²",
+
+    // General UI
+    next: "Ñawpaqman", back: "Kutiy", edit: "Allinchay", submit: "Apachiy", selected: "Akllasqa", days: "p'unchaykuna", day: "p'unchay",
+    // Step 1: Dates
+    titleChooseYourStay: "Akllay wasiykita p'unchaykuna", undertitleChooseYourStay: "Ama hina, akllay ñawpaqta qhaway wasiyki p'unchaykuna", noDatesSelected: "Mana p'unchaykuna akllasqachu",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Akllay hayka p'unchayta kachkanki ñawpaqta akllasqa p'unchaykuna", selectDuration: "Akllay p'unchaykuna", exactlyAsSpecified: "Chaymanta rikhurqan hina", threeDays: "3 p'unchaykuna", fourDays: "4 p'unchaykuna", fiveDays: "5 p'unchaykuna", sixDays: "6 p'unchaykuna", sevenDays: "7 p'unchaykuna", eightDays: "8 p'unchaykuna", nineDays: "9 p'unchaykuna", tenDays: "10 p'unchaykuna", enterExactDates: "Hinaspa yaykuy cheqaqta hayka p'unchayta puriykita", from: "Kaymanta", until: "Kaykama", youCanChoose: "Ñuqaykuqa aswan yapayta atiykiku", maxDaysAllowedText: "Ñuqaykuqa aswan {totalDays} p'unchaykunata yapayta atiykiku",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Akllay wasiykita", undertitlePickAccommodation: "Huk hinataq aswan wasikunata akllaykuyki. Aswan runakunapaq reservakuykita ruwanki chayqa aswan yapay.", FormSuites: "Suytukunata", FormRooms: "Wasikuna", accommodationCategories: "Wasikuna kastakunata",
+    // Step 4: Travelers
+    titleWhoTraveling: "Piq ripunki?", undertitleWhoTraveling: "Yaykuy runa hatun kuna qhari wawa, hinallataq willaykuyku ima especial mañakuykuna kachkanki chayta.", adults14Plus: "Hatun runa (14+)", children: "Wawakuna", specialRequests: "Especial mañakuykuna (hinaptin: mikhuna, yaykuna, uywakuna)", specialRequestsTxt: "Ama hina, willaykuyku ima especial mañakuykuna kachkanki chayta.",
+    // Step 5: Contact
+    titleContactInformation: "Rimanakuy willakuna", undertitleContactInformationBooking: "Ama hina, qaway willaykuykita qhawaykiku, chaywan qatiqta qhipariyta atinki.", firstName: "Sutin", lastName: "Ayllunpa sutin", emailVF: "Email", phoneNumber: "Telefonpa yupay",
+    // Step 6: Review
+    titleReview: "Qhaway & Apachiy", undertitleReviewBooking: "Ama hina, qhawaykuy ñawpaqta reservakuy willaykuna apachinaykipaq.", reviewStayDates: "Wasiyki p'unchaykuna", reviewTravelDates: "Puriy p'unchaykuna", reviewDurationOFStay: "Wasiyki hayka p'unchay", reviewAccommodation: "Wasi", reviewTypes: "Kastakuna", reviewTravelers: "Ripukuna", reviewAdults: "Hatun runakuna", reviewSpecialRequests: "Especial mañakuykuna", firstAndLastname: "Sutin hinallataq ayllunpa sutin",
+    // Final Message
+    thankSubmission: "Sulpayki mañakuykita!", formSubmitted: "Formulariokita allinmi apachirqanki.", formTeam: "Ñuqanchikpa huñusqa llamk'apakuqkuna aswan ñawpaqta qhawaykiku.",
+};
