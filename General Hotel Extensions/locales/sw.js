0270d1190335cb9fe7b5d0fc69057bbc0b2e265d
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:36:14 +0200

    Create sw.js

---
+++ General Hotel Extensions/locales/sw.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Inafaa kwa: {min}-{max} watu",
+    sizeText: "Ukubwa: {size} m²",
+
+    // General UI
+    next: "Ifuatayo", back: "Rudi", edit: "Hariri", submit: "Tuma", selected: "Imechaguliwa", days: "siku", day: "siku",
+    // Step 1: Dates
+    titleChooseYourStay: "Chagua tarehe za ukaaji wako", undertitleChooseYourStay: "Tafadhali chagua kipindi cha ukaaji uliopanga", noDatesSelected: "Hakuna tarehe zilizochaguliwa",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Chagua muda wa ukaaji wako katika kipindi kilichochaguliwa", selectDuration: "Chagua muda", exactlyAsSpecified: "Kama ilivyobainishwa", threeDays: "Siku 3", fourDays: "Siku 4", fiveDays: "Siku 5", sixDays: "Siku 6", sevenDays: "Siku 7", eightDays: "Siku 8", nineDays: "Siku 9", tenDays: "Siku 10", enterExactDates: "Au ingiza muda kamili wa safari kwa siku", from: "Kuanzia", until: "Hadi", youCanChoose: "Unaweza kuongeza hadi", maxDaysAllowedText: "Unaweza kuongeza hadi siku {totalDays}",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Chagua malazi yako", undertitlePickAccommodation: "Unaweza kuchagua aina moja au zaidi za malazi. Ukihifadhi kwa watu wengi, ongeza zaidi inapohitajika.", FormSuites: "Suiti", FormRooms: "Vyumba", accommodationCategories: "Aina za malazi",
+    // Step 4: Travelers
+    titleWhoTraveling: "Nani atasafiri?", undertitleWhoTraveling: "Weka idadi ya watu wazima na watoto na tujulishe kama una maombi maalum.", adults14Plus: "Watu wazima (14+)", children: "Watoto", specialRequests: "Maombi maalum (km. chakula, upatikanaji, wanyama vipenzi)", specialRequestsTxt: "Tafadhali tujulishe ikiwa una maombi maalum.",
+    // Step 5: Contact
+    titleContactInformation: "Taarifa za mawasiliano", undertitleContactInformationBooking: "Tafadhali tupe taarifa zako ili tukamilishe uhifadhi wako.", firstName: "Jina la Kwanza", lastName: "Jina la Mwisho", emailVF: "Barua pepe", phoneNumber: "Namba ya simu",
+    // Step 6: Review
+    titleReview: "Kagua na Tuma", undertitleReviewBooking: "Tafadhali kagua taarifa za uhifadhi wako kabla ya kutuma.", reviewStayDates: "Tarehe za ukaaji", reviewTravelDates: "Tarehe za safari", reviewDurationOFStay: "Muda wa ukaaji", reviewAccommodation: "Malazi", reviewTypes: "Aina", reviewTravelers: "Wasafiri", reviewAdults: "Watu wazima", reviewSpecialRequests: "Maombi maalum", firstAndLastname: "Jina la kwanza na la mwisho",
+    // Final Message
+    thankSubmission: "Asante kwa ombi lako!", formSubmitted: "Fomu yako imetumwa kwa mafanikio.", formTeam: "Timu yetu itawasiliana nawe hivi karibuni.",
+};
