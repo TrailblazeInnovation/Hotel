6c55aeb0cb43b9c18aa4bb05f1f8314c192289d9
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:07:11 +0200

    Create uz.js

---
+++ General Hotel Extensions/locales/uz.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Mos: {min}-{max} kishi uchun",
+    sizeText: "Hajmi: {size} m²",
+
+    // General UI
+    next: "Keyingi", back: "Orqaga", edit: "Tahrirlash", submit: "Yuborish", selected: "Tanlangan", days: "kun", day: "kun",
+    // Step 1: Dates
+    titleChooseYourStay: "Turar joy sanalarini tanlang", undertitleChooseYourStay: "Iltimos, rejalashtirilgan turar joy muddatini tanlang", noDatesSelected: "Hech qanday sana tanlanmagan",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Tanlangan davrda qancha qolishni xohlaysiz, tanlang", selectDuration: "Muddati tanlang", exactlyAsSpecified: "Aniq ko‘rsatilganidek", threeDays: "3 kun", fourDays: "4 kun", fiveDays: "5 kun", sixDays: "6 kun", sevenDays: "7 kun", eightDays: "8 kun", nineDays: "9 kun", tenDays: "10 kun", enterExactDates: "Yoki safarning aniq muddatini kunlarda kiriting", from: "Dan", until: "Gacha", youCanChoose: "Siz eng ko‘p qo‘shishingiz mumkin", maxDaysAllowedText: "Siz eng ko‘p {totalDays} kun qo‘shishingiz mumkin",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Turar joyingizni tanlang", undertitlePickAccommodation: "Siz bir yoki bir nechta turdagi turar joyni tanlashingiz mumkin. Bir nechta odam uchun buyurtma qilsangiz, kerak bo‘lganda yana qo‘shing.", FormSuites: "Lyuks", FormRooms: "Xonalar", accommodationCategories: "Turar joy toifalari",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kim sayohat qiladi?", undertitleWhoTraveling: "Iltimos, kattalar va bolalar sonini kiriting va agar maxsus so‘rovingiz bo‘lsa, bizga xabar bering.", adults14Plus: "Kattalar (14+)", children: "Bolalar", specialRequests: "Maxsus so‘rovlar (masalan: dieta, kirish imkoniyati, uy hayvonlari)", specialRequestsTxt: "Agar maxsus so‘rovingiz bo‘lsa, iltimos bizga xabar bering.",
+    // Step 5: Contact
+    titleContactInformation: "Aloqa ma’lumotlari", undertitleContactInformationBooking: "Iltimos, buyurtmani yakunlashimiz uchun ma’lumotlaringizni bering.", firstName: "Ism", lastName: "Familiya", emailVF: "Elektron pochta", phoneNumber: "Telefon raqami",
+    // Step 6: Review
+    titleReview: "Ko‘rib chiqing va Yuboring", undertitleReviewBooking: "Iltimos, yuborishdan oldin buyurtma tafsilotlarini ko‘rib chiqing.", reviewStayDates: "Turar joy sanalari", reviewTravelDates: "Sayohat sanalari", reviewDurationOFStay: "Turar joy muddati", reviewAccommodation: "Turar joy", reviewTypes: "Turlari", reviewTravelers: "Sayohatchilar", reviewAdults: "Kattalar", reviewSpecialRequests: "Maxsus so‘rovlar", firstAndLastname: "Ism va Familiya",
+    // Final Message
+    thankSubmission: "So‘rovingiz uchun rahmat!", formSubmitted: "Formangiz muvaffaqiyatli yuborildi.", formTeam: "Bizning jamoamiz tez orada siz bilan bog‘lanadi.",
+};
