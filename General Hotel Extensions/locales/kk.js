5768c0f8b0409656beb86e53152e3d10b53bcd37
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:41:26 +0200

    Create kk.js

---
+++ General Hotel Extensions/locales/kk.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Идеалды: {min}-{max} адамға",
+    sizeText: "Өлшемі: {size} м²",
+
+    // General UI
+    next: "Келесі", back: "Артқа", edit: "Өңдеу", submit: "Жіберу", selected: "Таңдалды", days: "күн", day: "күн",
+    // Step 1: Dates
+    titleChooseYourStay: "Өзіңіздің тұратын күндерді таңдаңыз", undertitleChooseYourStay: "Жоспарланған тұру мерзімін таңдаңыз", noDatesSelected: "Күндер таңдалмаған",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Таңдалған мерзімде қанша уақыт қалғыңыз келетінін таңдаңыз", selectDuration: "Ұзақтығын таңдаңыз", exactlyAsSpecified: "Дәл көрсетілгендей", threeDays: "3 күн", fourDays: "4 күн", fiveDays: "5 күн", sixDays: "6 күн", sevenDays: "7 күн", eightDays: "8 күн", nineDays: "9 күн", tenDays: "10 күн", enterExactDates: "Немесе сапардың нақты ұзақтығын күндермен енгізіңіз", from: "Басталуы", until: "Аяқталуы", youCanChoose: "Ең көбі қосуға болады", maxDaysAllowedText: "Ең көбі {totalDays} күн қосуға болады",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Тұрғын үйді таңдаңыз", undertitlePickAccommodation: "Бір немесе бірнеше тұрғын үй түрін таңдауға болады. Егер бірнеше адамға тапсырыс берсеңіз, қажетіне қарай тағы қосыңыз.", FormSuites: "Люкс бөлмелер", FormRooms: "Бөлмелер", accommodationCategories: "Тұрғын үй санаттары",
+    // Step 4: Travelers
+    titleWhoTraveling: "Кім сапар шегеді?", undertitleWhoTraveling: "Ересектер мен балалар санын енгізіңіз және арнайы өтініштеріңіз болса бізге хабарлаңыз.", adults14Plus: "Ересектер (14+)", children: "Балалар", specialRequests: "Арнайы өтініштер (мысалы, тамақтану, қолжетімділік, үй жануарлары)", specialRequestsTxt: "Егер арнайы өтініштеріңіз болса, бізге хабарлаңыз.",
+    // Step 5: Contact
+    titleContactInformation: "Байланыс ақпараты", undertitleContactInformationBooking: "Тапсырысыңызды аяқтау үшін өз мәліметтеріңізді беріңіз.", firstName: "Аты", lastName: "Тегі", emailVF: "Электрондық пошта", phoneNumber: "Телефон нөмірі",
+    // Step 6: Review
+    titleReview: "Қарап шығып жіберу", undertitleReviewBooking: "Жібермес бұрын брондау мәліметтерін тексеріңіз.", reviewStayDates: "Тұратын күндер", reviewTravelDates: "Сапар күндері", reviewDurationOFStay: "Тұру ұзақтығы", reviewAccommodation: "Тұрғын үй", reviewTypes: "Түрлері", reviewTravelers: "Саяхатшылар", reviewAdults: "Ересектер", reviewSpecialRequests: "Арнайы өтініштер", firstAndLastname: "Аты және тегі",
+    // Final Message
+    thankSubmission: "Сұранысыңыз үшін рақмет!", formSubmitted: "Формаңыз сәтті жіберілді.", formTeam: "Біздің топ сізбен жақын арада хабарласады.",
+};
