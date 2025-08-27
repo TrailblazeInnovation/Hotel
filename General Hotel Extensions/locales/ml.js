3dd83380e6c94b24e5292cd94a0a530fbd605870
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:59:06 +0200

    Create ml.js

---
+++ General Hotel Extensions/locales/ml.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "ഉചിതം: {min}-{max} പേർക്ക്",
+    sizeText: "വലുപ്പം: {size} m²",
+
+    // General UI
+    next: "അടുത്തത്", back: "തിരികെ", edit: "തിരുത്തുക", submit: "സമർപ്പിക്കുക", selected: "തിരഞ്ഞെടുത്തു", days: "ദിവസങ്ങൾ", day: "ദിവസം",
+    // Step 1: Dates
+    titleChooseYourStay: "താമസത്തിനുള്ള തീയതികൾ തിരഞ്ഞെടുക്കുക", undertitleChooseYourStay: "ദയവായി നിങ്ങളുടെ പദ്ധതിയിട്ടിരിക്കുന്ന താമസകാലയളവ് തിരഞ്ഞെടുക്കുക", noDatesSelected: "തീയതികൾ ഒന്നും തിരഞ്ഞെടുക്കപ്പെട്ടിട്ടില്ല",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "തിരഞ്ഞെടുത്ത കാലയളവിൽ എത്ര ദിവസം താമസിക്കണമെന്ന് തിരഞ്ഞെടുക്കുക", selectDuration: "കാലാവധി തിരഞ്ഞെടുക്കുക", exactlyAsSpecified: "കൃത്യമായി വ്യക്തമാക്കിയതുപോലെ", threeDays: "3 ദിവസം", fourDays: "4 ദിവസം", fiveDays: "5 ദിവസം", sixDays: "6 ദിവസം", sevenDays: "7 ദിവസം", eightDays: "8 ദിവസം", nineDays: "9 ദിവസം", tenDays: "10 ദിവസം", enterExactDates: "അല്ലെങ്കിൽ യാത്രയുടെ കൃത്യമായ ദൈർഘ്യം ദിവസങ്ങളിൽ നൽകുക", from: "മുതൽ", until: "വരെ", youCanChoose: "നിങ്ങൾക്ക് പരമാവധി ചേർക്കാം", maxDaysAllowedText: "നിങ്ങൾക്ക് പരമാവധി {totalDays} ദിവസം ചേർക്കാം",
+    // Step 3: Accommodation
+    titlePickAccommodation: "നിങ്ങളുടെ താമസം തിരഞ്ഞെടുക്കുക", undertitlePickAccommodation: "നിങ്ങൾക്ക് ഒന്ന് അല്ലെങ്കിൽ അതിലധികം തരത്തിലുള്ള താമസം തിരഞ്ഞെടുക്കാം. നിരവധി ആളുകൾക്കായി ബുക്കിംഗ് ചെയ്യുകയാണെങ്കിൽ, ആവശ്യാനുസരണം കൂടുതൽ ചേർക്കുക.", FormSuites: "സ്യൂട്ടുകൾ", FormRooms: "മുറികൾ", accommodationCategories: "താമസ വിഭാഗങ്ങൾ",
+    // Step 4: Travelers
+    titleWhoTraveling: "ആരാണ് യാത്ര ചെയ്യുന്നത്?", undertitleWhoTraveling: "ദയവായി മുതിർന്നവരും കുട്ടികളും എത്ര പേർ ആണെന്ന് നൽകുക, പ്രത്യേക അഭ്യർത്ഥനകളുണ്ടെങ്കിൽ ഞങ്ങളെ അറിയിക്കുക.", adults14Plus: "മുതിർന്നവർ (14+)", children: "കുട്ടികൾ", specialRequests: "പ്രത്യേക അഭ്യർത്ഥനകൾ (ഉദാ: ഭക്ഷണം, ആക്സസിബിലിറ്റി, വളർത്തുമൃഗങ്ങൾ)", specialRequestsTxt: "നിങ്ങൾക്ക് പ്രത്യേക അഭ്യർത്ഥനകളുണ്ടെങ്കിൽ ദയവായി ഞങ്ങളെ അറിയിക്കുക.",
+    // Step 5: Contact
+    titleContactInformation: "ബന്ധപ്പെടാനുള്ള വിവരങ്ങൾ", undertitleContactInformationBooking: "ദയവായി നിങ്ങളുടെ ബുക്കിംഗ് പൂർത്തിയാക്കുന്നതിനായി നിങ്ങളുടെ വിവരങ്ങൾ നൽകുക.", firstName: "പേര്", lastName: "വാഴപ്പേര്", emailVF: "ഇമെയിൽ", phoneNumber: "ഫോൺ നമ്പർ",
+    // Step 6: Review
+    titleReview: "അവലോകനം ചെയ്യുക மற்றும் സമർപ്പിക്കുക", undertitleReviewBooking: "ദയവായി സമർപ്പിക്കുന്നതിന് മുമ്പ് നിങ്ങളുടെ ബുക്കിംഗ് വിശദാംശങ്ങൾ അവലോകനം ചെയ്യുക.", reviewStayDates: "താമസ തീയതികൾ", reviewTravelDates: "യാത്ര തീയതികൾ", reviewDurationOFStay: "താമസ കാലാവധി", reviewAccommodation: "താമസം", reviewTypes: "തരങ്ങൾ", reviewTravelers: "യാത്രക്കാരൻ", reviewAdults: "മുതിർന്നവർ", reviewSpecialRequests: "പ്രത്യേക അഭ്യർത്ഥനകൾ", firstAndLastname: "പേര് এবং വാഴപ്പേര്",
+    // Final Message
+    thankSubmission: "നിങ്ങളുടെ അഭ്യർത്ഥനയ്ക്ക് നന്ദി!", formSubmitted: "നിങ്ങളുടെ ഫോം വിജയകരമായി സമർപ്പിച്ചു.", formTeam: "ഞങ്ങളുടെ ടീം ഉടൻ തന്നെ നിങ്ങളുമായി ബന്ധപ്പെടും.",
+};
