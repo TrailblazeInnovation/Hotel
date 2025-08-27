f3874a18ef906ddf7b7d79c0a2054a89305de134
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:53:50 +0200

    Create af.js

---
+++ General Hotel Extensions/locales/af.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Ideaal vir: {min}-{max} persone",
+    sizeText: "Grootte: {size} m²",
+
+    // General UI
+    next: "Volgende", back: "Terug", edit: "Wysig", submit: "Indien", selected: "Geselekteer", days: "dae", day: "dag",
+    // Step 1: Dates
+    titleChooseYourStay: "Kies jou verblyfdatums", undertitleChooseYourStay: "Kies asseblief die tydperk van jou beplande verblyf", noDatesSelected: "Geen datums gekies nie",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Kies hoe lank jy in die gekose tydperk wil bly", selectDuration: "Kies ’n duur", exactlyAsSpecified: "Presies soos gespesifiseer", threeDays: "3 dae", fourDays: "4 dae", fiveDays: "5 dae", sixDays: "6 dae", sevenDays: "7 dae", eightDays: "8 dae", nineDays: "9 dae", tenDays: "10 dae", enterExactDates: "Of voer die presiese duur van die reis in dae in", from: "Van", until: "Tot", youCanChoose: "Jy kan ’n maksimum van", maxDaysAllowedText: "Jy kan tot {totalDays} dae byvoeg",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Kies jou akkommodasie", undertitlePickAccommodation: "Jy kan een of meer tipes akkommodasie kies. As jy vir meer mense bespreek, voeg nog by soos nodig.", FormSuites: "Suites", FormRooms: "Kamers", accommodationCategories: "Akkommodasie kategorieë",
+    // Step 4: Travelers
+    titleWhoTraveling: "Wie gaan reis?", undertitleWhoTraveling: "Voer die aantal volwassenes en kinders in en laat ons weet as jy enige spesiale versoeke het.", adults14Plus: "Volwassenes (14+)", children: "Kinders", specialRequests: "Spesiale versoeke (bv. dieet, toeganklikheid, troeteldiere)", specialRequestsTxt: "Laat ons asseblief weet as jy enige spesiale versoeke het.",
+    // Step 5: Contact
+    titleContactInformation: "Kontakinligting", undertitleContactInformationBooking: "Verskaf asseblief jou besonderhede sodat ons jou bespreking kan voltooi.", firstName: "Voornaam", lastName: "Van", emailVF: "E-pos", phoneNumber: "Telefoonnommer",
+    // Step 6: Review
+    titleReview: "Hersien en Dien In", undertitleReviewBooking: "Hersien asseblief jou bespreking besonderhede voordat jy indien.", reviewStayDates: "Verblyfdatums", reviewTravelDates: "Reisdatums", reviewDurationOFStay: "Duur van verblyf", reviewAccommodation: "Akkommodasie", reviewTypes: "Tipes", reviewTravelers: "Reisigers", reviewAdults: "Volwassenes", reviewSpecialRequests: "Spesiale versoeke", firstAndLastname: "Voornaam en Van",
+    // Final Message
+    thankSubmission: "Dankie vir jou versoek!", formSubmitted: "Jou vorm is suksesvol ingedien.", formTeam: "Ons span sal jou binnekort kontak.",
+};
