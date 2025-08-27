cd2616dbeaa03c1f878b1ebfcc5ff41922376047
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 14:11:53 +0200

    Update hotel-data.js

--- Hotel Therme Meran/hotel-data.js
+++ Hotel Therme Meran/hotel-data.js
@@ -12,8 +12,7 @@ export const hotelData = {
       "name": { "default": "Vita Suite" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/3d92a810-b199-4492-b49b-09f406eafb58/426/426/crop/wisthaler.com%2D24%2D02%2Dhtm%2Dhw96182%2Dweb.JPG",
       "sizeSqm": 60,
-      "capacity": { "min": 1, "max": 4 },
-      "priceFromPerPerson": 298
+      "capacity": { "min": 1, "max": 4 }
     },
     {
       "id": "suite_02",
@@ -21,8 +20,7 @@ export const hotelData = {
       "name": { "default": "Suite" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/c9c5a713-8d8e-4f21-b7d9-110c82164c9f/426/426/crop/3205%2D316460.webp",
       "sizeSqm": 60,
-      "capacity": { "min": 1, "max": 4 },
-      "priceFromPerPerson": 263
+      "capacity": { "min": 1, "max": 4 }
     },
     {
       "id": "suite_03",
@@ -30,8 +28,7 @@ export const hotelData = {
       "name": { "default": "Calla Suite" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/8bc77ab5-3167-4783-a8ed-7d42b453bebc/426/426/crop/3205%2D316408.webp",
       "sizeSqm": 72,
-      "capacity": { "min": 1, "max": 4 },
-      "priceFromPerPerson": 331
+      "capacity": { "min": 1, "max": 4 }
     },
     {
       "id": "room_01",
@@ -39,8 +36,7 @@ export const hotelData = {
       "name": { "default": "Acqua" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/fd938c17-b58e-46b2-b3d5-f744a5e3a439/426/426/crop/wisthaler.com%2D24%2D03%2Dfisi%2Dhtm%2Dhw98706%2D2.webp",
       "sizeSqm": 30,
-      "capacity": { "min": 1, "max": 3 },
-      "priceFromPerPerson": 252
+      "capacity": { "min": 1, "max": 3 }
     },
     {
       "id": "room_02",
@@ -48,8 +44,7 @@ export const hotelData = {
       "name": { "default": "MeranO" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/49655acb-a119-45d2-9f80-44cacf9c817b/426/426/crop/wisthaler.com%2D24%2D03%2Dfisi%2Dhtm%2Dhw90489.webp",
       "sizeSqm": 30,
-      "capacity": { "min": 1, "max": 3 },
-      "priceFromPerPerson": 233
+      "capacity": { "min": 1, "max": 3 }
     },
     {
       "id": "room_03",
@@ -57,8 +52,7 @@ export const hotelData = {
       "name": { "default": "Loggia" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/55e9bc7c-3df8-4236-bb6f-f247d1db00a1/426/426/crop/01.JPG",
       "sizeSqm": 30,
-      "capacity": { "min": 1, "max": 3 },
-      "priceFromPerPerson": 235
+      "capacity": { "min": 1, "max": 3 }
     },
     {
       "id": "room_04",
@@ -66,8 +60,7 @@ export const hotelData = {
       "name": { "default": "Cedro" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/d6ecc731-5ed8-4775-8754-f145467aecf7/426/426/crop/wisthaler.com%2D24%2D02%2Dhtm%2Dhw96272%2Dweb.JPG",
       "sizeSqm": 28,
-      "capacity": { "min": 1, "max": 2 },
-      "priceFromPerPerson": 214
+      "capacity": { "min": 1, "max": 2 }
     },
     {
       "id": "room_05",
@@ -75,8 +68,7 @@ export const hotelData = {
       "name": { "de": "Doppelzimmer Superior", "default": "Double Room Superior" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/58857828-7f98-4af5-aa9a-c5c1ecc0b270/426/426/crop/3205%2D262053.webp",
       "sizeSqm": 30,
-      "capacity": { "min": 1, "max": 3 },
-      "priceFromPerPerson": 217
+      "capacity": { "min": 1, "max": 3 }
     },
     {
       "id": "room_06",
@@ -84,8 +76,7 @@ export const hotelData = {
       "name": { "de": "Doppelzimmer Standard", "default": "Double Room Standard" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/8d53c197-52f2-4aca-b8bb-b071edc8b22f/426/426/crop/3205%2D347281.webp",
       "sizeSqm": 30,
-      "capacity": { "min": 1, "max": 3 },
-      "priceFromPerPerson": 196
+      "capacity": { "min": 1, "max": 3 }
     },
     {
       "id": "room_07",
@@ -93,8 +84,7 @@ export const hotelData = {
       "name": { "de": "Doppelzimmer Standard Junior", "default": "Double Room Standard Junior" },
       "image": "https://www.hoteltermemerano.it/img/sales-content/2331341d-bcd2-4e2b-acb7-9e44eda7da22/426/426/crop/3205%2D262051.webp",
       "sizeSqm": 28,
-      "capacity": { "min": 1, "max": 2 },
-      "priceFromPerPerson": 182
+      "capacity": { "min": 1, "max": 2 }
     }
   ]
 };
