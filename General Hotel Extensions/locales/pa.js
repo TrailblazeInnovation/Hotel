963305d7bdaec15448b7fefab3ee6e8be554fdec
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:00:46 +0200

    Create pa.js

---
+++ General Hotel Extensions/locales/pa.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "ਉਚਿਤ: {min}-{max} ਵਿਅਕਤੀਆਂ ਲਈ",
+    sizeText: "ਆਕਾਰ: {size} m²",
+
+    // General UI
+    next: "ਅਗਲਾ", back: "ਪਿੱਛੇ", edit: "ਸੋਧੋ", submit: "ਜਮ੍ਹਾਂ ਕਰੋ", selected: "ਚੁਣਿਆ ਗਿਆ", days: "ਦਿਨ", day: "ਦਿਨ",
+    // Step 1: Dates
+    titleChooseYourStay: "ਆਪਣੇ ਰਹਿਣ ਦੀਆਂ ਤਾਰੀਖਾਂ ਚੁਣੋ", undertitleChooseYourStay: "ਕਿਰਪਾ ਕਰਕੇ ਆਪਣੇ ਯੋਜਿਤ ਰਹਿਣ ਦੀ ਮਿਆਦ ਚੁਣੋ", noDatesSelected: "ਕੋਈ ਤਾਰੀਖ ਨਹੀਂ ਚੁਣੀ ਗਈ",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "ਚੁਣੀ ਗਈ ਮਿਆਦ ਵਿੱਚ ਤੁਸੀਂ ਕਿੰਨਾ ਸਮਾਂ ਰਹਿਣਾ ਚਾਹੁੰਦੇ ਹੋ, ਚੁਣੋ", selectDuration: "ਮਿਆਦ ਚੁਣੋ", exactlyAsSpecified: "ਬਿਲਕੁਲ ਜਿਵੇਂ ਦਰਸਾਇਆ ਗਿਆ ਹੈ", threeDays: "3 ਦਿਨ", fourDays: "4 ਦਿਨ", fiveDays: "5 ਦਿਨ", sixDays: "6 ਦਿਨ", sevenDays: "7 ਦਿਨ", eightDays: "8 ਦਿਨ", nineDays: "9 ਦਿਨ", tenDays: "10 ਦਿਨ", enterExactDates: "ਜਾਂ ਯਾਤਰਾ ਦੀ ਸਹੀ ਮਿਆਦ ਦਿਨਾਂ ਵਿੱਚ ਦਰਜ ਕਰੋ", from: "ਤੋਂ", until: "ਤੱਕ", youCanChoose: "ਤੁਸੀਂ ਵੱਧ ਤੋਂ ਵੱਧ ਜੋੜ ਸਕਦੇ ਹੋ", maxDaysAllowedText: "ਤੁਸੀਂ ਵੱਧ ਤੋਂ ਵੱਧ {totalDays} ਦਿਨ ਜੋੜ ਸਕਦੇ ਹੋ",
+    // Step 3: Accommodation
+    titlePickAccommodation: "ਆਪਣਾ ਰਹਿਣ-ਸਹਿਣ ਚੁਣੋ", undertitlePickAccommodation: "ਤੁਸੀਂ ਇੱਕ ਜਾਂ ਵੱਧ ਕਿਸਮਾਂ ਦੀ ਰਿਹਾਇਸ਼ ਚੁਣ ਸਕਦੇ ਹੋ। ਜੇ ਤੁਸੀਂ ਕਈ ਲੋਕਾਂ ਲਈ ਬੁਕਿੰਗ ਕਰ ਰਹੇ ਹੋ, ਤਾਂ ਲੋੜ ਅਨੁਸਾਰ ਹੋਰ ਸ਼ਾਮਲ ਕਰੋ।", FormSuites: "ਸੂਟਸ", FormRooms: "ਕਮਰੇ", accommodationCategories: "ਰਿਹਾਇਸ਼ ਸ਼੍ਰੇਣੀਆਂ",
+    // Step 4: Travelers
+    titleWhoTraveling: "ਕੌਣ ਯਾਤਰਾ ਕਰੇਗਾ?", undertitleWhoTraveling: "ਕਿਰਪਾ ਕਰਕੇ ਵੱਡਿਆਂ ਅਤੇ ਬੱਚਿਆਂ ਦੀ ਗਿਣਤੀ ਦਰਜ ਕਰੋ ਅਤੇ ਜੇ ਤੁਹਾਡੇ ਕੋਲ ਕੋਈ ਖਾਸ ਬੇਨਤੀਆਂ ਹਨ ਤਾਂ ਸਾਨੂੰ ਦੱਸੋ।", adults14Plus: "ਵੱਡੇ (14+)", children: "ਬੱਚੇ", specialRequests: "ਖਾਸ ਬੇਨਤੀਆਂ (ਜਿਵੇਂ ਕਿ ਖੁਰਾਕ, ਪਹੁੰਚ, ਪਾਲਤੂ ਜਾਨਵਰ)", specialRequestsTxt: "ਜੇ ਤੁਹਾਡੇ ਕੋਲ ਕੋਈ ਖਾਸ ਬੇਨਤੀਆਂ ਹਨ ਤਾਂ ਸਾਨੂੰ ਦੱਸੋ।",
+    // Step 5: Contact
+    titleContactInformation: "ਸੰਪਰਕ ਜਾਣਕਾਰੀ", undertitleContactInformationBooking: "ਕਿਰਪਾ ਕਰਕੇ ਆਪਣੀ ਬੁਕਿੰਗ ਪੂਰੀ ਕਰਨ ਲਈ ਆਪਣੀ ਜਾਣਕਾਰੀ ਦਿਓ।", firstName: "ਪਹਿਲਾ ਨਾਮ", lastName: "ਅਖੀਰਲਾ ਨਾਮ", emailVF: "ਈਮੇਲ", phoneNumber: "ਫ਼ੋਨ ਨੰਬਰ",
+    // Step 6: Review
+    titleReview: "ਸਮੀਖਿਆ ਕਰੋ ਅਤੇ ਜਮ੍ਹਾਂ ਕਰੋ", undertitleReviewBooking: "ਜਮ੍ਹਾਂ ਕਰਨ ਤੋਂ ਪਹਿਲਾਂ ਕਿਰਪਾ ਕਰਕੇ ਆਪਣੀ ਬੁਕਿੰਗ ਵੇਰਵੇ ਦੀ ਸਮੀਖਿਆ ਕਰੋ।", reviewStayDates: "ਰਹਿਣ ਦੀਆਂ ਤਾਰੀਖਾਂ", reviewTravelDates: "ਯਾਤਰਾ ਦੀਆਂ ਤਾਰੀਖਾਂ", reviewDurationOFStay: "ਰਹਿਣ ਦੀ ਮਿਆਦ", reviewAccommodation: "ਰਿਹਾਇਸ਼", reviewTypes: "ਕਿਸਮਾਂ", reviewTravelers: "ਯਾਤਰੀ", reviewAdults: "ਵੱਡੇ", reviewSpecialRequests: "ਖਾਸ ਬੇਨਤੀਆਂ", firstAndLastname: "ਪਹਿਲਾ ਨਾਮ ਅਤੇ ਅਖੀਰਲਾ ਨਾਮ",
+    // Final Message
+    thankSubmission: "ਤੁਹਾਡੀ ਬੇਨਤੀ ਲਈ ਧੰਨਵਾਦ!", formSubmitted: "ਤੁਹਾਡਾ ਫਾਰਮ ਸਫਲਤਾਪੂਰਵਕ ਜਮ੍ਹਾਂ ਹੋ ਗਿਆ ਹੈ।", formTeam: "ਸਾਡੀ ਟੀਮ ਜਲਦੀ ਹੀ ਤੁਹਾਡੇ ਨਾਲ ਸੰਪਰਕ ਕਰੇਗੀ।",
+};
