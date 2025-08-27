4a750704454def95c41a8954930498186db03aa8
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:16:11 +0200

    Create gd.js

---
+++ General Hotel Extensions/locales/gd.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Freagarrach airson: {min}-{max} daoine",
+    sizeText: "Meud: {size} m²",
+
+    // General UI
+    next: "Air adhart", back: "Air ais", edit: "Deasaich", submit: "Cuir a-steach", selected: "Air a thaghadh", days: "làithean", day: "latha",
+    // Step 1: Dates
+    titleChooseYourStay: "Tagh na làithean fuirich agad", undertitleChooseYourStay: "Tagh an ùine fuirich a tha thu air a phlanadh", noDatesSelected: "Cha deach làithean a thaghadh",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Tagh dè cho fada ’s a tha thu airson fuireach san ùine a chaidh a thaghadh", selectDuration: "Tagh fad-ùine", exactlyAsSpecified: "Dìreach mar a chaidh a shònrachadh", threeDays: "3 làithean", fourDays: "4 làithean", fiveDays: "5 làithean", sixDays: "6 làithean", sevenDays: "7 làithean", eightDays: "8 làithean", nineDays: "9 làithean", tenDays: "10 làithean", enterExactDates: "No cuir a-steach fad-ùine na turais ann an làithean", from: "Bho", until: "Gu", youCanChoose: "Faodaidh tu suas ri", maxDaysAllowedText: "Faodaidh tu suas ri {totalDays} làithean a chur ris",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Tagh an t-àite-fuirich agad", undertitlePickAccommodation: "Faodaidh tu aon seòrsa àite-fuirich no barrachd a thaghadh. Ma tha thu a’ dèanamh glèidheadh dha barrachd dhaoine, cuir ris barrachd ma tha feum air.", FormSuites: "Suitichean", FormRooms: "Seòmraichean", accommodationCategories: "Roinnean àite-fuirich",
+    // Step 4: Travelers
+    titleWhoTraveling: "Cò bhios a’ siubhal?", undertitleWhoTraveling: "Cuir a-steach àireamh nan inbheach is clann agus innis dhuinn ma tha iarrtasan sònraichte agad.", adults14Plus: "Inbhich (14+)", children: "Clann", specialRequests: "Iarrtasan sònraichte (m.e. biadh, ruigsinneachd, peataichean)", specialRequestsTxt: "Innis dhuinn ma tha iarrtasan sònraichte agad.",
+    // Step 5: Contact
+    titleContactInformation: "Fiosrachadh conaltraidh", undertitleContactInformationBooking: "Thoir seachad d’ fhiosrachadh gus an urrainn dhuinn crìoch a chur air do ghlèidheadh.", firstName: "Ainm", lastName: "Sloinneadh", emailVF: "Post-d", phoneNumber: "Àireamh fòn",
+    // Step 6: Review
+    titleReview: "Ath-bhreithneachadh & Cuir a-steach", undertitleReviewBooking: "Thoir sùil air mion-fhiosrachadh do ghlèidheadh mus cuir thu a-steach e.", reviewStayDates: "Làithean fuirich", reviewTravelDates: "Làithean siubhail", reviewDurationOFStay: "Fad-ùine fuirich", reviewAccommodation: "Àite-fuirich", reviewTypes: "Seòrsaichean", reviewTravelers: "Siubhailich", reviewAdults: "Inbhich", reviewSpecialRequests: "Iarrtasan sònraichte", firstAndLastname: "Ainm agus Sloinneadh",
+    // Final Message
+    thankSubmission: "Tapadh leat airson d’ iarrtas!", formSubmitted: "Chaidh am foirm agad a chur a-steach gu soirbheachail.", formTeam: "Cuiridh an sgioba againn fios thugad a dh’ aithghearr.",
+};
