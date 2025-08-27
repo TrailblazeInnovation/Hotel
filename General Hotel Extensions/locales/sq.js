0eccab2ba6d464e8b6cd41e32522be1e23acf05e
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:39:43 +0200

    Create sq.js

---
+++ General Hotel Extensions/locales/sq.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Ideale për: {min}-{max} persona",
+    sizeText: "Madhësia: {size} m²",
+
+    // General UI
+    next: "Tjetër", back: "Mbrapa", edit: "Modifiko", submit: "Dërgo", selected: "Zgjedhur", days: "ditë", day: "ditë",
+    // Step 1: Dates
+    titleChooseYourStay: "Zgjidhni datat e qëndrimit tuaj", undertitleChooseYourStay: "Ju lutemi zgjidhni periudhën e qëndrimit të planifikuar", noDatesSelected: "Asnjë datë e zgjedhur",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Zgjidhni sa gjatë dëshironi të qëndroni në periudhën e zgjedhur", selectDuration: "Zgjidhni kohëzgjatjen", exactlyAsSpecified: "Saktësisht siç u përcaktua", threeDays: "3 ditë", fourDays: "4 ditë", fiveDays: "5 ditë", sixDays: "6 ditë", sevenDays: "7 ditë", eightDays: "8 ditë", nineDays: "9 ditë", tenDays: "10 ditë", enterExactDates: "Ose vendosni kohëzgjatjen e saktë të udhëtimit në ditë", from: "Nga", until: "Deri", youCanChoose: "Mund të shtoni maksimumi", maxDaysAllowedText: "Mund të shtoni deri në {totalDays} ditë",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Zgjidhni akomodimin tuaj", undertitlePickAccommodation: "Mund të zgjidhni një ose më shumë lloje akomodimi. Nëse rezervoni për disa persona, shtoni më shumë sipas nevojës.", FormSuites: "Suita", FormRooms: "Dhoma", accommodationCategories: "Kategoritë e akomodimit",
+    // Step 4: Travelers
+    titleWhoTraveling: "Kush do të udhëtojë?", undertitleWhoTraveling: "Shkruani numrin e të rriturve dhe fëmijëve dhe na tregoni nëse keni kërkesa të veçanta.", adults14Plus: "Të rritur (14+)", children: "Fëmijë", specialRequests: "Kërkesa të veçanta (p.sh. dietë, aksesueshmëri, kafshë shtëpiake)", specialRequestsTxt: "Ju lutemi na tregoni nëse keni kërkesa të veçanta.",
+    // Step 5: Contact
+    titleContactInformation: "Informacion Kontakti", undertitleContactInformationBooking: "Ju lutemi jepni të dhënat tuaja që të mund të përfundojmë rezervimin tuaj.", firstName: "Emri", lastName: "Mbiemri", emailVF: "Email", phoneNumber: "Numri i Telefonit",
+    // Step 6: Review
+    titleReview: "Rishiko dhe Dërgo", undertitleReviewBooking: "Ju lutemi rishikoni të dhënat e rezervimit tuaj përpara se të dërgoni.", reviewStayDates: "Datat e qëndrimit", reviewTravelDates: "Datat e udhëtimit", reviewDurationOFStay: "Kohëzgjatja e qëndrimit", reviewAccommodation: "Akomodimi", reviewTypes: "Llojet", reviewTravelers: "Udhëtarët", reviewAdults: "Të rritur", reviewSpecialRequests: "Kërkesa të Veçanta", firstAndLastname: "Emri dhe mbiemri",
+    // Final Message
+    thankSubmission: "Faleminderit për kërkesën tuaj!", formSubmitted: "Formulari juaj u dërgua me sukses.", formTeam: "Ekipi ynë do t'ju kontaktojë së shpejti.",
+};
