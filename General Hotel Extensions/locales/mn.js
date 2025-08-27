f2c3b7fa81a8756b095f1fbb956c44269569b987
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:44:36 +0200

    Create mn.js

---
+++ General Hotel Extensions/locales/mn.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Зохимжтой: {min}-{max} хүн",
+    sizeText: "Хэмжээ: {size} м²",
+
+    // General UI
+    next: "Дараах", back: "Буцах", edit: "Засах", submit: "Илгээх", selected: "Сонгосон", days: "өдөр", day: "өдөр",
+    // Step 1: Dates
+    titleChooseYourStay: "Өөрийнхөө байрлах өдрүүдийг сонгоно уу", undertitleChooseYourStay: "Төлөвлөсөн байрлалаа хийх хугацааг сонгоно уу", noDatesSelected: "Өдөр сонгогдоогүй байна",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Сонгосон хугацаанд хэд хоног үлдэхээ сонгоно уу", selectDuration: "Хугацаа сонгох", exactlyAsSpecified: "Тодорхой заасан ёсоор", threeDays: "3 өдөр", fourDays: "4 өдөр", fiveDays: "5 өдөр", sixDays: "6 өдөр", sevenDays: "7 өдөр", eightDays: "8 өдөр", nineDays: "9 өдөр", tenDays: "10 өдөр", enterExactDates: "Эсвэл аяллынхаа яг хугацааг өдрөөр оруулна уу", from: "Эхлэх", until: "Дуусах", youCanChoose: "Та хамгийн ихдээ нэмж болно", maxDaysAllowedText: "Та хамгийн ихдээ {totalDays} өдөр нэмж болно",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Өөрийн байрлалыг сонгох", undertitlePickAccommodation: "Та нэг эсвэл хэд хэдэн төрлийн байрлалыг сонгож болно. Хэрэв олон хүний ​​төлөө захиалга хийвэл шаардлагатай бол илүүг нэмнэ үү.", FormSuites: "Люкс өрөө", FormRooms: "Өрөө", accommodationCategories: "Байрлалын ангилал",
+    // Step 4: Travelers
+    titleWhoTraveling: "Хэн аялж байна вэ?", undertitleWhoTraveling: "Насанд хүрэгчид болон хүүхдүүдийн тоог оруулна уу, хэрэв тусгай хүсэлт байвал бидэнд мэдэгдэнэ үү.", adults14Plus: "Насанд хүрэгчид (14+)", children: "Хүүхдүүд", specialRequests: "Тусгай хүсэлтүүд (жишээ нь: хооллолт, хүртээмж, гэрийн тэжээвэр амьтад)", specialRequestsTxt: "Хэрэв танд тусгай хүсэлт байгаа бол бидэнд мэдэгдэнэ үү.",
+    // Step 5: Contact
+    titleContactInformation: "Холбоо барих мэдээлэл", undertitleContactInformationBooking: "Захиалгаа гүйцээхийн тулд мэдээллээ өгнө үү.", firstName: "Нэр", lastName: "Овог", emailVF: "И-мэйл", phoneNumber: "Утасны дугаар",
+    // Step 6: Review
+    titleReview: "Хянаж илгээх", undertitleReviewBooking: "Илгээхээсээ өмнө захиалгын мэдээллээ шалгана уу.", reviewStayDates: "Байрлах өдрүүд", reviewTravelDates: "Аяллын өдрүүд", reviewDurationOFStay: "Байрлалын үргэлжлэх хугацаа", reviewAccommodation: "Байрлал", reviewTypes: "Төрлүүд", reviewTravelers: "Аялагчид", reviewAdults: "Насанд хүрэгчид", reviewSpecialRequests: "Тусгай хүсэлтүүд", firstAndLastname: "Нэр ба овог",
+    // Final Message
+    thankSubmission: "Таны хүсэлтийн төлөө баярлалаа!", formSubmitted: "Таны маягт амжилттай илгээгдлээ.", formTeam: "Манай баг тун удахгүй тантай холбогдох болно.",
+};
