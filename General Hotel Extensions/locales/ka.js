e31ac99bf82b55973e157b3403cb58d48381a10a
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:35:42 +0200

    Create ka.js

---
+++ General Hotel Extensions/locales/ka.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "იდეალურია: {min}-{max} პირისთვის",
+    sizeText: "ზომა: {size} მ²",
+
+    // General UI
+    next: "შემდეგი", back: "უკან", edit: "რედაქტირება", submit: "გაგზავნა", selected: "არჩეულია", days: "დღეები", day: "დღე",
+    // Step 1: Dates
+    titleChooseYourStay: "აირჩიეთ თქვენი ყოფნის თარიღები", undertitleChooseYourStay: "გთხოვთ, აირჩიოთ თქვენი დაგეგმილი ყოფნის პერიოდი", noDatesSelected: "თარიღები არჩეული არ არის",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "აირჩიეთ რამდენ ხანს გსურთ დარჩენა არჩეულ პერიოდში", selectDuration: "აირჩიეთ ხანგრძლივობა", exactlyAsSpecified: "ზუსტად როგორც მითითებულია", threeDays: "3 დღე", fourDays: "4 დღე", fiveDays: "5 დღე", sixDays: "6 დღე", sevenDays: "7 დღე", eightDays: "8 დღე", nineDays: "9 დღე", tenDays: "10 დღე", enterExactDates: "ან შეიყვანეთ მოგზაურობის ზუსტი ხანგრძლივობა დღეებში", from: "დან", until: "მდე", youCanChoose: "შეგიძლიათ დაამატოთ მაქსიმუმ", maxDaysAllowedText: "შეგიძლიათ დაამატოთ მაქსიმუმ {totalDays} დღე",
+    // Step 3: Accommodation
+    titlePickAccommodation: "აირჩიეთ საცხოვრებელი", undertitlePickAccommodation: "შეგიძლიათ აირჩიოთ ერთი ან რამდენიმე სახის საცხოვრებელი. თუ ერთზე მეტ ადამიანს უჯავშნით, საჭიროებისამებრ დაამატეთ მეტი.", FormSuites: "სუიტები", FormRooms: "ოთახები", accommodationCategories: "საცხოვრებელი კატეგორიები",
+    // Step 4: Travelers
+    titleWhoTraveling: "ვინ იმოგზაურებს?", undertitleWhoTraveling: "შეიყვანეთ მოზრდილებისა და ბავშვების რაოდენობა და გვაცნობეთ, თუ გაქვთ სპეციალური მოთხოვნები.", adults14Plus: "მოზრდილები (14+)", children: "ბავშვები", specialRequests: "სპეციალური მოთხოვნები ( напр. დიეტა, ხელმისაწვდომობა, შინაური ცხოველები)", specialRequestsTxt: "გთხოვთ, გვაცნობოთ, თუ გაქვთ სპეციალური მოთხოვნები.",
+    // Step 5: Contact
+    titleContactInformation: "საკონტაქტო ინფორმაცია", undertitleContactInformationBooking: "გთხოვთ, მოგვაწოდოთ თქვენი ინფორმაცია, რათა შევძლოთ თქვენი ჯავშნის დასრულება.", firstName: "სახელი", lastName: "გვარი", emailVF: "ელ. ფოსტა", phoneNumber: "ტელეფონის ნომერი",
+    // Step 6: Review
+    titleReview: "გადახედვა და გაგზავნა", undertitleReviewBooking: "გთხოვთ, გადაამოწმოთ თქვენი ჯავშნის ინფორმაცია გაგზავნამდე.", reviewStayDates: "ყოფნის თარიღები", reviewTravelDates: "მოგზაურობის თარიღები", reviewDurationOFStay: "ყოფნის ხანგრძლივობა", reviewAccommodation: "საცხოვრებელი", reviewTypes: "ტიპები", reviewTravelers: "მგზავრები", reviewAdults: "მოზრდილები", reviewSpecialRequests: "სპეციალური მოთხოვნები", firstAndLastname: "სახელი და გვარი",
+    // Final Message
+    thankSubmission: "გმადლობთ თქვენს მოთხოვნაზე!", formSubmitted: "თქვენი ფორმა წარმატებით გაიგზავნა.", formTeam: "ჩვენი გუნდი მალე დაგიკავშირდებათ.",
+};
