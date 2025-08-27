7a8ab69aee06bb9e5efd5423d52aba30ee1fdcad
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:43:30 +0200

    Create ur.js

---
+++ General Hotel Extensions/locales/ur.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "موزوں برائے: {min}-{max} افراد",
+    sizeText: "سائز: {size} م²",
+
+    // General UI
+    next: "اگلا", back: "پیچھے", edit: "ترمیم کریں", submit: "جمع کریں", selected: "منتخب شدہ", days: "دن", day: "دن",
+    // Step 1: Dates
+    titleChooseYourStay: "اپنے قیام کی تاریخیں منتخب کریں", undertitleChooseYourStay: "براہ کرم اپنے منصوبہ بند قیام کی مدت منتخب کریں", noDatesSelected: "کوئی تاریخ منتخب نہیں کی گئی",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "منتخب مدت میں آپ کتنا قیام کرنا چاہتے ہیں، منتخب کریں", selectDuration: "مدت منتخب کریں", exactlyAsSpecified: "بالکل جیسا بتایا گیا", threeDays: "3 دن", fourDays: "4 دن", fiveDays: "5 دن", sixDays: "6 دن", sevenDays: "7 دن", eightDays: "8 دن", nineDays: "9 دن", tenDays: "10 دن", enterExactDates: "یا سفر کی درست مدت دنوں میں درج کریں", from: "سے", until: "تک", youCanChoose: "آپ زیادہ سے زیادہ شامل کرسکتے ہیں", maxDaysAllowedText: "آپ زیادہ سے زیادہ {totalDays} دن شامل کرسکتے ہیں",
+    // Step 3: Accommodation
+    titlePickAccommodation: "اپنی رہائش کا انتخاب کریں", undertitlePickAccommodation: "آپ ایک یا ایک سے زیادہ اقسام کی رہائش منتخب کرسکتے ہیں۔ اگر آپ کئی افراد کے لئے بُک کر رہے ہیں تو ضرورت کے مطابق مزید شامل کریں۔", FormSuites: "سوئٹس", FormRooms: "کمرے", accommodationCategories: "رہائش کی اقسام",
+    // Step 4: Travelers
+    titleWhoTraveling: "کون سفر کرے گا؟", undertitleWhoTraveling: "بالغوں اور بچوں کی تعداد درج کریں اور ہمیں بتائیں اگر آپ کی کوئی خاص درخواست ہے۔", adults14Plus: "بالغ (14+)", children: "بچے", specialRequests: "خصوصی درخواستیں (مثال: خوراک، رسائی، پالتو جانور)", specialRequestsTxt: "براہ کرم ہمیں بتائیں اگر آپ کی کوئی خصوصی درخواست ہے۔",
+    // Step 5: Contact
+    titleContactInformation: "رابطہ کی معلومات", undertitleContactInformationBooking: "براہ کرم اپنی معلومات فراہم کریں تاکہ ہم آپ کی بُکنگ مکمل کرسکیں۔", firstName: "نام", lastName: "خاندان کا نام", emailVF: "ای میل", phoneNumber: "فون نمبر",
+    // Step 6: Review
+    titleReview: "جائزہ لیں اور جمع کریں", undertitleReviewBooking: "براہ کرم جمع کرنے سے پہلے اپنی بُکنگ کی تفصیلات چیک کریں۔", reviewStayDates: "قیام کی تاریخیں", reviewTravelDates: "سفر کی تاریخیں", reviewDurationOFStay: "قیام کا دورانیہ", reviewAccommodation: "رہائش", reviewTypes: "اقسام", reviewTravelers: "مسافر", reviewAdults: "بالغ", reviewSpecialRequests: "خصوصی درخواستیں", firstAndLastname: "نام اور خاندان کا نام",
+    // Final Message
+    thankSubmission: "آپ کی درخواست کا شکریہ!", formSubmitted: "آپ کا فارم کامیابی سے جمع ہوگیا ہے۔", formTeam: "ہماری ٹیم جلد ہی آپ سے رابطہ کرے گی۔",
+};
