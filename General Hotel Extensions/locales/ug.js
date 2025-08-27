48537ca1bca2e17bc7761a6b47052b5a20faaf3c
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:06:18 +0200

    Create ug.js

---
+++ General Hotel Extensions/locales/ug.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "ماس كېلىدۇ: {min}-{max} كىشى",
+    sizeText: "چوڭلۇقى: {size} م²",
+
+    // General UI
+    next: "كېيىنكى", back: "قايتىش", edit: "تەھرىرلەش", submit: "تاپشۇرۇش", selected: "تاللانغان", days: "كۈن", day: "كۈن",
+    // Step 1: Dates
+    titleChooseYourStay: "مەھسۇلاتىڭىزنىڭ قالىدىغان كۈنلىرىنى تاللاڭ", undertitleChooseYourStay: "ئالدىن پىلانلانغان قالىدىغان ۋاقتىڭىزنى تاللاڭ", noDatesSelected: "ھېچقانداق كۈن تاللانمىدى",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "تاللانغان ۋاقىتتا قانچە ۋاقتىڭىزنى قالماقچى بولسىڭىز تاللاڭ", selectDuration: "ۋاقىت تاللاڭ", exactlyAsSpecified: "بېكىتىلگەن دىقىقلىق بىلەن", threeDays: "3 كۈن", fourDays: "4 كۈن", fiveDays: "5 كۈن", sixDays: "6 كۈن", sevenDays: "7 كۈن", eightDays: "8 كۈن", nineDays: "9 كۈن", tenDays: "10 كۈن", enterExactDates: "ياكى ساياھەتنىڭ توغرا ۋاقتىنى كۈن بويىچە كىرگۈزۈڭ", from: "دىن", until: "غىچە", youCanChoose: "سىز ئەڭ كۆپ قوشالايسىز", maxDaysAllowedText: "سىز ئەڭ كۆپ {totalDays} كۈن قوشالايسىز",
+    // Step 3: Accommodation
+    titlePickAccommodation: "قالىدىغان يەرنى تاللاڭ", undertitlePickAccommodation: "سىز بىر ياكى بىر قانچە تۈرنى تاللىيالايسىز. كۆپ ئادەملەر ئۈچۈن زاكاس قىلسىڭىز، زۆرۈر بولسا تېخىمۇ قوشۇڭ.", FormSuites: "سۈيىت", FormRooms: "ئۆي", accommodationCategories: "قالىدىغان يەر تۈرلىرى",
+    // Step 4: Travelers
+    titleWhoTraveling: "كىم ساياھەت قىلىدۇ؟", undertitleWhoTraveling: "چوڭلار ۋە بالىلار سانىنى كىرگۈزۈپ، ئالاھىدە تەلەپلىرىڭىز بولسا بىزگە بىلدۈرۈڭ.", adults14Plus: "چوڭلار (14+)", children: "بالىلار", specialRequests: "ئالاھىدە تەلەپلەر (مەسىلەن: يېمەكلىك، قوشۇلۇش، ھايۋانات)", specialRequestsTxt: "ئالاھىدە تەلەپلىرىڭىز بولسا بىزگە بىلدۈرۈڭ.",
+    // Step 5: Contact
+    titleContactInformation: "ئالاقە ئۇچۇرى", undertitleContactInformationBooking: "زاكاسىمنى تاماملاش ئۈچۈن ئۇچۇرلىرىڭىزنى بەرڭ.", firstName: "ئىسمى", lastName: "فامىلىيە", emailVF: "ئېلخەت", phoneNumber: "تېلېفون نومۇرى",
+    // Step 6: Review
+    titleReview: "تەكشۈرۈش ۋە تاپشۇرۇش", undertitleReviewBooking: "تاپشۇرۇشتىن بۇرۇن زاكاس ئۇچۇرلىرىڭىزنى تەكشۈرۈڭ.", reviewStayDates: "قالىدىغان كۈنلەر", reviewTravelDates: "ساياھەت كۈنلىرى", reviewDurationOFStay: "قالىدىغان ۋاقتى", reviewAccommodation: "قالىدىغان يەر", reviewTypes: "تۈرلەر", reviewTravelers: "ساياھەتچىلەر", reviewAdults: "چوڭلار", reviewSpecialRequests: "ئالاھىدە تەلەپلەر", firstAndLastname: "ئىسمى ۋە فامىلىيەسى",
+    // Final Message
+    thankSubmission: "ئىلتىماسىڭىزغا رەھمەت!", formSubmitted: "جەدۋېلىڭىز مۇۋەپپەقىيەتلىك تاپشۇرۇلدى.", formTeam: "بىزنىڭ گۇرۇپپا تېزدىن سىز بىلەن ئالاقىلىشىدۇ.",
+};
