5dfdf8ca2acffad950e2ab56ad4a5cc3f48f69ae
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:55:42 +0200

    Create cy.js

---
+++ General Hotel Extensions/locales/cy.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Yn ddelfrydol ar gyfer: {min}-{max} person",
+    sizeText: "Maint: {size} m²",
+
+    // General UI
+    next: "Nesaf", back: "Nôl", edit: "Golygu", submit: "Cyflwyno", selected: "Dewiswyd", days: "dyddiau", day: "diwrnod",
+    // Step 1: Dates
+    titleChooseYourStay: "Dewiswch ddyddiadau eich arhosiad", undertitleChooseYourStay: "Dewiswch gyfnod eich arhosiad a gynlluniwyd os gwelwch yn dda", noDatesSelected: "Dim dyddiadau wedi’u dewis",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Dewiswch faint o amser rydych am aros yn y cyfnod a ddewiswyd", selectDuration: "Dewiswch hyd", exactlyAsSpecified: "Yn union fel y nodwyd", threeDays: "3 diwrnod", fourDays: "4 diwrnod", fiveDays: "5 diwrnod", sixDays: "6 diwrnod", sevenDays: "7 diwrnod", eightDays: "8 diwrnod", nineDays: "9 diwrnod", tenDays: "10 diwrnod", enterExactDates: "Neu rhowch hyd y daith yn union mewn dyddiau", from: "O", until: "Hyd at", youCanChoose: "Gallwch ychwanegu hyd at uchafswm o", maxDaysAllowedText: "Gallwch ychwanegu hyd at {totalDays} diwrnod",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Dewiswch eich llety", undertitlePickAccommodation: "Gallwch ddewis un neu fwy o fathau o lety. Os ydych yn archebu ar gyfer mwy o bobl, ychwanegwch ragor yn ôl yr angen.", FormSuites: "Suites", FormRooms: "Ystafelloedd", accommodationCategories: "Categorïau llety",
+    // Step 4: Travelers
+    titleWhoTraveling: "Pwy fydd yn teithio?", undertitleWhoTraveling: "Rhowch nifer yr oedolion a’r plant a gadewch i ni wybod os oes gennych geisiadau arbennig.", adults14Plus: "Oedolion (14+)", children: "Plant", specialRequests: "Ceisiadau arbennig (e.e. diet, hygyrchedd, anifeiliaid anwes)", specialRequestsTxt: "Rhowch wybod i ni os oes gennych unrhyw geisiadau arbennig.",
+    // Step 5: Contact
+    titleContactInformation: "Gwybodaeth gyswllt", undertitleContactInformationBooking: "Rhowch eich manylion er mwyn i ni gwblhau eich archeb.", firstName: "Enw Cyntaf", lastName: "Cyfenw", emailVF: "E-bost", phoneNumber: "Rhif ffôn",
+    // Step 6: Review
+    titleReview: "Adolygu a Chyflwyno", undertitleReviewBooking: "Adolygwch fanylion eich archeb cyn cyflwyno.", reviewStayDates: "Dyddiadau arhosiad", reviewTravelDates: "Dyddiadau teithio", reviewDurationOFStay: "Hyd arhosiad", reviewAccommodation: "Llety", reviewTypes: "Mathau", reviewTravelers: "Teithwyr", reviewAdults: "Oedolion", reviewSpecialRequests: "Ceisiadau arbennig", firstAndLastname: "Enw a chyfenw",
+    // Final Message
+    thankSubmission: "Diolch am eich cais!", formSubmitted: "Cyflwynwyd eich ffurflen yn llwyddiannus.", formTeam: "Bydd ein tîm yn cysylltu â chi’n fuan.",
+};
