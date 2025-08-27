b3e3ec70054a8a7b79a8828a83485a85c25c8364
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:54:58 +0200

    Create bn.js

---
+++ General Hotel Extensions/locales/bn.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "উপযুক্ত: {min}-{max} জন",
+    sizeText: "আকার: {size} মি²",
+
+    // General UI
+    next: "পরবর্তী", back: "পেছনে", edit: "সম্পাদনা", submit: "জমা দিন", selected: "নির্বাচিত", days: "দিন", day: "দিন",
+    // Step 1: Dates
+    titleChooseYourStay: "আপনার থাকার তারিখগুলি নির্বাচন করুন", undertitleChooseYourStay: "অনুগ্রহ করে আপনার পরিকল্পিত থাকার সময়কাল নির্বাচন করুন", noDatesSelected: "কোনো তারিখ নির্বাচন করা হয়নি",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "নির্বাচিত সময়ে কতদিন থাকতে চান তা নির্বাচন করুন", selectDuration: "সময়কাল নির্বাচন করুন", exactlyAsSpecified: "যেমন নির্দিষ্ট করা হয়েছে", threeDays: "৩ দিন", fourDays: "৪ দিন", fiveDays: "৫ দিন", sixDays: "৬ দিন", sevenDays: "৭ দিন", eightDays: "৮ দিন", nineDays: "৯ দিন", tenDays: "১০ দিন", enterExactDates: "অথবা ভ্রমণের সঠিক সময়কাল দিনগুলিতে লিখুন", from: "থেকে", until: "পর্যন্ত", youCanChoose: "আপনি সর্বাধিক যোগ করতে পারেন", maxDaysAllowedText: "আপনি সর্বাধিক {totalDays} দিন যোগ করতে পারেন",
+    // Step 3: Accommodation
+    titlePickAccommodation: "আপনার আবাসন নির্বাচন করুন", undertitlePickAccommodation: "আপনি এক বা একাধিক ধরণের আবাসন নির্বাচন করতে পারেন। একাধিক ব্যক্তির জন্য বুকিং করলে প্রয়োজনে আরও যোগ করুন।", FormSuites: "সুইট", FormRooms: "রুম", accommodationCategories: "আবাসনের বিভাগসমূহ",
+    // Step 4: Travelers
+    titleWhoTraveling: "কে ভ্রমণ করবে?", undertitleWhoTraveling: "প্রাপ্তবয়স্ক এবং শিশুদের সংখ্যা লিখুন এবং আপনার যদি বিশেষ অনুরোধ থাকে তবে আমাদের জানান।", adults14Plus: "প্রাপ্তবয়স্ক (১৪+)", children: "শিশু", specialRequests: "বিশেষ অনুরোধ (যেমন: খাদ্য, প্রবেশযোগ্যতা, পোষা প্রাণী)", specialRequestsTxt: "আপনার যদি কোনো বিশেষ অনুরোধ থাকে তবে আমাদের জানান।",
+    // Step 5: Contact
+    titleContactInformation: "যোগাযোগের তথ্য", undertitleContactInformationBooking: "আপনার বুকিং সম্পূর্ণ করার জন্য অনুগ্রহ করে আপনার তথ্য দিন।", firstName: "প্রথম নাম", lastName: "শেষ নাম", emailVF: "ইমেইল", phoneNumber: "ফোন নম্বর",
+    // Step 6: Review
+    titleReview: "পর্যালোচনা করুন এবং জমা দিন", undertitleReviewBooking: "জমা দেওয়ার আগে অনুগ্রহ করে আপনার বুকিংয়ের তথ্য পর্যালোচনা করুন।", reviewStayDates: "থাকার তারিখ", reviewTravelDates: "ভ্রমণের তারিখ", reviewDurationOFStay: "থাকার সময়কাল", reviewAccommodation: "আবাসন", reviewTypes: "ধরন", reviewTravelers: "ভ্রমণকারীরা", reviewAdults: "প্রাপ্তবয়স্ক", reviewSpecialRequests: "বিশেষ অনুরোধ", firstAndLastname: "প্রথম নাম এবং শেষ নাম",
+    // Final Message
+    thankSubmission: "আপনার অনুরোধের জন্য ধন্যবাদ!", formSubmitted: "আপনার ফর্ম সফলভাবে জমা হয়েছে।", formTeam: "আমাদের দল শীঘ্রই আপনার সাথে যোগাযোগ করবে।",
+};
