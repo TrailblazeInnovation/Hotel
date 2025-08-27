b8a2c2db315f7637029a933132aa092f80da7bcd
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:58:18 +0200

    Create lo.js

---
+++ General Hotel Extensions/locales/lo.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "ເໝາະສົມສໍາລັບ: {min}-{max} ຄົນ",
+    sizeText: "ຂະໜາດ: {size} ມ²",
+
+    // General UI
+    next: "ຕໍ່ໄປ", back: "ກັບຄືນ", edit: "ແກ້ໄຂ", submit: "ສົ່ງ", selected: "ເລືອກແລ້ວ", days: "ມື້", day: "ມື້",
+    // Step 1: Dates
+    titleChooseYourStay: "ເລືອກວັນທີການພັກຂອງທ່ານ", undertitleChooseYourStay: "ກະລຸນາເລືອກໄລຍະເວລາການພັກທີ່ວາງແຜນໄວ້", noDatesSelected: "ບໍ່ມີວັນທີຖືກເລືອກ",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "ເລືອກວ່າທ່ານຢາກພັກດົນປານໃດໃນໄລຍະເວລາທີ່ເລືອກ", selectDuration: "ເລືອກໄລຍະເວລາ", exactlyAsSpecified: "ຕາມທີ່ກໍານົດໄວ້", threeDays: "3 ມື້", fourDays: "4 ມື້", fiveDays: "5 ມື້", sixDays: "6 ມື້", sevenDays: "7 ມື້", eightDays: "8 ມື້", nineDays: "9 ມື້", tenDays: "10 ມື້", enterExactDates: "ຫຼືໃສ່ໄລຍະເວລາແທ້ຂອງການເດີນທາງເປັນມື້", from: "ຈາກ", until: "ເຖິງ", youCanChoose: "ທ່ານສາມາດເພີ່ມໄດ້ສູງສຸດ", maxDaysAllowedText: "ທ່ານສາມາດເພີ່ມໄດ້ສູງສຸດ {totalDays} ມື້",
+    // Step 3: Accommodation
+    titlePickAccommodation: "ເລືອກທີ່ພັກຂອງທ່ານ", undertitlePickAccommodation: "ທ່ານສາມາດເລືອກຫນຶ່ງຫຼືຫຼາຍປະເພດຂອງທີ່ພັກ. ຖ້າທ່ານຈອງສໍາລັບຫຼາຍຄົນ, ເພີ່ມເພີ່ມເຕີມຕາມຄວາມຈໍາເປັນ.", FormSuites: "ຊຸດຫ້ອງ", FormRooms: "ຫ້ອງ", accommodationCategories: "ປະເພດທີ່ພັກ",
+    // Step 4: Travelers
+    titleWhoTraveling: "ໃຜຈະເດີນທາງ?", undertitleWhoTraveling: "ໃສ່ຈໍານວນຜູ້ໃຫຍ່ແລະເດັກນ້ອຍ ແລະແຈ້ງໃຫ້ພວກເຮົາຮູ້ຖ້າທ່ານມີຄໍາຮ້ອງຂໍພິເສດ.", adults14Plus: "ຜູ້ໃຫຍ່ (14+)", children: "ເດັກນ້ອຍ", specialRequests: "ຄໍາຮ້ອງຂໍພິເສດ (ຕົວຢ່າງ: ອາຫານ, ການເຂົ້າເຖິງ, ສັດລ້ຽງ)", specialRequestsTxt: "ກະລຸນາແຈ້ງໃຫ້ພວກເຮົາຮູ້ຖ້າທ່ານມີຄໍາຮ້ອງຂໍພິເສດ.",
+    // Step 5: Contact
+    titleContactInformation: "ຂໍ້ມູນຕິດຕໍ່", undertitleContactInformationBooking: "ກະລຸນາໃຫ້ລາຍລະອຽດຂອງທ່ານ ເພື່ອໃຫ້ພວກເຮົາສາມາດສໍາເລັດການຈອງຂອງທ່ານ.", firstName: "ຊື່", lastName: "ນາມສະກຸນ", emailVF: "ອີເມວ", phoneNumber: "ເບີໂທ",
+    // Step 6: Review
+    titleReview: "ກວດທານ ແລະ ສົ່ງ", undertitleReviewBooking: "ກະລຸນາກວດລາຍລະອຽດການຈອງຂອງທ່ານກ່ອນສົ່ງ.", reviewStayDates: "ວັນທີພັກ", reviewTravelDates: "ວັນທີເດີນທາງ", reviewDurationOFStay: "ໄລຍະເວລາພັກ", reviewAccommodation: "ທີ່ພັກ", reviewTypes: "ປະເພດ", reviewTravelers: "ນັກເດີນທາງ", reviewAdults: "ຜູ້ໃຫຍ່", reviewSpecialRequests: "ຄໍາຮ້ອງຂໍພິເສດ", firstAndLastname: "ຊື່ ແລະ ນາມສະກຸນ",
+    // Final Message
+    thankSubmission: "ຂອບໃຈສໍາລັບຄໍາຮ້ອງຂໍຂອງທ່ານ!", formSubmitted: "ແບບຟອມຂອງທ່ານຖືກສົ່ງສໍາເລັດ.", formTeam: "ທີມງານຂອງພວກເຮົາຈະຕິດຕໍ່ທ່ານໃນໄວໆນີ້.",
+};
