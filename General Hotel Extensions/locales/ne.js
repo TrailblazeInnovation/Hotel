141116f546a98ae988b38d86456480c52e01dd64
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:45:18 +0200

    Create ne.js

---
+++ General Hotel Extensions/locales/ne.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "योग्य: {min}-{max} जना",
+    sizeText: "आकार: {size} m²",
+
+    // General UI
+    next: "अर्को", back: "पछाडि", edit: "सम्पादन", submit: "पेश गर्नुहोस्", selected: "चयन गरिएको", days: "दिनहरू", day: "दिन",
+    // Step 1: Dates
+    titleChooseYourStay: "तपाईंको बसाइको मिति छान्नुहोस्", undertitleChooseYourStay: "कृपया योजना गरिएको बसाइको अवधि छान्नुहोस्", noDatesSelected: "कुनै मिति चयन गरिएको छैन",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "चयन गरिएको अवधिमा कति समय बस्ने भनेर छान्नुहोस्", selectDuration: "अवधि छान्नुहोस्", exactlyAsSpecified: "ठ्याक्कै जसरी तोकिएको छ", threeDays: "३ दिन", fourDays: "४ दिन", fiveDays: "५ दिन", sixDays: "६ दिन", sevenDays: "७ दिन", eightDays: "८ दिन", nineDays: "९ दिन", tenDays: "१० दिन", enterExactDates: "वा यात्राको ठ्याक्कै अवधि दिनहरूमा लेख्नुहोस्", from: "देखि", until: "सम्म", youCanChoose: "तपाईंले अधिकतम थप्न सक्नुहुन्छ", maxDaysAllowedText: "तपाईंले अधिकतम {totalDays} दिन थप्न सक्नुहुन्छ",
+    // Step 3: Accommodation
+    titlePickAccommodation: "आवास छान्नुहोस्", undertitlePickAccommodation: "तपाईंले एक वा धेरै प्रकारका आवास छान्न सक्नुहुन्छ। धेरै जनाका लागि बुकिङ गर्ने हो भने आवश्यक परे थप्नुहोस्।", FormSuites: "सुइटहरू", FormRooms: "कोठाहरू", accommodationCategories: "आवासका श्रेणीहरू",
+    // Step 4: Travelers
+    titleWhoTraveling: "को यात्रा गर्दैछ?", undertitleWhoTraveling: "कृपया वयस्क र बच्चाहरूको संख्या लेख्नुहोस् र यदि विशेष अनुरोध छ भने हामीलाई बताउनुहोस्।", adults14Plus: "वयस्क (१४+)", children: "बच्चाहरू", specialRequests: "विशेष अनुरोधहरू (जस्तै: खाना, पहुँच, पाल्तु जनावर)", specialRequestsTxt: "यदि तपाईंको कुनै विशेष अनुरोध छ भने कृपया हामीलाई बताउनुहोस्।",
+    // Step 5: Contact
+    titleContactInformation: "सम्पर्क जानकारी", undertitleContactInformationBooking: "कृपया तपाईंको विवरण दिनुहोस् ताकि हामी तपाईंको बुकिङ पूरा गर्न सकौं।", firstName: "पहिलो नाम", lastName: "थर", emailVF: "इमेल", phoneNumber: "फोन नम्बर",
+    // Step 6: Review
+    titleReview: "समीक्षा गर्नुहोस् र पेश गर्नुहोस्", undertitleReviewBooking: "कृपया पेश गर्नु अघि तपाईंको बुकिङ विवरण जाँच गर्नुहोस्।", reviewStayDates: "बसाइको मितिहरू", reviewTravelDates: "यात्राको मितिहरू", reviewDurationOFStay: "बसाइको अवधि", reviewAccommodation: "आवास", reviewTypes: "प्रकारहरू", reviewTravelers: "यात्रीहरू", reviewAdults: "वयस्क", reviewSpecialRequests: "विशेष अनुरोधहरू", firstAndLastname: "नाम र थर",
+    // Final Message
+    thankSubmission: "तपाईंको अनुरोधको लागि धन्यवाद!", formSubmitted: "तपाईंको फारम सफलतापूर्वक पठाइयो।", formTeam: "हाम्रो टोली चाँडै नै तपाईंलाई सम्पर्क गर्नेछ।",
+};
