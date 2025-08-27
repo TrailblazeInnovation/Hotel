fafbc8d0c652bd5b64db5a836a1c63d6ce11ea8f
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:18:52 +0200

    Create mi.js

---
+++ General Hotel Extensions/locales/mi.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Pai mō: {min}-{max} tāngata",
+    sizeText: "Rahi: {size} m²",
+
+    // General UI
+    next: "Panuku", back: "Hoki", edit: "Whakatika", submit: "Tuku", selected: "Kua tīpakohia", days: "ngā rā", day: "rā",
+    // Step 1: Dates
+    titleChooseYourStay: "Kōwhiria ngā rā noho", undertitleChooseYourStay: "Tēnā kōwhiria te wā noho kua whakamaheretia e koe", noDatesSelected: "Kāore anō kia tīpakohia he rā",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Kōwhiria te roa e hiahia ana koe ki te noho i roto i te wā kua kōwhiria", selectDuration: "Kōwhiria te roa", exactlyAsSpecified: "Pērā tonu me te whakahau", threeDays: "3 rā", fourDays: "4 rā", fiveDays: "5 rā", sixDays: "6 rā", sevenDays: "7 rā", eightDays: "8 rā", nineDays: "9 rā", tenDays: "10 rā", enterExactDates: "Rānei, whakauruhia te roa tika o te haerenga i roto i ngā rā", from: "Mai i", until: "Tae noa ki", youCanChoose: "Ka taea e koe te tāpiri atu ki te mōrahi o", maxDaysAllowedText: "Ka taea e koe te tāpiri atu ki te mōrahi o ngā rā {totalDays}",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Kōwhiria tō nohoanga", undertitlePickAccommodation: "Ka taea e koe te kōwhiri i tētahi, neke atu rānei, momo nohoanga. Mēnā kei te tono koe mō te tokomaha ake, tāpirihia atu kia rite ki te hiahia.", FormSuites: "Whare rangatira", FormRooms: "Ngā rūma", accommodationCategories: "Ngā kāwai nohoanga",
+    // Step 4: Travelers
+    titleWhoTraveling: "Ko wai ka haere?", undertitleWhoTraveling: "Tāurua te maha o ngā pakeke me ngā tamariki, ā, whakamōhio mai mēnā he tono motuhake āu.", adults14Plus: "Pakeke (14+)", children: "Ngā tamariki", specialRequests: "Ngā tono motuhake (hei tauira: kai, urunga, kararehe)", specialRequestsTxt: "Tēnā whakamōhio mai mēnā he tono motuhake āu.",
+    // Step 5: Contact
+    titleContactInformation: "Ngā pārongo whakapā", undertitleContactInformationBooking: "Tēnā tukuna mai ō pārongo kia taea ai e mātou te whakaoti i tō tono.", firstName: "Ingoa tuatahi", lastName: "Ingoa whānau", emailVF: "Īmēra", phoneNumber: "Tau waea",
+    // Step 6: Review
+    titleReview: "Arotake & Tukuna", undertitleReviewBooking: "Tēnā arotakengia ngā pārongo o tō tono i mua i te tuku.", reviewStayDates: "Ngā rā noho", reviewTravelDates: "Ngā rā haere", reviewDurationOFStay: "Te roa o te noho", reviewAccommodation: "Nohoanga", reviewTypes: "Ngā momo", reviewTravelers: "Ngā haerenga", reviewAdults: "Ngā pakeke", reviewSpecialRequests: "Ngā tono motuhake", firstAndLastname: "Ingoa tuatahi me te Ingoa whānau",
+    // Final Message
+    thankSubmission: "Ngā mihi mō tō tono!", formSubmitted: "Kua tukuna pai tō puka.", formTeam: "Ka whakapā atu tō mātou rōpū ki a koe ā tōna wā.",
+};
