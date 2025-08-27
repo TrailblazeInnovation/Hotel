2cd67f3adb19f3c35f3d9bc4bba30473e51194a8
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:15:24 +0200

    Create la.js

---
+++ General Hotel Extensions/locales/la.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Idoneum pro: {min}-{max} personis",
+    sizeText: "Magnitudo: {size} m²",
+
+    // General UI
+    next: "Proximus", back: "Retro", edit: "Recensere", submit: "Submittere", selected: "Selectus", days: "dies", day: "dies",
+    // Step 1: Dates
+    titleChooseYourStay: "Elige dies commorationis tuae", undertitleChooseYourStay: "Quaeso elige tempus commorationis tuae destinatum", noDatesSelected: "Nullae dies selectae sunt",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Elige quamdiu manere velis intra tempus delectum", selectDuration: "Elige durationem", exactlyAsSpecified: "Ita ut definitum est", threeDays: "3 dies", fourDays: "4 dies", fiveDays: "5 dies", sixDays: "6 dies", sevenDays: "7 dies", eightDays: "8 dies", nineDays: "9 dies", tenDays: "10 dies", enterExactDates: "Aut exacte inserere diuturnitatem itineris in diebus", from: "Ab", until: "Ad", youCanChoose: "Potes addere usque ad maximum", maxDaysAllowedText: "Potes addere usque ad {totalDays} dies",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Elige hospitium tuum", undertitlePickAccommodation: "Potes eligere unum vel plura genera hospitii. Si pro pluribus personis reservas, adde plura prout opus est.", FormSuites: "Suite", FormRooms: "Cubicula", accommodationCategories: "Categoriae hospitii",
+    // Step 4: Travelers
+    titleWhoTraveling: "Quis iter faciet?", undertitleWhoTraveling: "Inserere numerum adultorum et puerorum et nobis indica si habes petitiones speciales.", adults14Plus: "Adultes (14+)", children: "Pueri", specialRequests: "Postulationes speciales (ex. cibus, accessibilitas, animalia domestica)", specialRequestsTxt: "Quaeso indica si habes petitiones speciales.",
+    // Step 5: Contact
+    titleContactInformation: "Notitia Contactus", undertitleContactInformationBooking: "Quaeso praebe tua data ut reservationem tuam perficere possimus.", firstName: "Praenomen", lastName: "Nomen", emailVF: "Email", phoneNumber: "Numerus telephonii",
+    // Step 6: Review
+    titleReview: "Recensere et Submittere", undertitleReviewBooking: "Quaeso recensere tua data reservationis antequam submittere.", reviewStayDates: "Dies commorationis", reviewTravelDates: "Dies itineris", reviewDurationOFStay: "Duratio commorationis", reviewAccommodation: "Hospitium", reviewTypes: "Genera", reviewTravelers: "Viatores", reviewAdults: "Adultes", reviewSpecialRequests: "Postulationes speciales", firstAndLastname: "Praenomen et Nomen",
+    // Final Message
+    thankSubmission: "Gratias pro petitione tua!", formSubmitted: "Forma tua feliciter submissa est.", formTeam: "Turma nostra mox te contactabit.",
+};
