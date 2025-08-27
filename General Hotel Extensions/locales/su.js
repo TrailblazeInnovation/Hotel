b4753f03e8f729ea9203cd1dc9ce752f31d8387a
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:05:13 +0200

    Create su.js

---
+++ General Hotel Extensions/locales/su.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Cocog pikeun: {min}-{max} jalma",
+    sizeText: "Ukuran: {size} m²",
+
+    // General UI
+    next: "Teras", back: "Balik", edit: "Édit", submit: "Kirim", selected: "Dipilih", days: "poé", day: "poé",
+    // Step 1: Dates
+    titleChooseYourStay: "Pilih tanggal nginep anjeun", undertitleChooseYourStay: "Mangga pilih periode nginep anu direncanakeun", noDatesSelected: "Teu aya tanggal anu kapilih",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Pilih sabaraha lila rék cicing dina periode anu kapilih", selectDuration: "Pilih durasi", exactlyAsSpecified: "Sakumaha ditangtukeun", threeDays: "3 poé", fourDays: "4 poé", fiveDays: "5 poé", sixDays: "6 poé", sevenDays: "7 poé", eightDays: "8 poé", nineDays: "9 poé", tenDays: "10 poé", enterExactDates: "Atawa lebetkeun durasi perjalanan anu pasti dina poé", from: "Ti", until: "Dugi ka", youCanChoose: "Anjeun tiasa nambihan maksimal", maxDaysAllowedText: "Anjeun tiasa nambihan maksimal {totalDays} poé",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Pilih akomodasi anjeun", undertitlePickAccommodation: "Anjeun tiasa milih hiji atanapi sababaraha jinis akomodasi. Lamun mesen pikeun sababaraha jalma, tambahkeun deui upami diperlukeun.", FormSuites: "Suite", FormRooms: "Kamar", accommodationCategories: "Kategori akomodasi",
+    // Step 4: Travelers
+    titleWhoTraveling: "Saha anu bakal iinditan?", undertitleWhoTraveling: "Lebetkeun jumlah déwasa jeung barudak sarta béjakeun ka kami lamun anjeun gaduh pamundut husus.", adults14Plus: "Dewasa (14+)", children: "Barudak", specialRequests: "Pamundut husus (misalna: kadaharan, aksésibilitas, piaraan)", specialRequestsTxt: "Mangga béjakeun ka kami lamun anjeun gaduh pamundut husus.",
+    // Step 5: Contact
+    titleContactInformation: "Inpo kontak", undertitleContactInformationBooking: "Mangga pasihan detil anjeun supaya urang tiasa ngalengkepan pesenan anjeun.", firstName: "Nami payun", lastName: "Nami tukang", emailVF: "E-mail", phoneNumber: "Nomer telepon",
+    // Step 6: Review
+    titleReview: "Tinjau & Kirim", undertitleReviewBooking: "Mangga tinjau detil pesenan anjeun sateuacan ngirim.", reviewStayDates: "Tanggal nginep", reviewTravelDates: "Tanggal perjalanan", reviewDurationOFStay: "Durasi nginep", reviewAccommodation: "Akomodasi", reviewTypes: "Jinis", reviewTravelers: "Nu iinditan", reviewAdults: "Dewasa", reviewSpecialRequests: "Pamundut husus", firstAndLastname: "Nami payun jeung nami tukang",
+    // Final Message
+    thankSubmission: "Hatur nuhun kana pamundut anjeun!", formSubmitted: "Formulir anjeun hasil dikirim.", formTeam: "Tim kami bakal ngahubungi anjeun enggal.",
+};
