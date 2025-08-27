8962dc0411ec3bae476dfcb888aa6ce9d7b70c71
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:56:12 +0200

    Create gu.js

---
+++ General Hotel Extensions/locales/gu.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "આદર્શ: {min}-{max} લોકો માટે",
+    sizeText: "કદ: {size} m²",
+
+    // General UI
+    next: "આગલું", back: "પાછળ", edit: "ફેરફાર કરો", submit: "સબમિટ કરો", selected: "પસંદ કર્યું", days: "દિવસ", day: "દિવસ",
+    // Step 1: Dates
+    titleChooseYourStay: "તમારા રોકાણની તારીખો પસંદ કરો", undertitleChooseYourStay: "કૃપા કરીને તમારી આયોજન કરેલી રોકાણની અવધિ પસંદ કરો", noDatesSelected: "કોઈ તારીખો પસંદ કરવામાં આવી નથી",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "પસંદ કરેલી અવધિમાં તમે કેટલો સમય રોકાવા માંગો છો તે પસંદ કરો", selectDuration: "અવધિ પસંદ કરો", exactlyAsSpecified: "ચોક્કસ જેમ સ્પષ્ટ કરવામાં આવ્યું છે", threeDays: "3 દિવસ", fourDays: "4 દિવસ", fiveDays: "5 દિવસ", sixDays: "6 દિવસ", sevenDays: "7 દિવસ", eightDays: "8 દિવસ", nineDays: "9 દિવસ", tenDays: "10 દિવસ", enterExactDates: "અથવા મુસાફરીની ચોક્કસ અવધિ દિવસોમાં દાખલ કરો", from: "થી", until: "સુધી", youCanChoose: "તમે વધુમાં વધુ ઉમેરો કરી શકો છો", maxDaysAllowedText: "તમે વધુમાં વધુ {totalDays} દિવસ ઉમેરો કરી શકો છો",
+    // Step 3: Accommodation
+    titlePickAccommodation: "તમારું રહેઠાણ પસંદ કરો", undertitlePickAccommodation: "તમે એક અથવા વધુ પ્રકારની રહેઠાણ પસંદ કરી શકો છો. જો તમે ઘણા લોકો માટે બુકિંગ કરી રહ્યા છો, તો જરૂર મુજબ વધુ ઉમેરો.", FormSuites: "સ્યુટ્સ", FormRooms: "રૂમ્સ", accommodationCategories: "રહેઠાણ શ્રેણીઓ",
+    // Step 4: Travelers
+    titleWhoTraveling: "કોણ મુસાફરી કરશે?", undertitleWhoTraveling: "કૃપા કરીને વયસ્કો અને બાળકોની સંખ્યા દાખલ કરો અને જો તમારી પાસે ખાસ વિનંતીઓ હોય તો અમને જણાવો.", adults14Plus: "વયસ્કો (14+)", children: "બાળકો", specialRequests: "ખાસ વિનંતીઓ (ઉદાહરણ: આહાર, સુલભતા, પાળતુ પ્રાણી)", specialRequestsTxt: "કૃપા કરીને અમને જણાવો જો તમારી પાસે કોઈ ખાસ વિનંતીઓ હોય.",
+    // Step 5: Contact
+    titleContactInformation: "સંપર્ક માહિતી", undertitleContactInformationBooking: "કૃપા કરીને તમારી વિગતો આપો જેથી અમે તમારું બુકિંગ પૂર્ણ કરી શકીએ.", firstName: "પ્રથમ નામ", lastName: "અટક", emailVF: "ઈમેઈલ", phoneNumber: "ફોન નંબર",
+    // Step 6: Review
+    titleReview: "સમીક્ષા કરો અને સબમિટ કરો", undertitleReviewBooking: "કૃપા કરીને સબમિટ કરતા પહેલા તમારા બુકિંગની વિગતોની સમીક્ષા કરો.", reviewStayDates: "રોકાણની તારીખો", reviewTravelDates: "મુસાફરીની તારીખો", reviewDurationOFStay: "રોકાણની અવધિ", reviewAccommodation: "રહેઠાણ", reviewTypes: "પ્રકારો", reviewTravelers: "મુસાફરો", reviewAdults: "વયસ્કો", reviewSpecialRequests: "ખાસ વિનંતીઓ", firstAndLastname: "પ્રથમ નામ અને અટક",
+    // Final Message
+    thankSubmission: "તમારી વિનંતી બદલ આભાર!", formSubmitted: "તમારું ફોર્મ સફળતાપૂર્વક સબમિટ થયું છે.", formTeam: "અમારી ટીમ જલ્દી જ તમને સંપર્ક કરશે.",
+};
