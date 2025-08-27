fcde4d4a1ddf4593e598529c49f01200e05395a5
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:00:06 +0200

    Create mr.js

---
+++ General Hotel Extensions/locales/mr.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "योग्य: {min}-{max} व्यक्तींसाठी",
+    sizeText: "आकार: {size} m²",
+
+    // General UI
+    next: "पुढे", back: "मागे", edit: "संपादित करा", submit: "सबमिट करा", selected: "निवडलेले", days: "दिवस", day: "दिवस",
+    // Step 1: Dates
+    titleChooseYourStay: "तुमच्या राहण्याच्या तारखा निवडा", undertitleChooseYourStay: "कृपया नियोजित राहण्याची कालावधी निवडा", noDatesSelected: "कोणत्याही तारखा निवडलेल्या नाहीत",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "निवडलेल्या कालावधीत तुम्हाला किती काळ राहायचे आहे ते निवडा", selectDuration: "कालावधी निवडा", exactlyAsSpecified: "नेमके जसे निर्दिष्ट केले आहे", threeDays: "३ दिवस", fourDays: "४ दिवस", fiveDays: "५ दिवस", sixDays: "६ दिवस", sevenDays: "७ दिवस", eightDays: "८ दिवस", nineDays: "९ दिवस", tenDays: "१० दिवस", enterExactDates: "किंवा प्रवासाचा नेमका कालावधी दिवसांमध्ये प्रविष्ट करा", from: "पासून", until: "पर्यंत", youCanChoose: "तुम्ही जास्तीत जास्त जोडू शकता", maxDaysAllowedText: "तुम्ही जास्तीत जास्त {totalDays} दिवस जोडू शकता",
+    // Step 3: Accommodation
+    titlePickAccommodation: "तुमचे निवासस्थान निवडा", undertitlePickAccommodation: "तुम्ही एक किंवा अधिक प्रकारचे निवास निवडू शकता. अनेक लोकांसाठी बुक करत असाल, तर आवश्यकतेनुसार अधिक जोडा.", FormSuites: "सुइट्स", FormRooms: "खोल्या", accommodationCategories: "निवासाच्या श्रेणी",
+    // Step 4: Travelers
+    titleWhoTraveling: "कोण प्रवास करणार?", undertitleWhoTraveling: "कृपया प्रौढ आणि मुलांची संख्या प्रविष्ट करा आणि तुमच्या काही विशेष विनंत्या असल्यास आम्हाला कळवा.", adults14Plus: "प्रौढ (१४+)", children: "मुले", specialRequests: "विशेष विनंत्या (उदा. आहार, प्रवेशयोग्यता, पाळीव प्राणी)", specialRequestsTxt: "कृपया तुमच्या काही विशेष विनंत्या असल्यास आम्हाला कळवा.",
+    // Step 5: Contact
+    titleContactInformation: "संपर्क माहिती", undertitleContactInformationBooking: "कृपया तुमचे तपशील द्या जेणेकरून आम्ही तुमची बुकिंग पूर्ण करू शकू.", firstName: "पहिले नाव", lastName: "आडनाव", emailVF: "ईमेल", phoneNumber: "फोन नंबर",
+    // Step 6: Review
+    titleReview: "पुनरावलोकन करा आणि सबमिट करा", undertitleReviewBooking: "कृपया सबमिट करण्यापूर्वी तुमच्या बुकिंग तपशीलांचे पुनरावलोकन करा.", reviewStayDates: "राहण्याच्या तारखा", reviewTravelDates: "प्रवासाच्या तारखा", reviewDurationOFStay: "राहण्याचा कालावधी", reviewAccommodation: "निवासस्थान", reviewTypes: "प्रकार", reviewTravelers: "प्रवासी", reviewAdults: "प्रौढ", reviewSpecialRequests: "विशेष विनंत्या", firstAndLastname: "पहिले नाव आणि आडनाव",
+    // Final Message
+    thankSubmission: "तुमच्या विनंतीबद्दल धन्यवाद!", formSubmitted: "तुमचा फॉर्म यशस्वीरित्या सबमिट केला आहे.", formTeam: "आमची टीम लवकरच तुमच्याशी संपर्क साधेल.",
+};
