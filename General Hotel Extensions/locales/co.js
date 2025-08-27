284e13197a505849800faf8b180fc443cd7e5406
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:17:49 +0200

    Create co.js

---
+++ General Hotel Extensions/locales/co.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Adattu per: {min}-{max} persone",
+    sizeText: "Dimensione: {size} m²",
+
+    // General UI
+    next: "Avanti", back: "Indietro", edit: "Mudificà", submit: "Mandà", selected: "Sceltu", days: "ghjorni", day: "ghjornu",
+    // Step 1: Dates
+    titleChooseYourStay: "Sceglite e date di u vostru sughjornu", undertitleChooseYourStay: "Per piacè sceglite u periodu di u vostru sughjornu pianificatu", noDatesSelected: "Nisuna data scelta",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Sceglite quantu tempu vulete stà durante u periodu sceltu", selectDuration: "Sceglite una durata", exactlyAsSpecified: "Esattamente cum’è specificatu", threeDays: "3 ghjorni", fourDays: "4 ghjorni", fiveDays: "5 ghjorni", sixDays: "6 ghjorni", sevenDays: "7 ghjorni", eightDays: "8 ghjorni", nineDays: "9 ghjorni", tenDays: "10 ghjorni", enterExactDates: "O inserite a durata esatta di u viaghju in ghjorni", from: "Da", until: "À", youCanChoose: "Pudete aghjunghje sin’à un massimu di", maxDaysAllowedText: "Pudete aghjunghje sin’à {totalDays} ghjorni",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Sceglite u vostru alloghju", undertitlePickAccommodation: "Pudete sceglie unu o più tippi d’alloghju. S’è vo prenotate per parechje persone, aghjunghjite d’altri secondu u bisognu.", FormSuites: "Suite", FormRooms: "Camere", accommodationCategories: "Categorie d’alloghju",
+    // Step 4: Travelers
+    titleWhoTraveling: "Quale viaghjerà?", undertitleWhoTraveling: "Mettite u numeru d’adulti è zitelli è diteci s’è vo avete dumande particulari.", adults14Plus: "Adulti (14+)", children: "Zitelli", specialRequests: "Dumande particulari (per esempiu: dieta, accessibilità, animali)", specialRequestsTxt: "Per piacè diteci s’è vo avete dumande particulari.",
+    // Step 5: Contact
+    titleContactInformation: "Infurmazione di cuntattu", undertitleContactInformationBooking: "Per piacè furnite i vostri ditagli per ch’è no pudimu cumplettà a vostra riservazione.", firstName: "Nome", lastName: "Cognome", emailVF: "Email", phoneNumber: "Numeru di telefunu",
+    // Step 6: Review
+    titleReview: "Rivede & Mandà", undertitleReviewBooking: "Per piacè rivedite i ditagli di a vostra riservazione nanzu di mandà.", reviewStayDates: "Date di sughjornu", reviewTravelDates: "Date di viaghju", reviewDurationOFStay: "Durata di sughjornu", reviewAccommodation: "Alloghju", reviewTypes: "Tippi", reviewTravelers: "Viaghjatori", reviewAdults: "Adulti", reviewSpecialRequests: "Dumande particulari", firstAndLastname: "Nome è Cognome",
+    // Final Message
+    thankSubmission: "Grazie per a vostra dumanda!", formSubmitted: "U vostru furmulariu hè statu mandatu cun successu.", formTeam: "U nostru squadra vi cuntatterà prestu.",
+};
