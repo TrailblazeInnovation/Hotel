463801b9642d5415010fcd154556a4cb5a7ba2c1
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:04:35 +0200

    Create si.js

---
+++ General Hotel Extensions/locales/si.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "සුදුසු: {min}-{max} පුද්ගලයින් සඳහා",
+    sizeText: "ප්රමාණය: {size} m²",
+
+    // General UI
+    next: "ඊළඟ", back: "ආපසු", edit: "සංස්කරණය", submit: "ඉදිරිපත් කරන්න", selected: "තෝරාගෙන ඇත", days: "දින", day: "දිනය",
+    // Step 1: Dates
+    titleChooseYourStay: "ඔබගේ නවාතැන් දිනයන් තෝරන්න", undertitleChooseYourStay: "කරුණාකර ඔබගේ සැලසුම්ගත් නවාතැන් කාලය තෝරන්න", noDatesSelected: "දිනයන් තෝරාගෙන නොමැත",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "තෝරාගත් කාලය තුළ ඔබ කොපමණ කාලයක් නවතී සිටිය යුතුද යන්න තෝරන්න", selectDuration: "කාලය තෝරන්න", exactlyAsSpecified: "නියමිත පරිදි", threeDays: "දින 3", fourDays: "දින 4", fiveDays: "දින 5", sixDays: "දින 6", sevenDays: "දින 7", eightDays: "දින 8", nineDays: "දින 9", tenDays: "දින 10", enterExactDates: "නැතිනම් සංචාරය සඳහා නිශ්චිත කාලය දිනවලින් ඇතුළත් කරන්න", from: "සිට", until: "දක්වා", youCanChoose: "ඔබට උපරිම එකතු කළ හැක", maxDaysAllowedText: "ඔබට උපරිම {totalDays} දින එකතු කළ හැක",
+    // Step 3: Accommodation
+    titlePickAccommodation: "ඔබගේ නවාතැන් තෝරන්න", undertitlePickAccommodation: "ඔබට නවාතැන් වර්ග එකක් හෝ කිහිපයක් තෝරාගත හැක. බොහෝ පුද්ගලයන් සඳහා වෙන්කරගන්නේ නම්, අවශ්‍යයනම් තවත් එකතු කරන්න.", FormSuites: "සුයිට්", FormRooms: "කාමර", accommodationCategories: "නවාතැන් කාණ්ඩ",
+    // Step 4: Travelers
+    titleWhoTraveling: "යනේ කවුද?", undertitleWhoTraveling: "කරුණාකර වැඩිහිටියන් සහ දරුවන් ගණන ඇතුළත් කරන්න සහ ඔබට විශේෂ ඉල්ලීම් තිබේනම් අපට කියන්න.", adults14Plus: "වැඩිහිටියන් (14+)", children: "දරුවන්", specialRequests: "විශේෂ ඉල්ලීම් (උදා: ආහාර, ප්රවේශය, සුරක්ෂිත සතුන්)", specialRequestsTxt: "ඔබට විශේෂ ඉල්ලීම් තිබේනම් කරුණාකර අපට කියන්න.",
+    // Step 5: Contact
+    titleContactInformation: "සಂಪර්ක තොරතුරු", undertitleContactInformationBooking: "කරුණාකර ඔබගේ වෙන්කරීම සම්පූර්ණ කිරීමට ඔබගේ විස්තර ලබා දෙන්න.", firstName: "මුල් නම", lastName: "අවසන් නම", emailVF: "ඊමේල්", phoneNumber: "දුරකථන අංකය",
+    // Step 6: Review
+    titleReview: "සමාලෝචනය කර ඉදිරිපත් කරන්න", undertitleReviewBooking: "ඉදිරිපත් කිරීමට පෙර කරුණාකර ඔබගේ වෙන්කරීමේ විස්තර සමාලෝචනය කරන්න.", reviewStayDates: "නවාතැන් දිනයන්", reviewTravelDates: "ගමන් දිනයන්", reviewDurationOFStay: "නවාතැන් කාලය", reviewAccommodation: "නවාතැන්", reviewTypes: "වර්ග", reviewTravelers: "ගමනකරුවන්", reviewAdults: "වැඩිහිටියන්", reviewSpecialRequests: "විශේෂ ඉල්ලීම්", firstAndLastname: "මුල් නම සහ අවසන් නම",
+    // Final Message
+    thankSubmission: "ඔබගේ ඉල්ලීම සඳහා ස්තූතියි!", formSubmitted: "ඔබගේ පෝරමය සාර්ථකව ඉදිරිපත් කරන ලදී.", formTeam: "අපගේ කණ්ඩායම ඉක්මනින් ඔබව සම්බන්ධ කරගනු ඇත.",
+};
