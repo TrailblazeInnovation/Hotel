b2e4e1e8af37af70d27b1cfd2653c55cec48ba28
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:08:38 +0200

    Create yi.js

---
+++ General Hotel Extensions/locales/yi.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "פּאַסיק פֿאַר: {min}-{max} מענטשן",
+    sizeText: "גרייס: {size} מ²",
+
+    // General UI
+    next: "ווייטער", back: "צוריק", edit: "רעדאַגירן", submit: "אַרײַנגעבן", selected: "געקליבן", days: "טעג", day: "טאָג",
+    // Step 1: Dates
+    titleChooseYourStay: "קלייַבן אײַערע בלײַבן־טעג", undertitleChooseYourStay: "ביטע קלײַבן דעם פּלאַנירטן בלײַבן־צײַט", noDatesSelected: "קיין טאָג נישט אויסגעקליבן",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "קלײַבט ווי לאַנג איר ווילט בלײַבן אין דער אויסגעקליבענער צײַט", selectDuration: "קלײַבט אַ געדויער", exactlyAsSpecified: "פּונקט ווי באשטימט", threeDays: "3 טעג", fourDays: "4 טעג", fiveDays: "5 טעג", sixDays: "6 טעג", sevenDays: "7 טעג", eightDays: "8 טעג", nineDays: "9 טעג", tenDays: "10 טעג", enterExactDates: "אָדער אַרײַנגעבן דעם פּינקטלעכן געדויער פֿון דער רייזע אין טעג", from: "פון", until: "ביז", youCanChoose: "איר קענט צולייגן אַ מאַקסימום פֿון", maxDaysAllowedText: "איר קענט צולייגן אַ מאַקסימום פֿון {totalDays} טעג",
+    // Step 3: Accommodation
+    titlePickAccommodation: "קלייַבט אײַער אַקאָמאָדאַציע", undertitlePickAccommodation: "איר קענט אויסקלײַבן איינס אָדער מער טיפּן פֿון אַקאָמאָדאַציע. אויב איר בוכט פֿאַר מער מענטשן, לייגט צו נאָך ווי נויטיק.", FormSuites: "סוויטן", FormRooms: "צימערן", accommodationCategories: "אַקאָמאָדאַציע־קאַטעגאָריעס",
+    // Step 4: Travelers
+    titleWhoTraveling: "ווער וועט רייזן?", undertitleWhoTraveling: "ביטע אַרײַנגעבן די צאָל אַדולטן און קינדער און לאָזן אונדז וויסן אויב איר האָט ספּעציעלע בקשות.", adults14Plus: "אַדולטן (14+)", children: "קינדער", specialRequests: "ספּעציעלע בקשות (צ.ב. דיעט, אַקסעס, פּעץ)", specialRequestsTxt: "ביטע לאָזן אונדז וויסן אויב איר האָט ספּעציעלע בקשות.",
+    // Step 5: Contact
+    titleContactInformation: "קאָנטאַקט־אינפֿאָרמאַציע", undertitleContactInformationBooking: "ביטע געבט אײַערע דעטאַלן כּדי מיר זאָלן קענען פאַרענדיקן אײַער בוקינג.", firstName: "פֿאַרנאָמען", lastName: "נאָכנאָמען", emailVF: "אימעיל", phoneNumber: "טעלעפֿאָן־נומער",
+    // Step 6: Review
+    titleReview: "אַריבערקוקן און אַרײַנגעבן", undertitleReviewBooking: "ביטע אַריבערקוקט די דעטאַלן פֿון אײַער בוקינג איידער אַרײַנגעבן.", reviewStayDates: "בלײַבן־טעג", reviewTravelDates: "רייזע־טעג", reviewDurationOFStay: "בלײַבן־געדויער", reviewAccommodation: "אַקאָמאָדאַציע", reviewTypes: "טיפּן", reviewTravelers: "רייזנדיקע", reviewAdults: "אַדולטן", reviewSpecialRequests: "ספּעציעלע בקשות", firstAndLastname: "נאָמען און נאָכנאָמען",
+    // Final Message
+    thankSubmission: "אַ דאַנק פֿאַר אײַער בקשה!", formSubmitted: "אײַער פֿאָרעם איז מצליח אַרײַנגעגעבן געוואָרן.", formTeam: "אונדזער מאַנשאַפֿט וועט באַלד קאָנטאַקטן אײַך.",
+};
