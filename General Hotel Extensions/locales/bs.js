84e6245829a83b6a55e018789255c6670490bbd8
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:40:28 +0200

    Create bs.js

---
+++ General Hotel Extensions/locales/bs.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Idealno za: {min}-{max} osoba",
+    sizeText: "Veličina: {size} m²",
+
+    // General UI
+    next: "Dalje", back: "Nazad", edit: "Uredi", submit: "Pošalji", selected: "Odabrano", days: "dana", day: "dan",
+    // Step 1: Dates
+    titleChooseYourStay: "Odaberite datume boravka", undertitleChooseYourStay: "Molimo odaberite period planiranog boravka", noDatesSelected: "Nisu odabrani datumi",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Odaberite koliko dugo želite ostati u odabranom periodu", selectDuration: "Odaberite trajanje", exactlyAsSpecified: "Tačno kako je navedeno", threeDays: "3 dana", fourDays: "4 dana", fiveDays: "5 dana", sixDays: "6 dana", sevenDays: "7 dana", eightDays: "8 dana", nineDays: "9 dana", tenDays: "10 dana", enterExactDates: "Ili unesite tačno trajanje putovanja u danima", from: "Od", until: "Do", youCanChoose: "Možete dodati najviše", maxDaysAllowedText: "Možete dodati najviše {totalDays} dana",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Odaberite smještaj", undertitlePickAccommodation: "Možete odabrati jednu ili više vrsta smještaja. Ako rezervišete za više osoba, dodajte dodatne po potrebi.", FormSuites: "Apartmani", FormRooms: "Sobe", accommodationCategories: "Kategorije smještaja",
+    // Step 4: Travelers
+    titleWhoTraveling: "Ko će putovati?", undertitleWhoTraveling: "Unesite broj odraslih i djece i obavijestite nas ako imate posebne zahtjeve.", adults14Plus: "Odrasli (14+)", children: "Djeca", specialRequests: "Posebni zahtjevi (npr. ishrana, pristupačnost, kućni ljubimci)", specialRequestsTxt: "Molimo vas da nas obavijestite ako imate posebne zahtjeve.",
+    // Step 5: Contact
+    titleContactInformation: "Kontakt informacije", undertitleContactInformationBooking: "Molimo unesite svoje podatke kako bismo mogli završiti vašu rezervaciju.", firstName: "Ime", lastName: "Prezime", emailVF: "E-mail", phoneNumber: "Broj telefona",
+    // Step 6: Review
+    titleReview: "Pregledaj i pošalji", undertitleReviewBooking: "Molimo pregledajte detalje svoje rezervacije prije slanja.", reviewStayDates: "Datumi boravka", reviewTravelDates: "Datumi putovanja", reviewDurationOFStay: "Trajanje boravka", reviewAccommodation: "Smještaj", reviewTypes: "Tipovi", reviewTravelers: "Putnici", reviewAdults: "Odrasli", reviewSpecialRequests: "Posebni zahtjevi", firstAndLastname: "Ime i prezime",
+    // Final Message
+    thankSubmission: "Hvala na vašem upitu!", formSubmitted: "Vaša forma je uspješno poslana.", formTeam: "Naš tim će vas uskoro kontaktirati.",
+};
