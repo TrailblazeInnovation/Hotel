df974102606e1ba96719112c35df085a92ba4307
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:09:22 +0200

    Create yo.js

---
+++ General Hotel Extensions/locales/yo.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Dara fun: {min}-{max} ènìyàn",
+    sizeText: "Iwọn: {size} m²",
+
+    // General UI
+    next: "Tẹ̀síwájú", back: "Padà", edit: "Ṣàtúnṣe", submit: "Fọwọ́sí", selected: "Yan", days: "ọjọ́", day: "ọjọ́",
+    // Step 1: Dates
+    titleChooseYourStay: "Yan ọjọ́ ìbùgbé rẹ", undertitleChooseYourStay: "Jọwọ yan àkókò ìbùgbé tí o ti gbero", noDatesSelected: "Ko si ọjọ́ tí a yan",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Yan bí o ṣe fẹ́ pẹ́ tó ní àkókò tí a yàn", selectDuration: "Yan àkókò", exactlyAsSpecified: "Gẹ́gẹ́ bí a ti sọ", threeDays: "ọjọ́ mẹ́ta", fourDays: "ọjọ́ mẹ́rin", fiveDays: "ọjọ́ márùn-ún", sixDays: "ọjọ́ mẹ́fà", sevenDays: "ọjọ́ méje", eightDays: "ọjọ́ mẹ́jọ", nineDays: "ọjọ́ mẹ́sàn-án", tenDays: "ọjọ́ mẹ́wàá", enterExactDates: "Tàbí fi àkókò gangan irin-àjò náà sínú ní ọjọ́", from: "Látọ̀dọ̀", until: "Títí di", youCanChoose: "O lè ṣàfikún tó pọ̀jù", maxDaysAllowedText: "O lè ṣàfikún tó pọ̀jù ọjọ́ {totalDays}",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Yan ibùgbé rẹ", undertitlePickAccommodation: "O lè yan irú ibùgbé kan tàbí ọ̀pọ̀. Tí o bá ń paṣẹ fún ọ̀pọ̀ ènìyàn, ṣàfikún síi gẹ́gẹ́ bí ó ṣe yẹ.", FormSuites: "Àgbègbè alárinká", FormRooms: "Yàrá", accommodationCategories: "Ẹ̀ka ibùgbé",
+    // Step 4: Travelers
+    titleWhoTraveling: "Tálọ́ ń rìn-àjò?", undertitleWhoTraveling: "Jọwọ fi iye àwọn àgbàlagbà àti àwọn ọmọ wá, kí o sì sọ fún wa bí o bá ní ìbéèrè pàtó.", adults14Plus: "Àgbàlagbà (14+)", children: "Àwọn ọmọ", specialRequests: "Ìbéèrè pàtó (àpẹẹrẹ: onjẹ, ààyè, ẹranko ile)", specialRequestsTxt: "Jọwọ sọ fún wa bí o bá ní ìbéèrè pàtó.",
+    // Step 5: Contact
+    titleContactInformation: "Alaye ìbánisọ̀rọ̀", undertitleContactInformationBooking: "Jọwọ pèsè àwọn àlàyé rẹ kí a lè parí ìpamọ́ rẹ.", firstName: "Orúkọ àkọ́kọ́", lastName: "Orúkọ ìdílé", emailVF: "Imeeli", phoneNumber: "Nọ́mbà tẹlifóònù",
+    // Step 6: Review
+    titleReview: "Ṣàyẹ̀wò & Fọwọ́sí", undertitleReviewBooking: "Jọwọ ṣàyẹ̀wò àwọn àlàyé ìpamọ́ rẹ kí o tó fi wọlé.", reviewStayDates: "Ọjọ́ ìbùgbé", reviewTravelDates: "Ọjọ́ irin-àjò", reviewDurationOFStay: "Àkókò ìbùgbé", reviewAccommodation: "Ibùgbé", reviewTypes: "Ìrú", reviewTravelers: "Arìn-àjò", reviewAdults: "Àgbàlagbà", reviewSpecialRequests: "Ìbéèrè pàtó", firstAndLastname: "Orúkọ àkọ́kọ́ àti Orúkọ ìdílé",
+    // Final Message
+    thankSubmission: "O ṣeun fún ìbéèrè rẹ!", formSubmitted: "Fọọmu rẹ ti fi wọlé ní àṣeyọrí.", formTeam: "Ẹgbẹ́ wa yóò kan si ọ́ laipẹ́.",
+};
