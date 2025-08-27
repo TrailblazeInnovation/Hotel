57d122506a21935aa98f089a57c0a9b964580078
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:10:04 +0200

    Create zu.js

---
+++ General Hotel Extensions/locales/zu.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Ilungele: {min}-{max} abantu",
+    sizeText: "Usayizi: {size} m²",
+
+    // General UI
+    next: "Okulandelayo", back: "Emuva", edit: "Hlela", submit: "Thumela", selected: "Kukhethiwe", days: "izinsuku", day: "usuku",
+    // Step 1: Dates
+    titleChooseYourStay: "Khetha izinsuku zokuhlala kwakho", undertitleChooseYourStay: "Sicela ukhethe isikhathi sokuhlala okuhleliwe", noDatesSelected: "Azikho izinsuku ezikhethiwe",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Khetha ukuthi ufuna ukuhlala isikhathi esingakanani phakathi nesikhathi esikhethiwe", selectDuration: "Khetha isikhathi", exactlyAsSpecified: "Ngokunembile njengoba kushiwo", threeDays: "Izinsuku ezi-3", fourDays: "Izinsuku ezi-4", fiveDays: "Izinsuku ezi-5", sixDays: "Izinsuku ezi-6", sevenDays: "Izinsuku ezi-7", eightDays: "Izinsuku ezi-8", nineDays: "Izinsuku ezi-9", tenDays: "Izinsuku ezi-10", enterExactDates: "Noma faka isikhathi esiqondile sohambo ngezinsuku", from: "Kusukela", until: "Kuya ku", youCanChoose: "Ungakwazi ukwengeza okungafika ku", maxDaysAllowedText: "Ungakwazi ukwengeza okungafika ku {totalDays} izinsuku",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Khetha indawo yakho yokuhlala", undertitlePickAccommodation: "Ungakhetha uhlobo olulodwa noma eziningi zezindawo zokuhlala. Uma ubhukhela abantu abaningi, engeza okwengeziwe njengoba kudingeka.", FormSuites: "Ama-Suite", FormRooms: "Amakamelo", accommodationCategories: "Izigaba zezindawo zokuhlala",
+    // Step 4: Travelers
+    titleWhoTraveling: "Ubani ozohamba?", undertitleWhoTraveling: "Sicela ufake inani labantu abadala nezingane bese usazisa uma unazo izicelo ezikhethekile.", adults14Plus: "Abantu abadala (14+)", children: "Izingane", specialRequests: "Izicelo ezikhethekile (isb: ukudla, ukufinyeleleka, izilwane ezifuywayo)", specialRequestsTxt: "Sicela usazise uma unazo izicelo ezikhethekile.",
+    // Step 5: Contact
+    titleContactInformation: "Ulwazi lokuxhumana", undertitleContactInformationBooking: "Sicela unikeze imininingwane yakho ukuze sikwazi ukuqedela ukubhukha kwakho.", firstName: "Igama", lastName: "Isibongo", emailVF: "I-imeyili", phoneNumber: "Inombolo yocingo",
+    // Step 6: Review
+    titleReview: "Buka & Thumela", undertitleReviewBooking: "Sicela ubuke imininingwane yokubhukha kwakho ngaphambi kokuthumela.", reviewStayDates: "Izinsuku zokuhlala", reviewTravelDates: "Izinsuku zokuhamba", reviewDurationOFStay: "Isikhathi sokuhlala", reviewAccommodation: "Indawo yokuhlala", reviewTypes: "Izinhlobo", reviewTravelers: "Abahambi", reviewAdults: "Abantu abadala", reviewSpecialRequests: "Izicelo ezikhethekile", firstAndLastname: "Igama nesibongo",
+    // Final Message
+    thankSubmission: "Siyabonga ngesicelo sakho!", formSubmitted: "Ifomu lakho lithunyelwe ngempumelelo.", formTeam: "Ithimba lethu lizokuthinta maduze.",
+};
