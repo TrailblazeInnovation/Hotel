d8bf54dde7590ab35073c6f84ffa78cad3377c34
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:05:52 +0200

    Create te.js

---
+++ General Hotel Extensions/locales/te.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "అనుకూలం: {min}-{max} మంది",
+    sizeText: "పరిమాణం: {size} m²",
+
+    // General UI
+    next: "తర్వాత", back: "వెనక్కి", edit: "మార్చు", submit: "సమర్పించు", selected: "ఎంచుకున్నారు", days: "రోజులు", day: "రోజు",
+    // Step 1: Dates
+    titleChooseYourStay: "మీ నివాస తేదీలను ఎంచుకోండి", undertitleChooseYourStay: "దయచేసి మీ ప్రణాళిక చేసిన నివాస వ్యవధిని ఎంచుకోండి", noDatesSelected: "తేదీలు ఎంచుకోబడలేదు",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "ఎంచుకున్న కాలంలో మీరు ఎంతకాలం ఉండాలనుకుంటున్నారో ఎంచుకోండి", selectDuration: "వ్యవధిని ఎంచుకోండి", exactlyAsSpecified: "సరిగ్గా పేర్కొన్నట్లుగా", threeDays: "3 రోజులు", fourDays: "4 రోజులు", fiveDays: "5 రోజులు", sixDays: "6 రోజులు", sevenDays: "7 రోజులు", eightDays: "8 రోజులు", nineDays: "9 రోజులు", tenDays: "10 రోజులు", enterExactDates: "లేదా ప్రయాణం యొక్క ఖచ్చితమైన వ్యవధిని రోజుల్లో నమోదు చేయండి", from: "నుండి", until: "వరకు", youCanChoose: "మీరు గరిష్టంగా జోడించవచ్చు", maxDaysAllowedText: "మీరు గరిష్టంగా {totalDays} రోజులు జోడించవచ్చు",
+    // Step 3: Accommodation
+    titlePickAccommodation: "మీ నివాసాన్ని ఎంచుకోండి", undertitlePickAccommodation: "మీరు ఒకటి లేదా అంతకంటే ఎక్కువ రకాల నివాసాన్ని ఎంచుకోవచ్చు. మీరు ఒకదాని కంటే ఎక్కువ మందికి బుక్ చేస్తే, అవసరానికి అనుగుణంగా మరిన్ని జోడించండి.", FormSuites: "స్యూట్లు", FormRooms: "గదులు", accommodationCategories: "నివాస వర్గాలు",
+    // Step 4: Travelers
+    titleWhoTraveling: "ఎవరు ప్రయాణిస్తున్నారు?", undertitleWhoTraveling: "దయచేసి పెద్దలు మరియు పిల్లల సంఖ్యను నమోదు చేయండి మరియు మీకు ఏవైనా ప్రత్యేక అభ్యర్థనలు ఉంటే మాకు తెలియజేయండి.", adults14Plus: "పెద్దవారు (14+)", children: "పిల్లలు", specialRequests: "ప్రత్యేక అభ్యర్థనలు (ఉదా: ఆహారం, ప్రాప్యత, పెంపుడు జంతువులు)", specialRequestsTxt: "మీకు ఏవైనా ప్రత్యేక అభ్యర్థనలు ఉంటే మాకు తెలియజేయండి.",
+    // Step 5: Contact
+    titleContactInformation: "సంప్రదించు సమాచారం", undertitleContactInformationBooking: "దయచేసి మీ బుకింగ్‌ను పూర్తి చేయడానికి మీ వివరాలను ఇవ్వండి.", firstName: "మొదటి పేరు", lastName: "ఇంటి పేరు", emailVF: "ఇమెయిల్", phoneNumber: "ఫోన్ నంబర్",
+    // Step 6: Review
+    titleReview: "సమీక్షించండి మరియు సమర్పించండి", undertitleReviewBooking: "దయచేసి సమర్పించే ముందు మీ బుకింగ్ వివరాలను సమీక్షించండి.", reviewStayDates: "నివాస తేదీలు", reviewTravelDates: "ప్రయాణ తేదీలు", reviewDurationOFStay: "నివాస వ్యవధి", reviewAccommodation: "నివాసం", reviewTypes: "రకాలు", reviewTravelers: "ప్రయాణికులు", reviewAdults: "పెద్దవారు", reviewSpecialRequests: "ప్రత్యేక అభ్యర్థనలు", firstAndLastname: "మొదటి పేరు మరియు ఇంటి పేరు",
+    // Final Message
+    thankSubmission: "మీ అభ్యర్థనకు ధన్యవాదాలు!", formSubmitted: "మీ ఫారమ్ విజయవంతంగా సమర్పించబడింది.", formTeam: "మా బృందం త్వరలోనే మిమ్మల్ని సంప్రదిస్తుంది.",
+};
