e368d0d5db0fc27f2e3e44e293d6a4a40f4a8f87
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:57:02 +0200

    Create km.js

---
+++ General Hotel Extensions/locales/km.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "សាកសមសម្រាប់: {min}-{max} នាក់",
+    sizeText: "ទំហំ: {size} ម៉ែត្រការ៉េ",
+
+    // General UI
+    next: "បន្ទាប់", back: "ថយក្រោយ", edit: "កែសម្រួល", submit: "ដាក់ស្នើ", selected: "បានជ្រើស", days: "ថ្ងៃ", day: "ថ្ងៃ",
+    // Step 1: Dates
+    titleChooseYourStay: "ជ្រើសរើសកាលបរិច្ឆេទស្នាក់នៅរបស់អ្នក", undertitleChooseYourStay: "សូមជ្រើសរើសរយៈពេលស្នាក់នៅដែលបានគ្រោងទុក", noDatesSelected: "គ្មានកាលបរិច្ឆេទត្រូវបានជ្រើសរើស",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "ជ្រើសរើសរយៈពេលដែលអ្នកចង់ស្នាក់នៅក្នុងរយៈពេលដែលបានជ្រើសរើស", selectDuration: "ជ្រើសរើសរយៈពេល", exactlyAsSpecified: "តាមដែលបានបញ្ជាក់", threeDays: "៣ ថ្ងៃ", fourDays: "៤ ថ្ងៃ", fiveDays: "៥ ថ្ងៃ", sixDays: "៦ ថ្ងៃ", sevenDays: "៧ ថ្ងៃ", eightDays: "៨ ថ្ងៃ", nineDays: "៩ ថ្ងៃ", tenDays: "១០ ថ្ងៃ", enterExactDates: "ឬបញ្ចូលរយៈពេលនៃការធ្វើដំណើរជាក់លាក់ជាថ្ងៃ", from: "ចាប់ពី", until: "រហូតដល់", youCanChoose: "អ្នកអាចបន្ថែមបានអតិបរមា", maxDaysAllowedText: "អ្នកអាចបន្ថែមបានអតិបរមា {totalDays} ថ្ងៃ",
+    // Step 3: Accommodation
+    titlePickAccommodation: "ជ្រើសរើសកន្លែងស្នាក់នៅរបស់អ្នក", undertitlePickAccommodation: "អ្នកអាចជ្រើសរើសប្រភេទកន្លែងស្នាក់នៅមួយ ឬច្រើន។ ប្រសិនបើអ្នកកំពុងបម្រើការចំពោះមនុស្សច្រើន សូមបន្ថែមបន្ថែមតាមតម្រូវការ។", FormSuites: "ស៊ុយត៍", FormRooms: "បន្ទប់", accommodationCategories: "ប្រភេទកន្លែងស្នាក់នៅ",
+    // Step 4: Travelers
+    titleWhoTraveling: "នរណានឹងធ្វើដំណើរ?", undertitleWhoTraveling: "បញ្ចូលចំនួនមនុស្សពេញវ័យ និងកុមារ ហើយប្រាប់យើងប្រសិនបើអ្នកមានសំណើពិសេស។", adults14Plus: "មនុស្សពេញវ័យ (14+)", children: "កុមារ", specialRequests: "សំណើពិសេស (ឧ. អាហារ សិទ្ធិចូលប្រើ បរិស្ថានសត្វចិញ្ចឹម)", specialRequestsTxt: "សូមប្រាប់យើងប្រសិនបើអ្នកមានសំណើពិសេស។",
+    // Step 5: Contact
+    titleContactInformation: "ព័ត៌មានទំនាក់ទំនង", undertitleContactInformationBooking: "សូមផ្ដល់ព័ត៌មានរបស់អ្នក ដើម្បីឲ្យយើងអាចបញ្ចប់ការកក់របស់អ្នក។", firstName: "នាមខ្លួន", lastName: "នាមត្រកូល", emailVF: "អ៊ីមែល", phoneNumber: "លេខទូរស័ព្ទ",
+    // Step 6: Review
+    titleReview: "ពិនិត្យ និងដាក់ស្នើ", undertitleReviewBooking: "សូមពិនិត្យព័ត៌មានកក់របស់អ្នកមុនពេលដាក់ស្នើ។", reviewStayDates: "កាលបរិច្ឆេទស្នាក់នៅ", reviewTravelDates: "កាលបរិច្ឆេទធ្វើដំណើរ", reviewDurationOFStay: "រយៈពេលស្នាក់នៅ", reviewAccommodation: "កន្លែងស្នាក់នៅ", reviewTypes: "ប្រភេទ", reviewTravelers: "អ្នកធ្វើដំណើរ", reviewAdults: "មនុស្សពេញវ័យ", reviewSpecialRequests: "សំណើពិសេស", firstAndLastname: "នាមខ្លួន និងនាមត្រកូល",
+    // Final Message
+    thankSubmission: "អរគុណសម្រាប់សំណើរបស់អ្នក!", formSubmitted: "ទម្រង់របស់អ្នកត្រូវបានដាក់ស្នើដោយជោគជ័យ។", formTeam: "ក្រុមរបស់យើងនឹងទាក់ទងអ្នកនៅពេលឆាប់ៗនេះ។",
+};
