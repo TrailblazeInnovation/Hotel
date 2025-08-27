1aa50b87b77b284d449ceb0b106dda8556869bf9
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 15:39:05 +0200

    Create mt.js

---
+++ General Hotel Extensions/locales/mt.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "Ideali għal: {min}-{max} persuni",
+    sizeText: "Daqs: {size} m²",
+
+    // General UI
+    next: "Li jmiss", back: "Lura", edit: "Editja", submit: "Issottometti", selected: "Magħżul", days: "jiem", day: "jum",
+    // Step 1: Dates
+    titleChooseYourStay: "Agħżel id-dati tal-waqfa tiegħek", undertitleChooseYourStay: "Jekk jogħġbok agħżel il-perjodu tal-waqfa ppjanata tiegħek", noDatesSelected: "Ebda dati magħżula",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Agħżel kemm trid toqgħod fil-perjodu magħżul", selectDuration: "Agħżel it-tul", exactlyAsSpecified: "Eżatt kif speċifikat", threeDays: "3 ijiem", fourDays: "4 ijiem", fiveDays: "5 ijiem", sixDays: "6 ijiem", sevenDays: "7 ijiem", eightDays: "8 ijiem", nineDays: "9 ijiem", tenDays: "10 ijiem", enterExactDates: "Jew daħħal it-tul eżatt tal-vjaġġ f’jiem", from: "Minn", until: "Sa", youCanChoose: "Tista' żżid sa massimu ta'", maxDaysAllowedText: "Tista' żżid sa {totalDays} ġurnata",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Agħżel l-akkomodazzjoni tiegħek", undertitlePickAccommodation: "Tista' tagħżel tip wieħed jew aktar ta’ akkomodazzjoni. Jekk qed tirriżerva għal aktar persuni, żid aktar skont il-bżonn.", FormSuites: "Suits", FormRooms: "Kmamar", accommodationCategories: "Kategoriji ta’ akkomodazzjoni",
+    // Step 4: Travelers
+    titleWhoTraveling: "Min se jivvjaġġa?", undertitleWhoTraveling: "Daħħal in-numru ta’ adulti u tfal u għidilna jekk għandekx talbiet speċjali.", adults14Plus: "Adulti (14+)", children: "Tfal", specialRequests: "Talbiet speċjali (eż. dieta, aċċessibilità, annimali domestiċi)", specialRequestsTxt: "Jekk jogħġbok għidilna jekk għandekx xi talbiet speċjali.",
+    // Step 5: Contact
+    titleContactInformation: "Informazzjoni ta’ Kuntatt", undertitleContactInformationBooking: "Jekk jogħġbok ipprovdi d-dettalji tiegħek sabiex inkunu nistgħu nlestu r-riżerva tiegħek.", firstName: "Isem", lastName: "Kunjom", emailVF: "Email", phoneNumber: "Numru tat-Telefon",
+    // Step 6: Review
+    titleReview: "Irrevedi u Issottometti", undertitleReviewBooking: "Jekk jogħġbok irrevedi d-dettalji tar-riżerva tiegħek qabel ma tibgħat.", reviewStayDates: "Dati tal-waqfa", reviewTravelDates: "Dati tal-vjaġġ", reviewDurationOFStay: "Tul tal-waqfa", reviewAccommodation: "Akkomodazzjoni", reviewTypes: "Tipi", reviewTravelers: "Vjaġġaturi", reviewAdults: "Adulti", reviewSpecialRequests: "Talbiet Speċjali", firstAndLastname: "Isem u kunjom",
+    // Final Message
+    thankSubmission: "Grazzi tat-talba tiegħek!", formSubmitted: "Il-formola tiegħek intbagħtet b’suċċess.", formTeam: "It-tim tagħna se jikkuntattjak dalwaqt.",
+};
