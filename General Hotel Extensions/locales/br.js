dd8c320467b057ba47d7a5dabba70ae402a2ba19
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-08-27 16:17:00 +0200

    Create br.js

---
+++ General Hotel Extensions/locales/br.js
@@ -0,0 +1,22 @@
+export const translations = {
+    // Formatting for dynamic room data
+    capacityText: "A-feson evit: {min}-{max} den",
+    sizeText: "Ment: {size} m²",
+
+    // General UI
+    next: "Da heul", back: "Distreiñ", edit: "Aozañ", submit: "Kas", selected: "Diuzet", days: "devezhioù", day: "devezh",
+    // Step 1: Dates
+    titleChooseYourStay: "Dibab deizioù chomadenn", undertitleChooseYourStay: "Dibabit mar plij ar prantad chomadenn bet raktreset", noDatesSelected: "N'eus bet dibabet deiziad ebet",
+    // Step 2: Duration
+    undertitleChooseYourStay2: "Dibabit pegeit e fell deoc’h chom e-pad ar prantad dibabet", selectDuration: "Dibab pad", exactlyAsSpecified: "Evel ma’z eo spisaet", threeDays: "3 devezh", fourDays: "4 devezh", fiveDays: "5 devezh", sixDays: "6 devezh", sevenDays: "7 devezh", eightDays: "8 devezh", nineDays: "9 devezh", tenDays: "10 devezh", enterExactDates: "Pe roit pad resis ar veaj e devezhioù", from: "Adalek", until: "Betek", youCanChoose: "Gallout a rit ouzhpennañ betek", maxDaysAllowedText: "Gallout a rit ouzhpennañ betek {totalDays} devezh",
+    // Step 3: Accommodation
+    titlePickAccommodation: "Dibab ho chomadenn", undertitlePickAccommodation: "Gallout a rit dibab ur seurt pe meur a seurt chomadenn. Mard oc’h o prennañ evit meur a zen, ouzhpennit muioc’h mar bez ezhomm.", FormSuites: "Suitoù", FormRooms: "Saloù", accommodationCategories: "Rummadoù chomadenn",
+    // Step 4: Travelers
+    titleWhoTraveling: "Piv a vo o veajiñ?", undertitleWhoTraveling: "Roit an niver a dud gour hag a vugale ha roit deomp da c’houzout m’ho peus goulennoù dibar.", adults14Plus: "Tud gour (14+)", children: "Bugale", specialRequests: "Goulennoù dibar (da skouer: boued, haeziñ, loened-kevredigezhel)", specialRequestsTxt: "Roit deomp da c’houzout m’ho peus goulennoù dibar.",
+    // Step 5: Contact
+    titleContactInformation: "Titouroù kennask", undertitleContactInformationBooking: "Roit mar plij ho titouroù evit ma c’hellomp peurechuiñ ho prennañ.", firstName: "Anv-bihan", lastName: "Anv-familh", emailVF: "Postel", phoneNumber: "Niverenn bellgomz",
+    // Step 6: Review
+    titleReview: "Adwelet & Kas", undertitleReviewBooking: "Adwelet ho titouroù prennañ a-raok kas anezhañ.", reviewStayDates: "Deizioù chomadenn", reviewTravelDates: "Deizioù beaj", reviewDurationOFStay: "Pad chomadenn", reviewAccommodation: "Chomadenn", reviewTypes: "Seurtoù", reviewTravelers: "Beajourien", reviewAdults: "Tud gour", reviewSpecialRequests: "Goulennoù dibar", firstAndLastname: "Anv-bihan hag Anv-familh",
+    // Final Message
+    thankSubmission: "Trugarez evit ho goulenn!", formSubmitted: "Kaset eo bet ho furmskrid gant berzh.", formTeam: "Ar skipailh a zarempredo ac’hanoc’h a-benn nebeut.",
+};
