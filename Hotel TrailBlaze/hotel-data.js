9ce9d53a237823c0d5d79d4cbf7207ae448d582f
TrailblazeInnovation <tech@trailblazeinnovation.com>
2025-09-01 16:09:41 +0200

    Update hotel-data.js

--- Hotel TrailBlaze/hotel-data.js
+++ Hotel TrailBlaze/hotel-data.js
@@ -2,87 +2,87 @@ export const hotelData = {
   "hotelName": "Hotel Therme Meran",
   "currency": "EUR",
   "categoryImages": {
-    "suites": "https://www.hoteltermemerano.it/img/sales-content/3d92a810-b199-4492-b49b-09f406eafb58/426/426/crop/wisthaler.com%2D24%2D02%2Dhtm%2Dhw96182%2Dweb.JPG",
-    "rooms": "https://www.hoteltermemerano.it/img/sales-content/fd938c17-b58e-46b2-b3d5-f744a5e3a439/426/426/crop/wisthaler.com%2D24%2D03%2Dfisi%2Dhtm%2Dhw98706%2D2.webp"
+    "suites": "https://cm4-production-assets.s3.amazonaws.com/1743412467586-suite-1.png",
+    "rooms": "https://cm4-production-assets.s3.amazonaws.com/1743412426951-screenshot-2025-01-12-212108.png"
   },
   "rooms": [
     {
       "id": "suite_01",
       "category": "suite",
-      "name": { "default": "Vita Suite" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/3d92a810-b199-4492-b49b-09f406eafb58/426/426/crop/wisthaler.com%2D24%2D02%2Dhtm%2Dhw96182%2Dweb.JPG",
+      "name": { "default": "Presidential Suite" },
+      "image": "https://cm4-production-assets.s3.amazonaws.com/1743412467586-suite-1.png",
       "sizeSqm": 60,
       "capacity": { "min": 1, "max": 4 }
     },
     {
       "id": "suite_02",
       "category": "suite",
-      "name": { "default": "Suite" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/c9c5a713-8d8e-4f21-b7d9-110c82164c9f/426/426/crop/3205%2D316460.webp",
+      "name": { "default": "Royal Suite" },
+      "image": "https://cm4-production-assets.s3.amazonaws.com/1743412475967-suite-2.png",
       "sizeSqm": 60,
       "capacity": { "min": 1, "max": 4 }
     },
     {
       "id": "suite_03",
       "category": "suite",
-      "name": { "default": "Calla Suite" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/8bc77ab5-3167-4783-a8ed-7d42b453bebc/426/426/crop/3205%2D316408.webp",
+      "name": { "default": "Panorama Suite" },
+      "image": "https://cm4-production-assets.s3.amazonaws.com/1743412482837-suite.png",
       "sizeSqm": 72,
       "capacity": { "min": 1, "max": 4 }
     },
     {
       "id": "room_01",
       "category": "room",
-      "name": { "default": "Acqua" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/fd938c17-b58e-46b2-b3d5-f744a5e3a439/426/426/crop/wisthaler.com%2D24%2D03%2Dfisi%2Dhtm%2Dhw98706%2D2.webp",
+      "name": { "default": "Premium Room" },
+      "image": "https://cm4-production-assets.s3.amazonaws.com/1743412426951-screenshot-2025-01-12-212108.png",
       "sizeSqm": 30,
       "capacity": { "min": 1, "max": 3 }
     },
     {
       "id": "room_02",
       "category": "room",
-      "name": { "default": "MeranO" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/49655acb-a119-45d2-9f80-44cacf9c817b/426/426/crop/wisthaler.com%2D24%2D03%2Dfisi%2Dhtm%2Dhw90489.webp",
-      "sizeSqm": 30,
+      "name": { "default": "Deluxe Room" },
+      "image": "https://cm4-production-assets.s3.amazonaws.com/1743412436301-screenshot-2025-01-12-212321.png",
+      "sizeSqm": 35,
       "capacity": { "min": 1, "max": 3 }
     },
     {
       "id": "room_03",
       "category": "room",
-      "name": { "default": "Loggia" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/55e9bc7c-3df8-4236-bb6f-f247d1db00a1/426/426/crop/01.JPG",
-      "sizeSqm": 30,
+      "name": { "default": "Superior Room" },
+      "image": "https://cm4-production-assets.s3.amazonaws.com/1743412443481-screenshot-2025-01-12-213453.png",
+      "sizeSqm": 38,
       "capacity": { "min": 1, "max": 3 }
     },
     {
       "id": "room_04",
       "category": "room",
-      "name": { "default": "Cedro" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/d6ecc731-5ed8-4775-8754-f145467aecf7/426/426/crop/wisthaler.com%2D24%2D02%2Dhtm%2Dhw96272%2Dweb.JPG",
+      "name": { "default": "Signature Room" },
+      "image": "https://cm4-production-assets.s3.amazonaws.com/1743412450111-screenshot-2025-01-12-214111.png",
       "sizeSqm": 28,
       "capacity": { "min": 1, "max": 2 }
     },
     {
       "id": "room_05",
       "category": "room",
-      "name": { "de": "Doppelzimmer Superior", "default": "Double Room Superior" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/58857828-7f98-4af5-aa9a-c5c1ecc0b270/426/426/crop/3205%2D262053.webp",
-      "sizeSqm": 30,
+      "name": { "default": "Executive Room" },
+      "image": "https://i.postimg.cc/d1Sbjqym/6ca0df66-b4f3-415d-b3b8-c8d1552859a7.png",
+      "sizeSqm": 40,
       "capacity": { "min": 1, "max": 3 }
     },
     {
       "id": "room_06",
       "category": "room",
-      "name": { "de": "Doppelzimmer Standard", "default": "Double Room Standard" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/8d53c197-52f2-4aca-b8bb-b071edc8b22f/426/426/crop/3205%2D347281.webp",
-      "sizeSqm": 30,
-      "capacity": { "min": 1, "max": 3 }
+      "name": { "default": "Alpine Room" },
+      "image": "https://i.postimg.cc/hPbqHqWg/7e2e5cf4-f790-42e3-af82-4d57ab5bb414.png",
+      "sizeSqm": 45,
+      "capacity": { "min": 1, "max": 4 }
     },
     {
       "id": "room_07",
       "category": "room",
-      "name": { "de": "Doppelzimmer Standard Junior", "default": "Double Room Standard Junior" },
-      "image": "https://www.hoteltermemerano.it/img/sales-content/2331341d-bcd2-4e2b-acb7-9e44eda7da22/426/426/crop/3205%2D262051.webp",
+      "name": { "default": "Classic Room" },
+      "image": "https://i.postimg.cc/zGX46bFS/03360da3-4f10-4cd5-9bd0-dc128263bca7.png",
       "sizeSqm": 28,
       "capacity": { "min": 1, "max": 2 }
     }
